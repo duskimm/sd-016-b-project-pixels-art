db1dda5fb17eaa123787df176e5bfba458a97170
blmarquess <blmarques.dev@gmail.com>
2021-09-10 17:41:54 -0300

    2nd and 3rd requirement done

---
+++ script.js
@@ -0,0 +1,12 @@
+const gameBoardSize = 5;
+
+function makeBox() {
+  let box = document.createElement('div');
+  box.className = pixel;
+  return box;
+}
+function makeGameBoard(size) {
+  for (let board in size) {
+
+  }
+}
