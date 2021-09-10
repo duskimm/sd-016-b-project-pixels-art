e8d22c7335de1adc101571281efecb78cdd56ce1
lucas <araujo2012@hotmail.com>
2021-09-10 16:34:18 -0300

    Requisito 7 completo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function newChild(type, text, classs, id) {
-  let newChildItem = document.createElement(type);
+  const newChildItem = document.createElement(type);
   newChildItem.innerText = text;
   if (classs !== '') {
     newChildItem.className = classs;
@@ -11,20 +11,29 @@ function newChild(type, text, classs, id) {
 }
 
 function createLines() {
-  let board = document.querySelector('#pixel-board');
+  const board = document.querySelector('#pixel-board');
   for (let index = 0; index < 5; index += 1) {
     board.append(newChild('div', '', 'line', ''));
   }
 }
 
 function createPixels() {
-  let lines = document.querySelectorAll('.line');
-  for (let line of lines) {
+  const lines = document.querySelectorAll('.line');
+  for (const line of lines) {
     for (let index = 0; index < 5; index += 1) {
       line.append(newChild('div', '', 'pixel', ''));
     }
   }
 }
+
+//Metodo event bubbling adquirido atraves deste link https://gomakethings.com/attaching-multiple-elements-to-a-single-event-listener-in-vanilla-js/
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('color')) {
+    const selectedColor = document.querySelector('.selected');
+    selectedColor.classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+}, false);
 
 window.onload = function () {
   createLines();
