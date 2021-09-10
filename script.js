aa03d8a7845dc00f368221d062696197119ae326
Taina <tainnaps@gmail.com>
2021-09-10 17:30:16 -0300

    Requisitos 2 e 3 concluidos

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ function setId(elementName, idName) {
   element.id = idName;
 }
 
+function setClass(elementName, className) {
+  const element = elementName;
+  element.classList.add(className);
+}
+
 function setInnerText(elementName, text) {
   const element = elementName;
   element.innerText = text;
@@ -25,3 +30,29 @@ setId(pageTitle, 'title');
 setInnerText(pageTitle, 'Paleta de Cores');
 
 addElementToDOM(pageBody, pageTitle);
+
+const colorPalette = creatElementHTML('div');
+
+setId(colorPalette, 'color-palette');
+
+addElementToDOM(pageBody, colorPalette);
+
+const numberOfColors = 5;
+
+for (let index = 0; index < numberOfColors; index += 1) {
+  const color = creatElementHTML('div');
+  setClass(color, 'color');
+  addElementToDOM(colorPalette, color);
+}
+
+const colorList = document.getElementsByClassName('color');
+
+setClass(colorList[0], 'black');
+
+setClass(colorList[1], 'blue');
+
+setClass(colorList[2], 'pink');
+
+setClass(colorList[3], 'yellow');
+
+setClass(colorList[4], 'green');
