45f23ba775361fa07701914531a41249e843c9cd
Everton-Curti <evercurti@gmail.com>
2021-09-10 17:55:21 -0300

    exercicio 2 resolvido

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ criarTitulo.id = 'title';
 criarTitulo.innerHTML = 'Paleta de Cores';
 capturaHeader.appendChild(criarTitulo);
 
+let capturarSection = document.querySelector('section');
+capturarSection.id = 'color-palette';
+let coresIndividuais = 4;
+for (let index = 0; index < coresIndividuais; index += 1){
+    let criarDivsPaleta = coresIndividuais;
+    criarDivsPaleta = document.createElement('div');
+    criarDivsPaleta.className = 'color';
+    capturarSection.appendChild(criarDivsPaleta);
+}
+
+let configurarCoresPaletas = document.querySelectorAll('.color');
+configurarCoresPaletas[0].style.backgroundColor = 'black';
+configurarCoresPaletas[1].style.backgroundColor = 'red';
+configurarCoresPaletas[2].style.backgroundColor = 'blue';
+configurarCoresPaletas[3].style.backgroundColor = 'green';
+
+
+
+
 
 
 
