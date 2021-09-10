45ccc9e87505c09a46094b86fea31af9daa41074
Breno Danyel <bdls1704@hotmail.com>
2021-09-10 15:38:52 -0300

    add requisito 1 e 2

---
+++ script.js
@@ -0,0 +1,24 @@
+const predefinedColors = ['red', 'black', 'blue', 'orange']
+
+function createColorPallete() {
+    const container = document.createElement('div');
+    container.id = 'color-palette';
+
+    for (let color of predefinedColors) {
+        const colorDiv = document.createElement('div');
+        colorDiv.classList.add('color');
+        colorDiv.style.backgroundColor = color;
+
+        container.appendChild(colorDiv);
+
+        console.log(color);
+    }
+
+    document.body.appendChild(container);
+}
+
+window.onload = function () {
+    createColorPallete();
+
+    console.log('ready');
+}
