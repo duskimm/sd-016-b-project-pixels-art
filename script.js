c0551831cbe8f6a760126e4e3bab854546237b05
João Victor Arouca <joao.silveira@estudante.ufla.br>
2021-09-10 17:03:42 -0300

    requisitos 4 e 5

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const cabecalho = document.createElement('header');
 const h1 = document.createElement('h1');
 const paletaDeCores = document.createElement('section');
 const cores = document.getElementsByClassName('color');
+const pixelBoard = document.createElement('div');
+const pixels = document.getElementsByClassName('pixel');
 
+document.body.style.backgroundColor = 'coral'
 /* Primeira cor */
 const corPreta = () => {
-  let cor1 = document.querySelectorAll('.color');
+  const cor1 = document.querySelectorAll('.color');
   cor1[0].style.backgroundColor = 'black';
   cor1[0].classList.add('selected');
-}
+};
 
 /* Cabeçalho e Título */
 document.body.appendChild(cabecalho);
@@ -23,12 +26,12 @@ cabecalho.style.marginBottom = '40px';
 
 /* Paleta de cores */
 document.body.appendChild(paletaDeCores);
-/* paletaDeCores.style.backgroundColor = 'black'; */
+paletaDeCores.style.width = '40%';
 paletaDeCores.style.height = '40px';
+paletaDeCores.style.margin = 'auto';
+paletaDeCores.style.justifyContent = 'center';
 paletaDeCores.id = 'color-palette';
 paletaDeCores.classList.add('container');
-paletaDeCores.style.justifyContent = 'center';
-
 
 const generateColors = () => {
   for (let index = 0; index < 4; index += 1) {
@@ -36,9 +39,8 @@ const generateColors = () => {
     paletaDeCores.appendChild(divColor);
     divColor.classList.add('color');
     divColor.style.border = '1px solid black';
-    
   }
-}
+};
 generateColors();
 
 const styleColors = () => {
@@ -48,17 +50,46 @@ const styleColors = () => {
     cores[index].style.margin = '5px';
     cores[index].style.borderRadius = '45px';
   }
-}
+};
 styleColors();
 
 const generateRandomColor = () => {
   for (let index = 1; index < cores.length; index += 1) {
-    let r = Math.floor(Math.random() * (255));
-    let g = Math.floor(Math.random() * (255));
-    let b = Math.floor(Math.random() * (255));
-    let rgb = `rgb(${r}, ${g}, ${b})`;
+    const r = Math.floor(Math.random() * (255));
+    const g = Math.floor(Math.random() * (255));
+    const b = Math.floor(Math.random() * (255));
+    const rgb = `rgb(${r}, ${g}, ${b})`;
     cores[index].style.backgroundColor = rgb;
-  }  
-}
+  }
+};
 generateRandomColor();
-window.onload = corPreta();
+
+/* Quadro de pixels */
+document.body.appendChild(pixelBoard);
+pixelBoard.id = 'pixel-board';
+pixelBoard.classList.add('container');
+pixelBoard.style.maxWidth = '200px';
+pixelBoard.style.height = '200px';
+pixelBoard.style.margin = '100px auto';
+pixelBoard.style.justifyContent = 'center';
+pixelBoard.style.flexWrap = 'wrap';
+
+const generateBoard = () => {
+  for (let index = 0; index < 25; index += 1) {
+    const divPixels = document.createElement('div');
+    pixelBoard.appendChild(divPixels);
+    divPixels.classList.add('pixel');
+    divPixels.style.border = '1px solid black';
+  }
+};
+generateBoard();
+
+const stylePixels = () => {
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.width = '40px';
+    pixels[index].style.height = '40px';
+    pixels[index].style.backgroundColor = 'white';
+  }
+};
+stylePixels();
+window.onload = corPreta;
