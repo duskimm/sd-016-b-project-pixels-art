0195f251dfa708139f516db864f608de8a35d38b
Erik Duarte Rodrigues <erikduarterodrigues@gmail.com>
2021-09-10 18:42:29 -0300

    Entrega desafio 8

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const color = document.querySelectorAll('.color');
+const pixel = document.querySelectorAll('.pixel');
 
 function selectedColor(event) {
     for (let i = 0; i < color.length; i += 1) {
@@ -10,3 +11,15 @@ function selectedColor(event) {
   for (let i = 0; i < color.length; i += 1) {
     color[i].addEventListener('click', selectedColor);
   }
+
+  function paint(event) {
+    const coloredpixel = event.target;
+    const selected = document.querySelector('.selected');
+    const compStyle = window.getComputedStyle(selected);
+    coloredpixel.style.backgroundColor = compStyle.backgroundColor;
+  }
+
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].addEventListener('click', paint);
+  }
+
