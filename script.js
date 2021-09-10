9561b453ff349dc8a74f74d413eee674b3e02bf4
Alissandra <alissandraraysa@gmail.com>
2021-09-10 16:18:58 -0300

    deixa apenas 4 cores

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function criaDivCores (array){
     let arrayCores = ["black", "red", "green", "blue"];
     let divPaleta = document.getElementById('color-palette');
-    for (let i = 0; i <=arrayCores.length; i+=1){
+    for (let i = 0; i <arrayCores.length; i+=1){
         let caixa = document.createElement('div');
         caixa.style.background = arrayCores[i];
         caixa.className = "color"
