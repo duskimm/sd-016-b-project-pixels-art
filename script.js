02cd767fade514fa12805179219dfd2a8fccfd94
igor rafael <igor.rafael.briano@gmail.com>
2021-09-10 18:19:06 -0300

    ajuste no item 6

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 window.onload = function() {
   selectColor();
-
   function selectColor () {
-    let firstload = true;
-    if(firstload){
       let colorSelect = document.querySelector('.color');
       colorSelect.classList.add('selected');
-    }else {
-
-    }
+      firstload = false;
   }
 }
