baf5b94eb75c24bde83120ca6dad6f24219438c1
Israel Sant'Anna <israel.santanna@gmail.com>
2021-09-10 16:02:25 -0300

    Requirement 3

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-function getRandomColor(seed) {
+const colors = [];
+
+function isInvalidColor(color) {
+  return color.endsWith('100%, 100%)') || color.endsWith('0%, 0%)') || colors.includes(color);
+}
+
+function getRandomColor() {
   let randomColor = `hsl(
-    ${Math.ceil(Math.random() * 357) + seed},
+    ${Math.ceil(Math.random() * 360)},
     ${Math.ceil(Math.random() * 100)}%,
     ${Math.ceil(Math.random() * 100)}%
   )`;
-  if (randomColor.endsWith('100%, 100%)')) {
-    randomColor = getRandomColor();
-  }
+  if (isInvalidColor(randomColor)) randomColor = getRandomColor();
   return randomColor;
+}
+
+function setColor(element, color) {
+  element.setAttribute('style', `background-color: ${color};`);
 }
 
 function generateColorPalette() {
   const colorPaletteSlots = document.getElementById('color-palette').children;
-  const colors = [];
-  for (let index = 0; index < colorPaletteSlots.length; index += 1) {
-    colors.push(getRandomColor(index));
+  for (let index = 0; index < colorPaletteSlots.length - 1; index += 1) {
+    colors.push(getRandomColor());
   }
+  setColor(colorPaletteSlots[0], 'black');
   colors.forEach((color, index) => {
-    colorPaletteSlots[index].setAttribute('style', `background-color: ${color};`);
+    setColor(colorPaletteSlots[index + 1], color);
   });
 }
 
 window.onload = () => {
   generateColorPalette();
 };
+
+const inputColor = document.querySelectorAll('.color:not(.fixed-color)');
+inputColor.forEach((input) => {
+  input.addEventListener('click', (evt) => setColor(evt.target, getRandomColor()));
+});
