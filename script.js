1c42cd1e2deba7c2e388837e4f248a0dcf0ddf85
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-10 18:17:38 -0300

    requisito 10 parcial

--- script.js
+++ script.js
@@ -1,32 +1,60 @@
-function createPixel(className, parent) {
+// Cria os pixels
+function createDiv(className, parent) {
   let newPixel = document.createElement('div');
   newPixel.className = className;
   parent.appendChild(newPixel);
 }
 
+let generateButton = document.getElementById('generate-board');
+
+let pixelBoard = document.getElementById('pixel-board');
+
 let lines = document.getElementsByClassName('pixel-line');
 
-for (let index = 0; index < lines.length; index += 1) {
-  for (let i = 1; i <= 5; i += 1) {
-    createPixel('pixel', lines[index]);
+// função que cria os pixels de acordo com o input recebido
+function generateLines() {
+  let input = parseInt(document.getElementById('board-size').value);
+  for (let index = 1; index <= input; index += 1) {
+    createDiv('pixel-line', pixelBoard);
+  }
+
+  document.getElementById('board-size').value = '';
+}
+
+function generatePixels() {
+  for (let index = 0; index < lines.length; index += 1) {
+    for (let i = 1; i <= lines.length; i += 1) {
+      createDiv('pixel', lines[index]);
+    }
   }
 }
+
+// Gera os pixels iniciais
+generatePixels();
+
+// Função que checa se já existe um quadro de pixels
+function checkBoard() {
+  pixelBoard.innerHTML = '';
+  generateLines();
+  generatePixels();
+  console.log(pixelBoard);
+
+}
+
+generateButton.addEventListener('click', checkBoard);
 
 let colors = document.getElementsByClassName('color');
 
-function removeSelected() {
+// Função que remove/adiciona a classe selected as cores
+function selectColor(event) {
   for (let i = 0; i < colors.length; i += 1) {
     colors[i].classList.remove('selected');
   }
-}
-
-function addSelected(event) {
   event.target.classList.add('selected');
 }
 
 for (let index = 0; index < colors.length; index += 1) {
-  colors[index].addEventListener('click', removeSelected);
-  colors[index].addEventListener('click', addSelected);
+  colors[index].addEventListener('click', selectColor);
 }
 
 let pixels = document.getElementsByClassName('pixel');
