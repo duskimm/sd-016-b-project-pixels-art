d93f675746a3c797c9f875e063e1c11c5a055b9e
Gabriel Oliveira <gabrielfrox@gmail.com>
2021-09-10 20:06:31 -0300

    Requisito 7 Lint OK

--- script.js
+++ script.js
@@ -25,8 +25,28 @@ function lineDef() {
 lineDef();
 
 // Requisito 6 - Classe selected
-function colorSelected() {
+function defaultColorSelected() {
   localStorage.setItem('defaultColor', 'black');
   colorBtns[0].className = 'color selected';
+}
+defaultColorSelected();
+
+// Requisito 7 - Aplicar classe a cor escolhida
+const paleta = document.querySelector('#color-palette');
+let targetAtual = '';
+
+function unselect() {
+  const selectedToBeRemoved = document.querySelector('.selected');
+  selectedToBeRemoved.className = 'color';
+}
+
+function colorSelected() {
+  paleta.addEventListener('click', (event) => { // Referência para a arrowFunction https://www.30secondsofcode.org/articles/s/javascript-arrow-function-event-listeners
+    if (event.target.className === 'color') {
+      unselect();
+      targetAtual = event.target;
+      targetAtual.className = 'color selected';
+    }
+  });
 }
 colorSelected();
