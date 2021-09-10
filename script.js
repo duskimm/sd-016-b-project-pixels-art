372f2ce3dea0943945b8b4b573e45c1b80073429
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-10 16:10:50 -0300

    requisito 7 concluido

--- script.js
+++ script.js
@@ -10,5 +10,21 @@ for (let index = 0; index < lines.length; index += 1) {
   for (let i = 1; i <= 5; i += 1) {
     createPixel('pixel', lines[index]);
   }
-  console.log(lines[index]);
+}
+
+let color = document.getElementsByClassName('color');
+
+function removeSelected() {
+  for (let i = 0; i < color.length; i += 1) {
+    color[i].classList.remove('selected');
+  }
+}
+
+function addSelected(event) {
+  event.target.classList.add('selected');
+}
+
+for (let index = 0; index < color.length; index += 1) {
+  color[index].addEventListener('click', removeSelected);
+  color[index].addEventListener('click', addSelected);
 }
