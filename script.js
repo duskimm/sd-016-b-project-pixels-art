e727b13459459257d074709f49baaa29b0340a4f
Marcos Carvalho <marcosfilhosp@hotmail.com>
2021-09-10 18:44:51 -0300

    Fix lint issues

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createColumnLines(size) {
       column.appendChild(lines);
     }
   }
-} 
+}
 createColumnLines(5);
 
 function startingColors() {
@@ -22,6 +22,69 @@ function startingColors() {
   document.querySelector('#blue').style.backgroundColor = 'blue';
   const black = document.querySelector('#black');
   black.className = 'color selected';
-} 
+}
 startingColors();
 
+function choosePalette() {
+  paleta.addEventListener('click', (cor) => {
+    const eventTarget = cor.target;
+    const color = document.getElementsByClassName('color');
+    for (let index = 0; index < color.length; index += 1) {
+      color[index].classList.remove('selected');
+      if (eventTarget.className === 'color') {
+        eventTarget.classList.add('selected');
+      }
+    }
+  });
+}
+choosePalette();
+
+function paint() {
+  pixelBoard.addEventListener('click', (pixel) => {
+    const element = pixel.target;
+    const selected = document.querySelector('.selected').style.backgroundColor;
+    if (element.className === 'pixel') {
+      element.style.backgroundColor = selected;
+    }
+  });
+}
+paint();
+
+function blankPixels() {
+  const button = document.querySelector('#clear-board');
+  button.addEventListener('click', () => {
+    const board = document.querySelectorAll('.pixel');
+    for (let index = 0; index < board.length; index += 1) {
+      const element = board[index];
+      element.style.backgroundColor = 'white';
+    }
+  });
+}
+blankPixels();
+
+function sizePixel() {
+  const input = document.querySelector('#board-size');
+  const buttonVQV = document.querySelector('#generate-board');
+  buttonVQV.addEventListener('click', () => {
+    if (input.value === '') {
+      alert('Board inválido!');
+    } else if (input.value < 5) {
+      input.value = 5;
+    } else if (input.value > 50) {
+      input.value = 50;
+    }
+    pixelBoard.innerHTML = '';
+    createColumnLines(input.value);
+  });
+} sizePixel();
+
+function randomColor() {
+  const color = document.getElementsByClassName('color');
+  for (let index = 1; index < color.length; index += 1) {
+    const r = parseInt(Math.random() * 255, 10);
+    const g = parseInt(Math.random() * 255, 10);
+    const b = parseInt(Math.random() * 255, 10);
+    color[index].style.backgroundColor = `rgba(${r}, ${g}, ${b})`;
+  }
+}
+randomColor();
