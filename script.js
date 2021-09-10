4164b7564d16247c2f92ca4e02be87da6817d315
Guilherme Rosa <gui.rsa@hotmail.com>
2021-09-10 16:53:47 -0300

    exercice 4

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('vamo q vamo')
+console.log('vamo q vamo');
+const colorPaintBlack = document.getElementById('blackColor');
+const colorPaintRed = document.getElementById('redColor');
+
+function selectedVerfifier() {
+  const selected = document.querySelectorAll('.color');
+  for (let index = 0; index<selected.length; index += 1) {
+    selected[index].classList.add('selected');
+    selected[index].classList.remove('selected');
+  }
+}
+selectedVerfifier();
