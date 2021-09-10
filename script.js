b7a67acacd94f8ea9b00665831df45f560dedd01
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-09-10 17:07:11 -0300

    Adiciona requisito 8

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ document.getElementById('black').classList.add('selected');
 
 let color = document.getElementById('color-palette')
 
-
 color.addEventListener('click', function(event) {
 
   for (let index = 0; index < color.children.length; index++) {
     color.children[index].className = 'color';
   }
   event.target.classList.add('selected');
+
 })
+
+
+let pixel = document.querySelectorAll('.pixel');
+
+for (let index = 0; index < pixel.length; index++) {
+  pixel[index].addEventListener('click', function(event) {
+    let selected = document.querySelector('.selected');
+    event.target.id = selected.id;
+  })
+}
