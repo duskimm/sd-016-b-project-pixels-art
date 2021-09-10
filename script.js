735a8ea307045d99cac7045f6756b15382e8ac7b
Rael N. Pires <rnogpiresfinancial@gmail.com>
2021-09-10 17:50:23 -0300

    Requisito 4

---
+++ script.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+  let numberOfLines = 25;
+
+  function createDivs(numberOfDivs) {
+    for (let index = 1; index <= numberOfDivs; index += 1) {
+      let pixelDiv = document.createElement('div');
+      pixelDiv.className = 'pixel';
+      pixelDiv.style.backgroundColor = 'white';
+      pixelDiv.style.color = 'white';
+      let board = document.getElementById('pixel-board');
+      board.appendChild(pixelDiv);
+    }
+  }
+  
+  createDivs(numberOfLines);
+
+  // function createPixels(className) {     //className 'pixels';
+  //   let pixel = document.createElement('div');
+  //   pixel.className = className;
+  //   pixel.style.backgroundColor = 'white';
+  //   pixel.style.color = 'white';
+  //   return pixel;
+  // }
+
+  // function addPixelsToBoard() {
+  //   for (index = 0; index < numberOfLines; index += 1) {
+  //     let pixel = createPixels('pixels');
+  //     let pixelDiv = document.querySelectorAll('.pixels-div');
+  //     pixelDiv[index].appendChild(pixel);
+  //   }
+  // }
+
+  // addPixelsToBoard();
+}
