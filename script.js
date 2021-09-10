08520b09f667d647fcbb4c2da43b07c9849646a4
MychaelJales <mychael_duarte@hotmail.com>
2021-09-10 17:18:26 -0300

    finaliza os requisitos 1, 2, 3 e 4

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-window.onload = function() {
-  let numberOfColors = 5;
+const numberGridColumnsAndLines = 5;
 
-  let arrayColors = ['green', 'blue', 'red', 'gray', 'black'];
+let lines = document.querySelectorAll('.gridColumns');
 
-  let colorPalette = document.getElementById('color-palette');
-  for (let index = 0; index < numberOfColors; index += 1){
-    let divColorPalette = document.createElement('div');
+// Criando as divs da paleta de cores
+function colorPalettesFormation(arrayColors) {
+  const colorPalette = document.getElementById('color-palette');
+  for (let index = 0; index < arrayColors.length; index += 1) {
+    const divColorPalette = document.createElement('div');
     colorPalette.appendChild(divColorPalette);
     divColorPalette.className = arrayColors[index];
-    divColorPalette.classList.add('containerPalette');
+    divColorPalette.classList.add('color');
   }
 }
+
+// Criando as linhas do grid
+function gridLines(divLines) {
+  for (let index = 0; index < numberGridColumnsAndLines; index += 1) {
+    const divGridLines = document.createElement('div');
+    divLines.appendChild(divGridLines);
+    divGridLines.classList.add('pixel');
+  }
+}
+
+// Criando divs do grid de pixels
+function gridFormation() {
+  const pixelBoard = document.getElementById('pixel-board');
+
+  for (let index = 0; index < numberGridColumnsAndLines; index += 1) {
+    const divGridColumns = document.createElement('div');
+    pixelBoard.appendChild(divGridColumns);
+    divGridColumns.classList.add('gridColumns');
+    lines = document.querySelectorAll('.gridColumns');
+    gridLines(lines[index]);
+  }
+}
+
+window.onload = function () {
+  const arrayColorsPalette = ['black', 'blue', 'red', 'green'];
+
+  colorPalettesFormation(arrayColorsPalette);
+
+  gridFormation();
+};
