5ce73ba76bf61d6766b4a21968f72e3dada0232c
Matheus <matheusmoura2110@gmail.com>
2021-09-10 16:58:23 -0300

    requisito 3 em css

---
+++ script.js
@@ -0,0 +1,8 @@
+function addColor() {
+  const colors = ['black', 'white', 'blue', 'red']
+  const selectC = document.queryselector = (".color")
+  for (let index of colors) {
+    colors[index].style.backgroundColor = selectC[index]
+  }
+}
+;
