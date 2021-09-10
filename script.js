8cc4ab6b7518b893fd88ff119d33de0dc51afbb2
Gabriel Oliveira <gabrielfrox@gmail.com>
2021-09-10 16:40:51 -0300

    Requisitos 4 e 5 Lint OK

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ colorBtns[0].style.backgroundColor = 'black';
 colorBtns[1].style.backgroundColor = 'red';
 colorBtns[2].style.backgroundColor = 'green';
 colorBtns[3].style.backgroundColor = 'purple';
+
+// Requisito 4 = Quadro de pixels
+const pixelBoard = document.querySelector('#pixel-board');
+const pixelBoardLines = pixelBoard.children;
+
+function pixelAttribution(index) {
+  for (let i = 0; i < 5; i += 1) {
+    const onePixel = document.createElement('section');
+    onePixel.className = 'pixel';
+    pixelBoardLines[index].appendChild(onePixel);
+  }
+}
+
+function lineDef() {
+  for (let i = 0; i < 5; i += 1) {
+    pixelAttribution(i);
+  }
+}
+lineDef();
