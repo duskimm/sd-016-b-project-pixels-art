acb2fdd94be935b4ccb7c17ae102931e792a8ff6
MychaelJales <mychael_duarte@hotmail.com>
2021-09-10 15:40:18 -0300

    resolvendo os requisitos 1 e 2

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+window.onload = function() {
+  let numberOfColors = 5;
 
+  let arrayColors = ['green', 'blue', 'red', 'gray', 'black'];
+
+  let colorPalette = document.getElementById('color-palette');
+  for (let index = 0; index < numberOfColors; index += 1){
+    let divColorPalette = document.createElement('div');
+    colorPalette.appendChild(divColorPalette);
+    divColorPalette.className = arrayColors[index];
+    divColorPalette.classList.add('containerPalette');
+  }
+}
