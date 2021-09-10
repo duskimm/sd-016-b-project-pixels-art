43b02e601e43715c3d7d30b638ae993a4d30f473
Eglany Junior <contas.eglanyjr@gmail.com>
2021-09-10 17:35:34 -0400

    correcoes do lint e do lint:style

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
-window.onload = function () {
-  let numberPixels = 5;
-  const colorPalette = document.querySelector('.color');
-  const linePixel = document.querySelectorAll('.line-pixel');
-  const boardElements = document.getElementById('pixel-board');
-  const pixel = document.querySelector('.pixel');
-  
-  creatBoard (linePixel);
-  
-  function creatBoard(linePixel) {
-    for(let index = 0; index < linePixel.length; index += 1) {
-      createLine(linePixel[index])
-  }
-  }
+const numberPixels = 5;
+const linePixel = document.querySelectorAll('.line-pixel');
+const boardElements = document.getElementById('pixel-board');
 
-  function createLine (line) {
-    for(let index = 1; index <= numberPixels; index += 1){
-      let pixel = createPixel("pixel");
-      line.appendChild(pixel);
-    }
+creatBoard(linePixel);
+
+function creatBoard(linePixel) {
+  for (let index = 0; index < linePixel.length; index += 1) {
+    createLine(linePixel[index]);
   }
-
-  function createPixel(classe) {
-    let pixel = document.createElement('div')
-    pixel.className = 'pixel'
-    return pixel;
-  }
-
-  boardElements.addEventListener('click', presetColor);
-  boardElements.addEventListener('dblclick', clearPixel);
-
-  function clearPixel (event){
-    event.target.style.backgroundColor = 'white';
-  }
-
-  function presetColor(event) {
-    event.target.style.backgroundColor= 'black';
-  }
-
 }
 
+function createLine(line) {
+  for (let index = 1; index <= numberPixels; index += 1) {
+    let pixel = createPixel('pixel');
+    line.appendChild(pixel);
+  }
+}
+
+function createPixel(classe) {
+  let pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  return pixel;
+}
+
+boardElements.addEventListener('click', presetColor);
+boardElements.addEventListener('dblclick', clearPixel);
+
+function clearPixel(event) {
+  event.target.style.backgroundColor = 'white';
+}
+
+function presetColor(event) {
+  event.target.style.backgroundColor = 'black';
+}
