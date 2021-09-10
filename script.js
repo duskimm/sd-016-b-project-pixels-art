09826b42557b8a9c94ab531ee74e925774543613
Gabriel Oliveira <gabrielfrox@gmail.com>
2021-09-10 15:40:46 -0300

    Requisitos 2 e 3 lint OK

---
+++ script.js
@@ -0,0 +1,6 @@
+// Requisito 2 - Cores dos botões
+const colorBtns = document.getElementsByClassName('color');
+colorBtns[0].style.backgroundColor = 'black';
+colorBtns[1].style.backgroundColor = 'red';
+colorBtns[2].style.backgroundColor = 'green';
+colorBtns[3].style.backgroundColor = 'purple';
