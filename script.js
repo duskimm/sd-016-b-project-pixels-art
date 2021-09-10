9a218b37dd65946f4b5ce0e49cba39678f390e1d
Everton-Curti <evercurti@gmail.com>
2021-09-10 15:42:34 -0300

    refatorando arquivo js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function exercicioUm(){
-    let capturaHeader = document.querySelector('header');
-    let criarTitulo = document.createElement('h1');
-    criarTitulo.id = 'title';
-    criarTitulo.innerHTML = 'Paleta de Cores';
-    capturaHeader.appendChild(criarTitulo);
-}
-exercicioUm();
+let capturaHeader = document.querySelector('header');
+let criarTitulo = document.createElement('h1');
+criarTitulo.id = 'title';
+criarTitulo.innerHTML = 'Paleta de Cores';
+capturaHeader.appendChild(criarTitulo);
+
+
+
 
