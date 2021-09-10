604fc41bae8f2205717daf98c1cb2bd94c48d185
Bruno Matumoto <brunomatumoto@MacBook-Air-de-Bruno.local>
2021-09-10 18:02:55 -0300

    Add stage 4 completed to project

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function colorsPallete(colorsHexa) {
   return containerPallete;
 }
 colorsPallete(['#000000', '#D9D9D9', '#F2B138', '#003F63']);
+
+function createPixel() {
+  const boardPixel = document.getElementById('pixel-board');
+  for (let i = 0; i < 25; i += 1) {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    boardPixel.appendChild(pixel);
+  }
+}
+createPixel();
