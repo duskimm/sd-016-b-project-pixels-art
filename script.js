eeb37e0a5a0b5bc99b0e2ca3215013b630a0c874
Fredi Wilkens <frediwilkens@gmail.com>
2021-09-10 18:06:44 -0300

    requisito 4 e 5

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-let blockOfPalette = document.getElementById('color-palette');
-let colorPalette = document.getElementsByClassName('color');
+function createPixelBoard() {
+  let pixelBoard = document.createElement('section');
+  pixelBoard.id = 'pixel-board';
+  document.body.appendChild(pixelBoard);
+  pixelBoard.style.height = '210px';
+  pixelBoard.style.width = '210px';
+}
+createPixelBoard(5);
+
+function createPixel(n) {
+  let pixelBoard = document.getElementById('pixel-board');
+
+  for(let index = 0; index < n; index += 1) {
+    let pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixelBoard.appendChild(pixel);
+  }
+}
+createPixel(25);
