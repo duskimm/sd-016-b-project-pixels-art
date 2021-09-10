454f068a244a725e9a954ac5f4d489e3f8b17059
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2021-09-10 18:52:25 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 window.onload = function() {
-    
+    const colorSelected = document.getElementById("color-palette");
+    const board = document.getElementById("pixel-board");
+    // Colors
+    document.getElementById("black").style.backgroundColor = "black";
+    document.getElementById("red").style.backgroundColor = "red";
+    document.getElementById("green").style.backgroundColor = "green";
+    document.getElementById("yellow").style.backgroundColor = "yellow";
+    // event that swith the class selected
+    colorSelected.addEventListener("click", function(event) {
+        document.querySelector(".selected").classList.remove("selected");
+        event.target.classList.add("selected");
+
+    });
+    // use the background-color selected to paint the pixel in board
+    board.addEventListener("click", function(event) {
+        const colorPaint = document.querySelector(".selected").style.backgroundColor;
+        event.target.style.backgroundColor = colorPaint;
+    });
 };
