92f8578de1fceb54fbef2e11caa85e599e77021c
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-09-10 17:06:13 -0300

    Conclui requisito 6

--- script.js
+++ script.js
@@ -3,27 +3,31 @@ const board = document.getElementById('pixel-board');
 
 function createPaletteAndAddColor() {
   const colors = ['black', 'red', 'blue', 'yellow'];
-  for (let c of colors) {
+  for (const c of colors) {
     const color = document.createElement('div');
     color.classList.add('color');
     palette.appendChild(color);
     color.style.backgroundColor = c;
+    if (color.style.backgroundColor === 'black') {
+      color.classList.add('selected')
+    }
   }
 }
 
 function createPixels(line, column) {
   for (let l = 0; l < line; l += 1) {
-    const line = document.createElement('div');
-    line.classList.add('line');
-    board.appendChild(line);
+    const lines = document.createElement('div');
+    lines.classList.add('line');
+    board.appendChild(lines);
     for (let c = 0; c < column; c += 1) {
       const pixel = document.createElement('div');
       pixel.classList.add('pixel');
-      line.appendChild(pixel);
+      lines.appendChild(pixel);
       pixel.style.backgroundColor = 'white';
     }
   }
 }
 
+
 createPaletteAndAddColor();
-createPixels(5, 5)
+createPixels(5, 5);
