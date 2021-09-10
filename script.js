0828ee48da9664f8b6a2989efec249f6a2e1f600
lucas <araujo2012@hotmail.com>
2021-09-10 15:39:34 -0300

    Requisito 5 completo

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 window.onload = function () {
     createLines();
     createPixels();
+}
 
-    function newChild(type, text, classs, id) {
-        let newChildItem = document.createElement(type);
-        newChildItem.innerText = text;
-        if (classs !== '') {
-            newChildItem.className = classs;
-        }
-        if (id !== '') {
-            newChildItem.id = id;
-        }
-        return newChildItem;
+function newChild(type, text, classs, id) {
+    let newChildItem = document.createElement(type);
+    newChildItem.innerText = text;
+    if (classs !== '') {
+        newChildItem.className = classs;
     }
+    if (id !== '') {
+        newChildItem.id = id;
+    }
+    return newChildItem;
+}
 
-    function createLines() {
-        let board = document.querySelector('#pixel-board');
+function createLines() {
+    let board = document.querySelector('#pixel-board');
+    for (let index = 0; index < 5; index += 1) {
+        board.append(newChild('div', '', 'line', ''));
+    }
+}
+
+function createPixels() {
+    let lines = document.querySelectorAll('.line');
+    for (let line of lines) {
         for (let index = 0; index < 5; index += 1) {
-            board.append(newChild('div', '', 'line', ''));
-        }
-    }
-
-    function createPixels() {
-        let lines = document.querySelectorAll('.line');
-        for (let line of lines) {
-            for (let index = 0; index < 5; index += 1) {
-                line.append(newChild('div', '', 'pixel', ''));
-            }
+            line.append(newChild('div', '', 'pixel', ''));
         }
     }
 }
