311746167c2f84acab6692ac917472c15874f605
igor rafael <igor.rafael.briano@gmail.com>
2021-09-10 17:14:47 -0300

    finalizado item 6

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  selectColor();
+
+  function selectColor () {
+    let firstload = true;
+    if(firstload){
+      let colorSelect = document.querySelector('.color');
+      colorSelect.classList.add('selected');
+    }else {
+
+    }
+  }
+}
