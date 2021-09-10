299cf30b127a2397979c022721b8284ccb7fa344
Gabriel A <gabrielaug4@gmail.com>
2021-09-10 16:55:16 -0300

    req06

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ setColorsToDivs[2].style.backgroundColor = 'blue';
 setColorsToDivs[3].style.backgroundColor = 'green';
 
 for (let index = 0; index < 25; index += 1) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    document.querySelector('#pixel-board').appendChild(pixel);
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  document.querySelector('#pixel-board').appendChild(pixel);
+}
+
+window.onload = function() {
+  setColorsToDivs[0].classList.add('selected');
 }
