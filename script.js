48da931f222c26cf2aead2b51399e5d124e0cbdb
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-09-10 20:56:04 -0300

    Refatorando lint

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-let colorSelected = document.querySelectorAll('#color-palette .color');
-let btnClear = document.getElementById('clear-board');
-btnClear.addEventListener('click', clearAll);
-
+const colorSelected = document.querySelectorAll('#color-palette .color');
+const btnClear = document.getElementById('clear-board');
 const pixelBoard = document.getElementById('pixel-board');
 let color = 'black';
 
+function clearAll() {
+  // Reseta a cor de fundo de todos os pixels
+  for (let index = 0; index < pixelBox.length; index += 1) {
+    pixelBox[index].style.backgroundColor = 'white';
+  }
+  // Reseta a cor selected para black
+  for (let index = 0; index < colorSelected.length; index += 1) {
+    if (colorSelected[index].id !== 'black') {
+      colorSelected[index].classList.remove('selected');
+    } else {
+      colorSelected[index].className = 'color selected';
+    }
+  }
+}
+
+btnClear.addEventListener('click', clearAll);
+
 function createPixelBoxes(size) {
   for (let index = 0; index < size; index += 1) {
-    let box = document.createElement('div');
+    const box = document.createElement('div');
     box.className = 'pixel';
     pixelBoard.append(box);
   }
@@ -30,26 +45,11 @@ for (let index = 0; index < pixelBox.length; index += 1) {
   pixelBox[index].addEventListener('click', paintsElement);
 }
 
-for (let index = 0; index < colorSelected.length; index += 1) {
-  colorSelected[index].addEventListener('click', chosenColor);
-  console.log(color);
-}
-
 function chosenColor(event) {
   color = event.target.id;
 }
 
-function clearAll() {
-  // Reseta a cor de fundo de todos os pixels
-  for (let index = 0; index < pixelBox.length; index += 1) {
-    pixelBox[index].style.backgroundColor = 'white';
-  }
-  // Reseta a cor selected para black
-  for (let index = 0; index < colorSelected.length; index += 1) {
-    if (colorSelected[index].id !== 'black') {
-      colorSelected[index].classList.remove('selected');
-    } else {
-      colorSelected[index].className = 'color selected';
-    }
-  }
+for (let index = 0; index < colorSelected.length; index += 1) {
+  colorSelected[index].addEventListener('click', chosenColor);
+  console.log(color);
 }
