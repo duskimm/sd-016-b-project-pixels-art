dba40f8fdb052b1c1ea38cfc64d5600680447cf3
Felipe Sanches <sanchesmeirelles@gmail.com>
2021-09-10 15:55:20 -0300

    Requisito 7 ok

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 window.onload = function () {
-  let startBlack = document.querySelector('.cblack')
-  function getBlack() {
-    startBlack.classList.add('selected');
+  let getBlack = document.querySelector('.cblack');
+  let getBlue = document.querySelector('.cblue');
+  let getRed = document.querySelector('.cred');
+  let getGreen = document.querySelector('.cgreen');
+
+  function getColorBlack() {
+    getBlack.classList.add('selected');
   }
-  getBlack();
+  getColorBlack();
+
+  let getPalletColors = document.querySelector('#color-palette');
+  
+  function verifySelected (n){
+    let answer = false;
+    for (let i = 0; i < n.length; i += 1){
+      if (n[i] === 'selected'){
+        answer = true;
+      }
+    }
+    return answer;
+  }
+  // Selecionar cores
+  getBlue.addEventListener('click', function (){
+    let getSelect = document.querySelector('.selected');
+    getSelect.classList.remove('selected');      getBlue.classList.add('selected');
+  })
+  getBlack.addEventListener('click', function (){
+    let getSelect = document.querySelector('.selected');
+    getSelect.classList.remove('selected');
+      getBlack.classList.add('selected');
+  })
+  getRed.addEventListener('click', function (){
+    let getSelect = document.querySelector('.selected');
+    getSelect.classList.remove('selected');
+      getRed.classList.add('selected');
+  })
+  getGreen.addEventListener('click', function (){
+      let getSelect = document.querySelector('.selected');
+      getSelect.classList.remove('selected');
+      getGreen.classList.add('selected');
+  })
+
+
 }
