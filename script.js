a8326957fbabd23a64397e0d46c81e2db8cd3527
Gilson Alencar <gilson.alencar94@gmail.com>
2021-09-10 17:51:00 -0300

    Requirement 9

--- script.js
+++ script.js
@@ -13,9 +13,17 @@ function paintPixel(event) {
   pixel.style.backgroundColor = color;
 }
 
+function clearBoard() {
+  const board = document.querySelectorAll('.pixel');
+  for (let i = 0; i < board.length; i += 1) {
+    board[i].style.backgroundColor = 'white';
+  }
+}
+
 window.onload = function main() {
   const paletaDeCores = document.querySelectorAll('.color');
   const pixels = document.querySelectorAll('.pixel');
+  const clearButton = document.querySelector('#clear-board');
 
   for (let i = 0; i < paletaDeCores.length; i += 1) {
     paletaDeCores[i].addEventListener('click', selectColor);
@@ -26,4 +34,6 @@ window.onload = function main() {
   for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].addEventListener('click', paintPixel);
   }
+
+  clearButton.addEventListener('click', clearBoard);
 };
