f431a894cd25d26355093fe258ab22d5d65c6d96
Gilson Alencar <gilson.alencar94@gmail.com>
2021-09-10 17:44:51 -0300

    Requirement 8

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
+function selectColor(event) {
+  const selectedColor = document.querySelector('.selected');
+  selectedColor.classList.remove('selected');
+  event.target.classList.add('selected');
+  // console.log(selectedColor);
+  // console.log(event.target);
+}
+
+function paintPixel(event) {
+  const selected = document.querySelector('.selected');
+  const color = window.getComputedStyle(selected).backgroundColor;
+  const pixel = event.target;
+  pixel.style.backgroundColor = color;
+}
+
 window.onload = function main() {
   const paletaDeCores = document.querySelectorAll('.color');
+  const pixels = document.querySelectorAll('.pixel');
 
-  function selectColor(event) {
-    const selectedColor = document.querySelector('.selected');
-    selectedColor.classList.remove('selected');
-    event.target.classList.add('selected');
-    console.log(selectedColor);
-    console.log(event.target);
-  }
-
-  // percorrer paleta e adicionar eventListener
   for (let i = 0; i < paletaDeCores.length; i += 1) {
     paletaDeCores[i].addEventListener('click', selectColor);
   }
 
-  // se uma cor for clicada, remover a classe selected da cor que a tiver,
-  // e então adicionar a classe selected a essa cor
+  // adicionar evento para que ao clicar em um pixel ele seja preenchido
+  // com a cor selecionada
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', paintPixel);
+  }
 };
