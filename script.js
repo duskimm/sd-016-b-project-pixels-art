7dab150bd866bc722a66139ed2c3befb88d5f997
Douglas Ialamov <douglas.ialamov@gmail.com>
2021-09-10 16:24:21 -0300

    Color Pallet Done with background correction and challenge 2 done

---
+++ script.js
@@ -0,0 +1,7 @@
+function ramdomColors () {
+  let otherColors = Math.floor(Math.random() * 255);
+  let otherColors1 = Math.floor(Math.random() * 255);
+  let otherColors2 = Math.floor(Math.random() * 255);
+  let paletColor = `rgb(${otherColors}, ${otherColors1}, ${otherColors2})`;
+
+  
