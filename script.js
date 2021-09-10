3fdbbc0d5dfab969fa262bd9aef946b9ba565b06
André Melo <adr.smelo@gmail.com>
2021-09-10 17:57:19 -0300

    Desafio 03 Concluido

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const palette = document.getElementById('color-palette');
+const colorPalette = document.getElementById('color-palette');
 
 function createColors() {
   const colors = ['black', 'red', 'blue', 'green'];
   for (let index = 0; index < colors.length; index += 1) {
     const color = document.createElement('div');
     color.className = 'color';
-    palette.appendChild(color);
+    colorPalette.appendChild(color);
     color.style.backgroundColor = colors[index];
   }
 }
