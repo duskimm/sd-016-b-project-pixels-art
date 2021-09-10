1b4a4649a63aa9785966fa01e5585ed895c2b3b6
leandro <leandrochagask@gmail.com>
2021-09-10 20:18:39 -0300

    Requisito 08

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let colorPalette = document.querySelector('#color-palette')
 let color = document.querySelectorAll('.color');
 
+let pixelBoard = document.querySelector('#pixel-board')
+
 // Requisito 7
 colorPalette.addEventListener('click', function (event) {
   for (let i = 0; i < color.length; i += 1) {
@@ -8,7 +10,15 @@ colorPalette.addEventListener('click', function (event) {
   }
 
   if ( event.target.classList.contains( 'color' ) ) {  
-    console.log('hello');
     event.target.classList.add('selected');
+  }
+});
+
+// Requisito 8
+pixelBoard.addEventListener('click', function (event) {
+  if ( event.target.classList.contains( 'pixel' ) ) {
+    let selected = document.querySelector('.selected');
+    let cor = getComputedStyle(selected).backgroundColor;
+    event.target.style.backgroundColor = cor;
   }
 });
