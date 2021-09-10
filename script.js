50aa73bc601846a027a1ca49a7bf56d0caa8f591
Leandro Boralli <leboralli@gmail.com>
2021-09-10 16:38:29 -0300

    criando as linhas

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const arrayCores = ['preto', 'vermelho', 'azul', 'verde'];
+const linhasColunas = 5;
 
 // Criando as divs da paleta
 for (let index = 0; index < arrayCores.length; index += 1) {
   let paleta = document.querySelector(".paleta");
   let cor = arrayCores[index];
   let elemento = document.createElement('div');
-  elemento.className = cor;
-  
+  elemento.className = 'bloco-' + cor;
+  // console.log(elemento);
+  paleta.appendChild(elemento);
+}
+
+for (let linha = 0; linha < linhasColunas; linha += 1) {
+  let quadro = document.querySelector('.quadro');
+  let linhaQuadro = document.createElement('div');
+  linhaQuadro.className = 'linha' + linha;
+  quadro.appendChild(linhaQuadro);
+  for (let coluna = linha; coluna < linhasColunas; coluna += 1) {
+
+  }
 }
