5fe309e196a6e53cbc6e45b6390f51f50f512987
Victor <victor.hugofla@hotmail.com>
2021-09-10 18:32:31 -0300

    Finaliza requisito 9

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-// Adicionar classe selected
+// get the DOM elements
 const colors = document.querySelectorAll('.color');
+const pixels = document.querySelectorAll('.pixel');
+const button = document.getElementById('clear-board');
+
+// add class 'selected'
 for (let index = 0; index < colors.length; index += 1) {
   colors[index].addEventListener('click', addClassName);
-	function addClassName (event) {
-		let selected = event.target;
-		for (let i = 0; i < colors.length; i += 1) {
-			colors[i].classList.remove("selected");
-		}
-		selected.classList.add("selected");
+  function addClassName (event) {
+    let selected = event.target;
+    for (let i = 0; i < colors.length; i += 1) {
+      colors[i].classList.remove("selected");
+    }
+    selected.classList.add("selected");
 	}}
 
-// Pintar cores
-const pixels = document.querySelectorAll('.pixel');
+// painting the pixels
 for (let index = 0; index < pixels.length; index += 1) {
-	pixels[index].addEventListener('click', function (event) {
-		let selected = event.target;
-		let checkClass = document.querySelector('.selected');
-		selected.style.backgroundColor = checkClass.classList[0];
-	})
+  pixels[index].addEventListener('click', function (event) {
+    let selected = event.target;
+    let checkClass = document.querySelector('.selected');
+    selected.style.backgroundColor = checkClass.classList[0];
+  })
 }
+
+// cleaning the frame
+button.addEventListener('click', function(){
+  for (let index = 0; index < pixels.length; index++) {
+    pixels[index].style.backgroundColor = 'white';
+  }
+})
