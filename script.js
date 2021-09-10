0e0bfa4284e0f882436621083f6cf6532b20df54
Leandro Boralli <leboralli@gmail.com>
2021-09-10 17:06:20 -0300

    requisitos 1

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ const linhasColunas = 5;
 
 // Criando as divs da paleta
 for (let index = 0; index < arrayCores.length; index += 1) {
-  let paleta = document.querySelector(".paleta");
+  let paleta = document.querySelector("#color-palette");
   let cor = arrayCores[index];
   let elemento = document.createElement('div');
-  elemento.className = 'bloco-' + cor;
-  // console.log(elemento);
+  elemento.id = 'color-palette';
+  elemento.className = cor;
+  console.log(elemento);
   paleta.appendChild(elemento);
 }
 
-for (let linha = 0; linha < linhasColunas; linha += 1) {
-  let quadro = document.querySelector('.quadro');
-  let linhaQuadro = document.createElement('div');
-  linhaQuadro.className = 'linha' + linha;
-  quadro.appendChild(linhaQuadro);
-  for (let coluna = linha; coluna < linhasColunas; coluna += 1) {
+// for (let linha = 0; linha < linhasColunas; linha += 1) {
+//   let quadro = document.querySelector('.quadro');
+//   let linhaQuadro = document.createElement('div');
+//   linhaQuadro.className = 'linha' + linha;
+//   quadro.appendChild(linhaQuadro);
+//   for (let coluna = linha; coluna < linhasColunas; coluna += 1) {
 
-  }
-}
+//   }
+// }
