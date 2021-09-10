d65d7d987ba182ae30a648c877911b34882fbe99
Gabriel A <gabrielaug4@gmail.com>
2021-09-10 15:43:41 -0300

    req.02

---
+++ script.js
@@ -0,0 +1,5 @@
+const setColorsToDivs = document.getElementsByClassName('color');
+setColorsToDivs[0].style.backgroundColor = 'black';
+setColorsToDivs[1].style.backgroundColor = 'red';
+setColorsToDivs[2].style.backgroundColor = 'blue';
+setColorsToDivs[3].style.backgroundColor = 'green';
