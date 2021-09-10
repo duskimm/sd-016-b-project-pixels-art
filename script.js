1994e0cf2f284ea9d0a32e6527c67f1213ad752d
Hiago Briano <hiago.artist@hotmail.com>
2021-09-10 17:18:55 -0300

    requisito 7 feito

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
 window.onload = function () {
 let colorBlack = document.querySelector('.corBlack');
 colorBlack.classList.add('selected');
+
+let colorBlue = document.querySelector('.corBlue');
+let colorGreen = document.querySelector('.corGreen');
+let colorYellow = document.querySelector('.corYellow');
+
+colorBlue.addEventListener('click', selecaoBlue);
+
+function selecaoBlue(){
+  colorBlue.classList.add('selected');
+  colorBlack.classList.remove("selected");
+  colorGreen.classList.remove("selected");
+  colorYellow.classList.remove("selected");
+} 
+
+colorGreen.addEventListener('click', selecaoGreen);
+
+function selecaoGreen(){
+  colorGreen.classList.add('selected');
+  colorBlack.classList.remove("selected");
+  colorBlue.classList.remove("selected");
+  colorYellow.classList.remove("selected");
+} 
+
+colorYellow.addEventListener('click', selecaoYellow);
+
+function selecaoYellow(){
+  colorYellow.classList.add('selected');
+  colorBlack.classList.remove("selected");
+  colorBlue.classList.remove("selected");
+  colorGreen.classList.remove("selected");
+} 
+
+colorBlack.addEventListener('click', selecaoBlack);
+
+function selecaoBlack(){
+  colorBlack.classList.add('selected');
+  colorYellow.classList.remove("selected");
+  colorBlue.classList.remove("selected");
+  colorGreen.classList.remove("selected");
+} 
 }
