dbfbd0c2c1cf19a703338a0bf1e03e569f1471d1
Bruno Alves Fay <brunofay1@hotmail.com>
2021-09-10 19:36:56 -0300

    até o 5

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ h1Title.innerHTML = params;
 }
 title("Paleta de Cores");
 /* numero 2 */
+let dadDivBody = document.querySelector ("body")
 function creatPaletColorsDiv(params){
-    let dadDivPaletColors = document.querySelector ("body")
     let divPaletColors = document.createElement ("div");
     divPaletColors.id = "color-palette"
-    dadDivPaletColors.appendChild(divPaletColors)
+    dadDivBody.appendChild(divPaletColors)
     for (let index = 0; index <= params; index+=1) { 
         let dadDivColors = document.querySelector("#color-palette")
         let divColors = document.createElement ("div");
@@ -31,6 +31,7 @@ function divBackGroundColor() {
    /*  peguei a referencia do codigo de cores aleatorias nesse video https://youtu.be/9nt9HRdPSpo */
     let catchDivColor = document.querySelectorAll(".color")
     for (let index = 1; index < catchDivColor.length; index += 1) {
+        
         let r =Math.floor(Math.random()*255)
         let g =Math.floor(Math.random()*255)
         let b =Math.floor(Math.random()*255)
@@ -38,3 +39,27 @@ function divBackGroundColor() {
     }    
 }
 divBackGroundColor()
+
+/* numero 4 */
+
+function pixelSquare(pixels){
+ let divSquare = document.createElement("div")
+ divSquare.id = "pixel-board" ;
+ dadDivBody.appendChild(divSquare);
+ let divColuns;
+    for (let index = 0; index < pixels; index += 1) {
+        let divPixel = document.createElement("div")
+        /* divPixel.classList = "pixel" */
+        divSquare.appendChild(divPixel)
+
+    for (let indexColun = 0; indexColun < pixels; indexColun += 1) {
+        divColuns = document.createElement("div")
+        divColuns.classList = "pixel"
+        divPixel.appendChild(divColuns)
+        
+    }
+}
+} 
+pixelSquare(5)
+
+/* remover a classe dos pais  */
