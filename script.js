6d7ac9ae0361f96bed23a7eb5c7b19d21e5dfd17
AirtonL <junynnhoh@gmail.com>
2021-09-10 18:43:23 -0300

    adicionando requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function criaHeader() {
-  let titulo = document.querySelector('header');
-  let criaH1 = document.createElement('h1');
+  const titulo = document.querySelector('header');
+  const criaH1 = document.createElement('h1');
   criaH1.innerText = 'Paleta de Cores';
   criaH1.id = 'title';
   titulo.appendChild(criaH1);
@@ -8,25 +8,65 @@ function criaHeader() {
 criaHeader();
 
 function criaCores() {
-  let cores = ['black', 'red', 'pink', 'green'];
-  let quadrados = document.querySelectorAll('.color');
-  for (let index in cores) {
+  const cores = ['black', 'red', 'pink', 'green'];
+  const quadrados = document.querySelectorAll('.color');
+  for (let index = 0; index < cores.length; index += 1) {
     quadrados[index].style.backgroundColor = cores[index];
   }
 }
 criaCores();
 
 function criaQuadro() {
-  let localizaDiv = document.querySelector('#pixel-board');
+  const localizaDiv = document.querySelector('#pixel-board');
   for (let index = 0; index < 5; index += 1) {
-    let criaUl = document.createElement('ul');
+    const criaUl = document.createElement('ul');
     criaUl.className = 'uls';
     localizaDiv.appendChild(criaUl);
-    for(index2 = 0; index2 < 5; index2 += 1){
-      localizaUl = document.querySelectorAll('.uls')[index];
-      let criaLi = document.createElement('li');
+    for (let index2 = 0; index2 < 5; index2 += 1) {
+      const localizaUl = document.querySelectorAll('.uls')[index];
+      const criaLi = document.createElement('li');
       criaLi.className = 'pixel';
       localizaUl.appendChild(criaLi);
     }
   }
-} criaQuadro();
+}
+criaQuadro();
+
+function trocaClass(event) {
+  const pegaClass = document.querySelector('.selected');
+  pegaClass.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+const black = document.getElementById('black');
+const red = document.getElementById('red');
+const pink = document.getElementById('pink');
+const green = document.getElementById('green');
+
+black.addEventListener('click', trocaClass);
+red.addEventListener('click', trocaClass);
+pink.addEventListener('click', trocaClass);
+green.addEventListener('click', trocaClass);
+
+function trocaCor() {
+  const pixel = document.getElementById('pixel-board');
+  pixel.addEventListener('click', (event) => {
+    if (event.target.className === 'pixel') {
+      const corAtual = document.querySelector('.selected').id;
+      const corSelecionada = event.target;
+      corSelecionada.style.backgroundColor = corAtual;
+    }
+  });
+}
+trocaCor();
+
+function limpaQuadro() {
+  const pegaButton = document.getElementById('clear-board');
+  const pegaPixels = document.querySelectorAll('.pixel');
+  pegaButton.addEventListener('click', function () {
+    for (let index = 0; index < pegaPixels.length; index += 1) {
+      pegaPixels[index].style.backgroundColor = 'white';
+    }
+  });
+}
+limpaQuadro();
