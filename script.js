50e9fdd97eb1236ffec14651391166a264af4b5e
lucas <araujo2012@hotmail.com>
2021-09-10 18:02:24 -0300

    Adiciona comentarios para explicar brevemente as funcoes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Funcao para criar elementos fora do dom, recebe como parametros respectivamente o tipo do elemento, o texto, as classes, e o id, se passar '' como parametro ele nao adiciona o parametro em questao.
 function newChild(type, text, classs, id) {
   const newChildItem = document.createElement(type);
   newChildItem.innerText = text;
@@ -10,6 +11,7 @@ function newChild(type, text, classs, id) {
   return newChildItem;
 }
 
+// funcao para criar linhas onde serao colocados os pixels
 function createLines() {
   const board = document.querySelector('#pixel-board');
   for (let index = 0; index < 5; index += 1) {
@@ -17,6 +19,7 @@ function createLines() {
   }
 }
 
+// funcao para colocar pixels a serem pintados nas linhas
 function createPixels() {
   const lines = document.querySelectorAll('.line');
   for (let indexLines = 0; indexLines < lines.length; indexLines += 1) {
@@ -26,6 +29,7 @@ function createPixels() {
   }
 }
 
+// funcao para mudar a classe do elemento da palheta que voce clicar para ter selected, e remove do antigo que a possuia
 function changeSelectedPosition(event) {
   if (event.target.classList.contains('color')) {
     const selectedColor = document.querySelector('.selected');
@@ -34,6 +38,7 @@ function changeSelectedPosition(event) {
   }
 }
 
+// funcao para pintar os pixels com a cor do elemento da palheta de cores que possui a classe selected
 function paintPixels(event) {
   if (event.target.classList.contains('pixel')) {
     const selectedColor = document.querySelector('.selected');
@@ -44,16 +49,19 @@ function paintPixels(event) {
 }
 
 /* Metodo event bubbling adquirido atraves deste link https://gomakethings.com/attaching-multiple-elements-to-a-single-event-listener-in-vanilla-js/ */
+// evento que acontece quando clickamos em qualquer lugar da pagina
 document.addEventListener('click', (event) => {
   changeSelectedPosition(event);
   paintPixels(event);
 }, false);
 
+// evento que acontece quando a pagina termina de carregar
 window.onload = () => {
   createLines();
   createPixels();
 };
 
+// evento que acontece quando clicamos no botao limpar
 document.querySelector('#clear-board').addEventListener('click', () => {
   const pixels = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixels.length; index += 1) {
