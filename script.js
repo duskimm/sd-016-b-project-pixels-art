42a5908995727a9d1ab4c3564d5c44f237f69c3f
Erik Duarte Rodrigues <erikduarterodrigues@gmail.com>
2021-09-10 19:33:46 -0300

    Resolvendo LINT

--- script.js
+++ script.js
@@ -2,24 +2,23 @@ const color = document.querySelectorAll('.color');
 const pixel = document.querySelectorAll('.pixel');
 
 function selectedColor(event) {
-    for (let i = 0; i < color.length; i += 1) {
-      color[i].classList.remove('selected');
-    }
-    event.target.classList.add('selected');
-  }
-
   for (let i = 0; i < color.length; i += 1) {
-    color[i].addEventListener('click', selectedColor);
+    color[i].classList.remove('selected');
   }
+  event.target.classList.add('selected');
+}
 
-  function paint(event) {
-    const coloredpixel = event.target;
-    const selected = document.querySelector('.selected');
-    const compStyle = window.getComputedStyle(selected);
-    coloredpixel.style.backgroundColor = compStyle.backgroundColor;
-  }
+for (let i = 0; i < color.length; i += 1) {
+  color[i].addEventListener('click', selectedColor);
+}
 
-  for (let i = 0; i < pixel.length; i += 1) {
-    pixel[i].addEventListener('click', paint);
-  }
+function paint(event) {
+  const coloredpixel = event.target;
+  const selected = document.querySelector('.selected');
+  const compStyle = window.getComputedStyle(selected);
+  coloredpixel.style.backgroundColor = compStyle.backgroundColor;
+}
 
+for (let i = 0; i < pixel.length; i += 1) {
+  pixel[i].addEventListener('click', paint);
+}
