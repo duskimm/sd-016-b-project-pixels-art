c75286401f1c46e632b02e970c4b88e5e5f693ec
Victor Shin <victor.kamiguchi@gmail.com>
2021-09-10 18:00:04 -0300

    C6 Requisito 8 - Pintar quadro de pixels

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-// function addClassSelected(event) {
-//   event.target.classList.add('selected');
-// }
-
 function resetColor() {
   const blackColor = document.querySelector('.black');
   blackColor.classList.add('selected');
 }
 
-window.onload = resetColor;
-
-function test1() {
-  const classSelected = document.querySelector('.selected');
-  classSelected.classList.remove('selected');
-}
-
 function addClassSelected(event) {
   const classSelected = document.querySelector('.selected');
   const eventTarget = event.target;
-  if (eventTarget.classList.contains('selected') === false) {
+  if (
+    eventTarget.classList.contains('selected') === false &&
+    eventTarget.classList.contains('color') === true
+  ) {
     classSelected.classList.remove('selected');
     event.target.classList.add('selected');
   }
 }
 
+function paintPix(event) {
+  const classSelected = document.querySelector('.selected');
+  const eventTarget = event.target;
+  const bgColor = window
+    .getComputedStyle(classSelected, null)
+    .getPropertyValue('background-color');
+  eventTarget.style.backgroundColor = bgColor;
+}
+
+// constantes
 const btnRed = document.querySelector('.color__content');
+const pixelBo = document.querySelector('#pixel-board');
+
+window.onload = resetColor;
+
 btnRed.addEventListener('click', addClassSelected);
+pixelBo.addEventListener('click', paintPix);
