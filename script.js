9d4bc81b784d17deba35535a33f93869bbe8241d
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-09-10 16:50:05 -0300

    Conclui requisito 4 e 5 e corrige o 2

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const palette = document.getElementById('color-palette');
+const board = document.getElementById('pixel-board');
 
 function createPaletteAndAddColor() {
-  const colors = ['black', 'red', 'blue', 'yellow', 'gray', 'brown', 'aqua', 'blueviolet', 'green'];
-  for (let i = 0; i < 9; i += 1) {
+  const colors = ['black', 'red', 'blue', 'yellow'];
+  for (let c of colors) {
     const color = document.createElement('div');
     color.classList.add('color');
     palette.appendChild(color);
-    color.style.backgroundColor = colors[i];
+    color.style.backgroundColor = c;
+  }
+}
+
+function createPixels(line, column) {
+  for (let l = 0; l < line; l += 1) {
+    const line = document.createElement('div');
+    line.classList.add('line');
+    board.appendChild(line);
+    for (let c = 0; c < column; c += 1) {
+      const pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      line.appendChild(pixel);
+      pixel.style.backgroundColor = 'white';
+    }
   }
 }
 
 createPaletteAndAddColor();
+createPixels(5, 5)
