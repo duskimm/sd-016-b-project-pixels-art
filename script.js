22f9790b9cefe54ad4f7954ee4c81c2c0065ca85
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-09-10 21:43:21 -0300

    Requisitos 08 e 09 parcialmente completos; Verificar a necessidade de duplo-clique para modificar a cor e configurar o botão de apagar

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const colors = document.querySelectorAll('.color');
-let corSelecionada = document.querySelector('.selected');
-let corAtual = window.getComputedStyle(corSelecionada, null).getPropertyValue("background-color");
+const pixels = document.querySelectorAll('.pixel');
+const corSelecionada = document.querySelector('.selected');
+let corAtual = window.getComputedStyle(corSelecionada, null).getPropertyValue('background-color');
 console.log(corAtual);
+
+// Requisito 7 - Troca qual cor da paleta possui a classe 'selected'
+
+function trocaSelected(event) {
+  const comSelected = document.querySelector('.selected');
+  comSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+  corAtual = window.getComputedStyle(comSelected, null).getPropertyValue('background-color');
+  return corAtual;
+}
 
 for (let index = 0; index < colors.length; index += 1) {
   colors[index].addEventListener('click', trocaSelected);
 }
 
-function trocaSelected(event) {
-  let comSelected = document.querySelector('.selected');
-  comSelected.classList.remove('selected');
-  event.target.classList.add('selected');
+// Pintar os pixels
+
+function trocaCor(event) {
+  event.target.style.backgroundColor = corAtual;
 }
+
+for (let i = 0; i < pixels.length; i += 1) {
+  pixels[i].addEventListener('click', trocaCor);
+}
+
+// Adicionar botão que limpa os pixels
+const recebeBotao = document.getElementsByClassName('buttons')[0];
+const botaoApaga = document.createElement('button');
+botaoApaga.innerText = 'Limpar';
+botaoApaga.setAttribute('id', 'clear-board');
+recebeBotao.appendChild(botaoApaga);
+
+// Limpar os pixels
