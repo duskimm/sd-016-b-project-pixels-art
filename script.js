4092353479de9d309e4e6913aa4a5372de75adfb
Fredi Wilkens <frediwilkens@gmail.com>
2021-09-11 20:44:42 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 function createPixelBoard(numberOfPixelsPerLine) {
   const pixelBoard = document.createElement('section');
   const pixelSize = 42;
-  let height = pixelSize * numberOfPixelsPerLine;
-  let width = pixelSize * numberOfPixelsPerLine;
+  const height = pixelSize * numberOfPixelsPerLine;
+  const width = pixelSize * numberOfPixelsPerLine;
   pixelBoard.id = 'pixel-board';
   document.body.appendChild(pixelBoard);
-  pixelBoard.style.height = height + 'px';
-  pixelBoard.style.width =  width + 'px';
+  pixelBoard.style.height = `${height}px`;
+  pixelBoard.style.width = `${width}px`;
 }
 createPixelBoard(5);
 
 function createPixel(n) {
   const pixelBoard = document.getElementById('pixel-board');
 
-  for(let index = 0; index < n; index += 1) {
-    let pixel = document.createElement('div');
+  for (let index = 0; index < n; index += 1) {
+    const pixel = document.createElement('div');
     pixel.className = 'pixel';
     pixelBoard.appendChild(pixel);
   }
 }
 createPixel(25);
 
-window.onload = function() {
-  let blockSelected = document.querySelector('.black');
-  blockSelected.classList.add('selected');
+function setSelectedColor() {
+  const color = document.querySelector('.selected');
+  const background = window.getComputedStyle(color, null).getPropertyValue('background-color');
+  return background;
 }
 
+window.onload = () => {
+  const blockSelected = document.querySelector('.black');
+  blockSelected.classList.add('selected');
+  setSelectedColor();
+};
+
 function checkSelectedColor() {
-  let paletteOfColors = document.getElementsByClassName('color');
+  const paletteOfColors = document.getElementsByClassName('color');
   for (let index = 0; index < paletteOfColors.length; index += 1) {
     if (paletteOfColors[index].classList.contains('selected') === true) {
       paletteOfColors[index].classList.toggle('selected');
@@ -36,12 +43,40 @@ function checkSelectedColor() {
 }
 
 function chooseColor() {
-  let paletteOfColors = document.getElementsByClassName('color');
+  const paletteOfColors = document.getElementsByClassName('color');
   for (let index = 0; index < paletteOfColors.length; index += 1) {
     paletteOfColors[index].addEventListener('click', () => {
       checkSelectedColor();
       paletteOfColors[index].classList.toggle('selected');
-    })
+      setSelectedColor();
+    });
   }
 }
-chooseColor()
+chooseColor();
+
+function checkSelectedPixel() {
+  const pixels = document.getElementsByClassName('pixel');
+  const classs = 'pixel-selected';
+  for (let index = 0; index < pixels.length; index += 1) {
+    if (pixels[index].classList.contains(classs) === true) {
+      pixels[index].classList.toggle(classs);
+    }
+  }
+}
+
+function addColor(pixel) {
+  const pixels = pixel;
+  pixels.style.backgroundColor = setSelectedColor();
+}
+
+function choosePixel() {
+  const pixels = document.getElementsByClassName('pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', () => {
+      checkSelectedPixel();
+      addColor(pixels[index]);
+      pixels[index].classList.toggle('pixel-selected');
+    });
+  }
+}
+choosePixel();
