95d9230c90482334d6f48f7a4a0267dfb640fae6
Nathan Nunes <nunesxd7@hotmail.com>
2021-09-11 19:03:45 -0300

    ate o 4 com lint e funcional

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-window.onload = function() {
-  let baseGrid = 5;
-  const container = document.getElementById('pixel-board')
-  
+window.onload = function start() {
   function grid() {
+    const baseGrid = 5;
+    const container = document.getElementById('pixel-board');
     for (let i = 0; i < baseGrid; i += 1) {
-      const pixel = document.createElement('div')
-      pixel.className = 'pixel'
+      const rowDiv = document.createElement('div');
       for (let index = 0; index < baseGrid; index += 1) {
-        const columnGrid = document.createElement('div')
-        columnGrid.className = 'pixel'
-        pixel.appendChild(columnGrid)
+        const columnGrid = document.createElement('div');
+        columnGrid.className = 'pixel';
+        rowDiv.appendChild(columnGrid);
+        container.appendChild(rowDiv);
       }
-      container.appendChild(pixel)
     }
   }
-grid()
-}
+  grid();
+};
