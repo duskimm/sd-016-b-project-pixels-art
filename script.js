707cd8e112d2617b3a8c0dba2f34a6168baa0465
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-09-11 00:27:42 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const colorSelected = document.querySelectorAll('#color-palette .color');
 const btnClear = document.getElementById('clear-board');
 const pixelBoard = document.getElementById('pixel-board');
+// Seletores de cor
+const fisrtColor = document.getElementById('firstColor');
+const secondColor = document.getElementById('secondColor');
+const thirdColor = document.getElementById('thirdColor');
+const fourtColor = document.getElementById('fourtColor');
+
 let color = 'black';
 btnClear.addEventListener('click', clearAll);
+
+function colorRgb() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  return `rgba(${red}, ${green}, ${blue}, 1)`;
+}
+console.log(colorRgb());
+
+fisrtColor.style.background = 'black';
+secondColor.style.backgroundColor = colorRgb();
+thirdColor.style.backgroundColor = colorRgb();
+fourtColor.style.backgroundColor = colorRgb();
 
 function createPixelBoxes(size) {
   for (let index = 0; index < size; index += 1) {
@@ -55,9 +74,9 @@ for (let index = 0; index < pixelBox.length; index += 1) {
  * Seta a cor escolhida e controla com a classe selected
  */
 function chosenColor(event) {
-  color = event.target.id;
+  color = event.target.style.backgroundColor;
   for (let index = 0; index < colorSelected.length; index += 1) {
-    if (color === colorSelected[index].id) {
+    if (color === colorSelected[index].style.backgroundColor) {
       colorSelected[index].className = 'color selected';
     } else {
       colorSelected[index].className = 'color';
