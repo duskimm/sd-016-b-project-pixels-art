292b1b8aff0cfa3924278c78d62bd9f1ebe6b302
Renata Domingues <blackbirdfly.r@gmail.com>
2021-09-11 12:43:26 -0300

    Requisito 6inicio

---
+++ script.js
@@ -0,0 +1,5 @@
+function addClassSelected() {
+  const blackInitial = document.getElementById('color-palette').firstElementChild; // recupera onde quero trabalhar //
+  blackInitial.classList.add('selected');
+}
+addClassSelected();
