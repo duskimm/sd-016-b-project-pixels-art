355832b3c5fc6b2f8ff00fdf63348c7af6c7341e
blmarquess <blmarques.dev@gmail.com>
2021-09-11 18:10:07 -0300

    function for macke pixel grid done

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const gameBoardSize = 5;
+const gBSize = 5;
 
-function makeBox() {
-  let box = document.createElement('div');
-  box.className = pixel;
-  return box;
-}
-function makeGameBoard(size) {
-  for (let board in size) {
+const gridlocal = document.querySelector('.game-board');
 
+function makeGrid() {
+  for (let line = 0; line < gBSize; line += 1) {
+    const gridLine = document.createElement('ul');
+    gridLine.className = 'pixel-line';
+    gridlocal.appendChild(gridLine);
+    for (let bloc = 0; bloc < gBSize; bloc += 1) {
+      const box = document.createElement('li');
+      box.className = 'pixel';
+      gridLine.appendChild(box);
+    }
+    const lin = document.createElement('br');
+    gridLine.appendChild(lin);
   }
 }
+
+makeGrid();
