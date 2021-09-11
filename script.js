28f96050a01a1132b658300735841f0fab46fc79
Mariana Saraiva <marianascm8@gmail.com>
2021-09-11 15:13:33 -0300

    Requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,57 +1,47 @@
 const palleteColor = document.getElementById('color-palette');
-const boardPixel = document.getElementById('pixel-board')
+
 
 // criar div para armazenar as cores do projeto, com classe "color" e ainda colocar a cor de forma dinâmica.
 function createColorsPallete(color) {
-  let createElement = document.createElement('span');
-  createElement.className = 'color';
-  createElement.style.backgroundColor = color;
-  palleteColor.appendChild(createElement);
+  let element = document.createElement('span');
+  element.classList = 'color';
+  element.style.backgroundColor = color;
+  palleteColor.appendChild(element);
 };
 createColorsPallete('black');
 createColorsPallete('red');
 createColorsPallete('orange');
 createColorsPallete('yellow');
-createColorsPallete('green');
-createColorsPallete('blue');
-createColorsPallete('purple');
-createColorsPallete('pink');
+// createColorsPallete('green');
+// createColorsPallete('blue');
+// createColorsPallete('purple');
+// createColorsPallete('pink');
+
+// selecionando a cor preta com a classe selected
 
 // criar quadro Pixel com 5 elementos de largura e 5 elementos de altura. Fazer a lógica do quadrado de asteriscos.
-function createboardPixel(number) {
-  // for para criar as linhas do Quadro Pixel.
-  for (let index = 1; index <= number; index += 1){
-    let createElement = document.createElement('div');
-    createElement.className = 'pixel';
-    createElement.style.backgroundColor = 'white';
-    boardPixel.appendChild(createElement);
-  }
-  // for para criar as colunas do Quadro Pixel.
-  // for (let index = 0; index < number, index += 1){
-  //   let createElement = document.createElement('div');
-  //   createElement.className = 'pixel';
-  //   createElement.style.backgroundColor = 'white';
-  //   boardPixel.appendChild(createElement);
-  // }
+// Auxílio Airton Lopes para implementar a div line
 
+function createboardPixel(number) {
+  const boardPixel = document.querySelector('#pixel-board');
+  // for para criar as linhas do Quadro Pixel.
+  for (let index = 0; index < number; index += 1) {
+    let div = document.createElement('div');
+    boardPixel.appendChild(div);
+    div.className = 'line';
+    // for para criar as divs dentro da anterior.
+    for (let index1 = 0; index1 < number; index1 += 1) {
+      let divPai = document.querySelectorAll('.line')[index];
+      let element = document.createElement('div');
+      element.className = 'pixel';
+      element.style.backgroundColor = 'white';
+      divPai.appendChild(element);
+    }
+  }
 }
 createboardPixel(5);
 
-// function criaQuadrado (number){
-//   let n = number;
-//   let simbolo = "*";
-//   let linhaDeAsteriscos = "";
-  
-//   for (let index = 0; index < n; index += 1) {
-//     linhaDeAsteriscos = linhaDeAsteriscos + simbolo;  
-//   }
-  
-//   for (let index = 0; index < n; index += 1){
-//     console.log(linhaDeAsteriscos);
-//   }
 
-// }
-// criaQuadrado(5);
 
 
 
