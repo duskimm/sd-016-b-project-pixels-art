d70bd7f934061fc8debc864c9f079254f69d949f
Matheus <matheusmoura2110@gmail.com>
2021-09-11 19:16:07 -0300

    seleciona adiciona cor selecionada

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ window.onload = function () {
   firstColor.classList.add("selected");
   firstColor.style.background = "black";
 }
+
+const black = document.getElementById("bk");
+const red = document.getElementById("g");
+const blue = document.getElementById("b");
+const green = document.getElementById("r");
+
+function changeClass(event) {
+  const getClass = document.querySelector(".selected");
+  getClass.classList.remove("selected");
+  event.target.classList.add("selected");
+}
+black.addEventListener("click", changeClass);
+red.addEventListener("click", changeClass);
+blue.addEventListener("click", changeClass);
+green.addEventListener("click", changeClass);
+
+
+
