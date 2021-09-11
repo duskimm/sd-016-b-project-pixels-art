8d93a65f93ac958aaef7e889b3c8dcb2874dd6ca
Danielle Silva <danielle.luisasilva@gmail.com>
2021-09-11 16:06:07 -0300

    adiciona requisito 7

--- script.js
+++ script.js
@@ -13,10 +13,29 @@
 // PaletaCore(5);
 
 
-// quando a pagina carregar a cor preta deve estar selecionada
-// cor preta deve ter a classe Selected
-
-window.addEventListener('load', function(){
+// Exercicio 6
+window.addEventListener('load', function() {
     let corPreta = document.querySelector('#black');
     corPreta.classList.add('selected');
-})
+});
+
+// Exercicio 7
+
+// a classe Selected deve ser add a cor selecionada
+// somente uma pode ter por vez (eu acho que passa por for e if)
+let idBlack = document.querySelector('#black');
+let idRed = document.querySelector('#red');
+let idGreen = document.querySelector('#green');
+let idBlue = document.querySelector('#blue');
+
+function mudaClasseSelected (event) {
+    let classSelected = document.querySelector('.selected');
+
+    classSelected.classList.remove('selected');
+    event.target.classList.add('selected');
+
+}
+idBlack.addEventListener('click', mudaClasseSelected);
+idRed.addEventListener('click', mudaClasseSelected);
+idGreen.addEventListener('click', mudaClasseSelected);
+idBlue.addEventListener('click', mudaClasseSelected);
