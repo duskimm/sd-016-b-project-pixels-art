0689dda1e57bf46abf2e2867df01a79d0b51ae14
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-09-10 21:51:05 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,22 +2,6 @@ const colorSelected = document.querySelectorAll('#color-palette .color');
 const btnClear = document.getElementById('clear-board');
 const pixelBoard = document.getElementById('pixel-board');
 let color = 'black';
-
-function clearAll() {
-  // Reseta a cor de fundo de todos os pixels
-  for (let index = 0; index < pixelBox.length; index += 1) {
-    pixelBox[index].style.backgroundColor = 'white';
-  }
-  // Reseta a cor selected para black
-  for (let index = 0; index < colorSelected.length; index += 1) {
-    if (colorSelected[index].id !== 'black') {
-      colorSelected[index].classList.remove('selected');
-    } else {
-      colorSelected[index].className = 'color selected';
-    }
-  }
-}
-
 btnClear.addEventListener('click', clearAll);
 
 function createPixelBoxes(size) {
@@ -33,20 +17,52 @@ const pixelBox = document.querySelectorAll('.pixel');
 // console.log(pixelBox);
 
 /**
+ *Reseta os pixels
+ */
+function clearAll() {
+  // Reseta a cor de fundo de todos os pixels
+  for (let index = 0; index < pixelBox.length; index += 1) {
+    pixelBox[index].style.backgroundColor = 'white';
+  }
+  // Reseta a cor selected para black
+  for (let index = 0; index < colorSelected.length; index += 1) {
+    if (colorSelected[index].id !== 'black') {
+      colorSelected[index].classList.remove('selected');
+    } else {
+      colorSelected[index].className = 'color selected';
+    }
+  }
+}
+
+/**
  * Função que pinta os pixels
  */
 function paintsElement(event) {
   if (event.target.className === 'pixel') {
-    event.target.style.backgroundColor = color;
+    const colorToPaint = event.target;
+    colorToPaint.style.backgroundColor = color;
   }
 }
 
+/**
+ *Adiciona eventos a todos os pixels
+ */
 for (let index = 0; index < pixelBox.length; index += 1) {
   pixelBox[index].addEventListener('click', paintsElement);
 }
 
+/**
+ * Seta a cor escolhida e controla com a classe selected
+ */
 function chosenColor(event) {
   color = event.target.id;
+  for (let index = 0; index < colorSelected.length; index += 1) {
+    if (color === colorSelected[index].id) {
+      colorSelected[index].className = 'color selected';
+    } else {
+      colorSelected[index].className = 'color';
+    }
+  }
 }
 
 for (let index = 0; index < colorSelected.length; index += 1) {
