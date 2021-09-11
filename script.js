794bf5ebd563c96fb57987aa25aaa72c10fc49d5
HugoDaniel2000 <hugodanielc67@gmail.com>
2021-09-11 00:06:47 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // Requisito2
 const palette = document.getElementById('color-palette');
 function createPalleteOfColor(colors) {
+  const arrayId = ['black', 'blue', 'red', 'green'];
   for (let index = 0; index < colors.length; index += 1) {
     const addColor = colors[index];
     const divColor = document.createElement('div');
     divColor.className = 'color';
+    divColor.id = arrayId[index];
     palette.appendChild(divColor);
     divColor.style.backgroundColor = addColor;
   }
 }
 createPalleteOfColor(['black', 'blue', 'red', 'green']);
 
+const divBoardP = document.getElementById('pixel-board');
 function createDiv() {
-  const divS = document.getElementById('pixel-board');
   for (let index = 0; index < 25; index += 1) {
     const creatDiv = document.createElement('div');
     creatDiv.className = 'pixel';
-    divS.appendChild(creatDiv);
+    divBoardP.appendChild(creatDiv);
   }
 }
 createDiv();
 
-window.onload = function () {
+window.onload = function carregar() {
   const color = document.querySelector('.color');
   color.classList.add('selected');
 };
@@ -31,4 +33,10 @@ palette.addEventListener('click', (event) => {
     palette.children[index].classList.remove('selected');
   }
   event.target.classList.add('selected');
+});
+
+divBoardP.addEventListener('click', (event) => {
+  if (event.target.classList.contains('pixel')) {
+    event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+  }
 });
