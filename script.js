e63707d1f21661bc1d32f8e2c02fd095669a2a33
Luis Gustavo <l.gustavomelo@hotmail.com>
2021-09-11 16:59:44 -0300

    Requisito 7 feito

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function quadroPixel(number) {
 }
 quadroPixel(25)
 
-
+//  6 Requisito
 window.onload = function carregaPag(){
   
   let coresDiv = document.querySelectorAll('.color');
   coresDiv[0].classList.add('selected');
-
+}
   // for (let index = 1; index < coresDiv.length; index += 1) {
   //   if (coresDiv[index].className == "selected") {
   //     coresDiv[index].classList.remove("selected");
@@ -22,4 +22,16 @@ window.onload = function carregaPag(){
   //   } 
   // }
 
-}
+  const colors = document.getElementsByClassName('color');
+
+  function pegaCor(event) {
+    for(let index = 0; index < colors.length; index += 1){
+      colors[index].classList.remove('selected')
+    }
+    event.target.classList.add('selected')
+  }
+
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].addEventListener('click', pegaCor);
+  }
+  
