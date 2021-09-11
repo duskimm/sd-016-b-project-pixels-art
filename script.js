0745571ec03c43308f4be78bd225d4c7166525b1
Danilo-Couto <couto.danilo95@gmail.com>
2021-09-11 16:07:56 -0300

    requisito 6

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function inicialColor() {
+  const selectedColor = document.createElement('class');
+  selectedColor.className = 'selected';
+  const blackColor = document.querySelector('.color');
+  blackColor.classList.add("selected");
+};
