7a27e53ca2dec187dba3a75e9afec629deb773af
Rafael Catarino <rafaelscatarino@hotmail.com>
2021-09-11 18:01:54 -0300

    adiciona requisito 6

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function startBlack() {
+  const createClass = document.createElement('class');
+  createClass.className = 'selected';
+  const colorBlack = document.querySelector('.color');
+  colorBlack.classList.add('selected');
+};
