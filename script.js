5f34851f511bd3a6cc184320386c44d3e6cd202e
Rafael Catarino <rafaelscatarino@hotmail.com>
2021-09-11 19:00:42 -0300

    adiciona requisito 6

--- script.js
+++ script.js
@@ -4,3 +4,40 @@ window.onload = function startBlack() {
   const colorBlack = document.querySelector('.color');
   colorBlack.classList.add('selected');
 };
+
+let colorBlack = document.querySelector('.colorBlack');
+let colorBlue = document.querySelector('.colorBlue');
+let colorRed = document.querySelector('.colorRed');
+let colorGreen = document.querySelector('.colorGreen');
+
+colorBlack.addEventListener('click', clickBlack);
+function clickBlack() {
+  colorBlack.classList.add('selected');
+  colorBlue.classList.remove('selected');
+  colorRed.classList.remove('selected');
+  colorGreen.classList.remove('selected');
+}
+
+colorBlue.addEventListener('click', clickBlue);
+function clickBlue() {
+  colorBlack.classList.remove('selected');
+  colorBlue.classList.remove('selected');
+  colorRed.classList.remove('selected');
+  colorGreen.classList.remove('selected');
+}
+
+colorRed.addEventListener('click', clickRed);
+function clickRed() {
+  colorBlack.classList.remove('selected');
+  colorBlue.classList.remove('selected');
+  colorRed.classList.add('selected');
+  colorGreen.classList.remove('selected');
+}
+
+colorGreen.addEventListener('click', clickGreen);
+function clickGreen() {
+  colorBlack.classList.remove('selected');
+  colorBlue.classList.remove('selected');
+  colorRed.classList.remove('selected');
+  colorGreen.classList.add('selected');
+}
