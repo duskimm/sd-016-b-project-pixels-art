ce85de5dc0e43829737388dd8f3362a64f990b39
Danilo-Couto <couto.danilo95@gmail.com>
2021-09-11 20:43:42 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 window.onload = function inicialColor() {
-  const selectedColor = document.createElement('class');
-  selectedColor.className = 'selected';
   const blackColor = document.querySelector('.color');
-  blackColor.classList.add("selected");
+  blackColor.classList.add('selected');
 };
+
+//----
+// este era meu codigo orignal, mas com ajuda do Hugo Daniel, cheguei ao debaixo.
+
+/*  const mouseOverPalette = document.getElementById('color-palette');
+mouseOverPalette.addEventListener('mouseover', clickAlert);
+
+function clickAlert() {
+  */
+/* const colorArray = document.querySelectorAll('.color');
+
+  for (const index in colorArray) {
+    colorArray[index].addEventListener('click', (event) => {
+      for (const index in colorArray) {
+        colorArray[index].className = 'color';
+      }
+      event.target.classList.add('selected');
+    });
+  }
+//} */
+
+const colorArray = document.querySelector('#color-palette');
+colorArray.addEventListener('click', (event) => {
+  for (let index = 0; index < colorArray.children.length; index += 1) {
+    colorArray.children[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+});
