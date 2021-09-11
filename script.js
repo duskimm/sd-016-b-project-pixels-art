f438530c23b79e6df03c02cda2a15021021f7c9d
Danielle Silva <danielle.luisasilva@gmail.com>
2021-09-11 13:27:54 -0300

    adiciona requisito 4

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-// Exercicio
-const cores = document.querySelector('#color-palette');
+// Exercicio 4
+// function PaletaCore(pixels) {
+//     let divPai = document.querySelector('#pixel-board');
+//     let divPixels = document.querySelector('.pixel');
+    
+//     for (let index = 0; index < pixels; index += 1) {
+//         divPixels = document.createElement('div');
+//         divPixels.classList.add('pixel');
+//         divPai.appendChild(divPixels);  
+//         }  
+// }
+
+// PaletaCore(5);
+
