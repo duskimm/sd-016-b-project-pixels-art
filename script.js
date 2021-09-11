72183b8bbc461546dc9606ee358668b61c2c6e47
Izabela <izabelahammer@gmail.com>
2021-09-11 20:40:52 -0300

    quarto requisito faz uma matriz para a pintura com um id pixel-board

---
+++ script.js
@@ -0,0 +1,32 @@
+ // criando a matriz do quadro de pintura 5x5
+// o loop abaixo cria as linhas
+ for (let i = 0; i < 5; i += 1) {
+
+    const Matrix = document.getElementById('pixel-board');
+  
+    const createLine = document.createElement('line');
+  
+    createLine.className = 'line';
+  
+    Matrix.appendChild(createLine);
+  
+  }
+   
+  const line = document.getElementsByClassName('line');
+
+// o loop abaixo pega cada linha e acrescenta colunas. 
+  for (let i = 0; i < 5; i += 1) {
+  
+    for (let j = 0; j < 5; j += 1) {
+  
+      const createColumn = document.createElement('Column');
+  
+      createColumn.className = 'pixel';
+  
+      line[i].appendChild(createColumn);
+  
+    }
+  
+  }
+
+  
