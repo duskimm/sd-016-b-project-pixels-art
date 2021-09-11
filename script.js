c4f8d08cd21ba42922981b8f67004c301497c239
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-09-11 13:22:22 -0300

    adiciona requisito 12

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-document.getElementById('black').classList.add('selected');
+document.getElementById('firstColor').classList.add('selected');
+
+for (let index = 1; index <= 3; index++) {
+
+  // função para gerar cores aleatorias retirada do site https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
+
+  function gerar_cor(opacidade = 1) {
+    let r = Math.random() * 255;
+    let g = Math.random() * 255;
+    let b = Math.random() * 255;
+
+    return `rgba(${r}, ${g}, ${b}, ${opacidade})`;
+  }
+  document.getElementById('firstColor').style.backgroundColor = 'black';
+  let color = document.getElementsByClassName('color');
+  color[index].style.backgroundColor = gerar_cor();
+}
 
 let color = document.getElementById('color-palette')
 
@@ -17,7 +33,7 @@ let pixel = document.querySelectorAll('.pixel');
 for (let index = 0; index < pixel.length; index++) {
   pixel[index].addEventListener('click', function(event) {
     let selected = document.querySelector('.selected');
-    event.target.id = selected.id;
+    event.target.style.backgroundColor = selected.style.backgroundColor;
   })
 }
 
@@ -26,6 +42,6 @@ let botao = document.getElementsByTagName('button')[0];
 
 botao.addEventListener('click', function() {
   for (let index = 0; index < pixel.length; index++) {
-    pixel[index].id = 'null';
+    pixel[index].style.backgroundColor = 'white';
   }
 })
