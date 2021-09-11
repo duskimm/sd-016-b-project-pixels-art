48d4c0cca5cf3629f3a4cf56d24ff51a297d3a28
Renata <renatamagno@live.com>
2021-09-11 16:54:11 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Função para selecionar o cor na paleta
+function selectColor(event){
+  if(event.target.classList.contains("color")){
+    const selectedColor = document.querySelector('.selected');
+    selectColor.classlist.remove('selected');
+    target.classList.add('selected');
+  }
+} 
 
+// let tamanhoGrade = 
+// function criarGrade(tamanhoGrade) {
+//   for (let i = 0; i < tamanhoGrade; i += 1) {
+
+//   }
+//   criarGrade(5);
+
+// window.onload = () => {
+
+
+// };
