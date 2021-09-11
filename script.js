015e3ee60c58a15a541368ee12ff5020515dd3fe
Bruno Melo <melobrunoti@gmail.com>
2021-09-11 10:19:48 -0300

    Requisitos 4-8

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+
+
 const pixelLine = document.querySelectorAll('.line');
+let pixelBoard = document.querySelector('#pixel-board')
+
 
 function createPixelLine(divLine) {
   for (let index = 0; index < 5; index += 1) {
@@ -14,3 +18,31 @@ function createPixelBoard(line) {
   }
 }
 createPixelBoard(pixelLine);
+
+const colors = document.getElementsByClassName('color');
+
+//creditos ao canal Rocketseat, video aula muito esclarecedora, que me ajudou a entender a "referencia" a funcao https://www.youtube.com/watch?v=UftSB4DaRU4&ab_channel=Rocketseat.
+
+function selectColor(event) {
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+
+for (let index = 0; index < colors.length; index += 1) {
+  colors[index].addEventListener('click', selectColor);
+
+}
+
+function paintPixel(event){
+    if(event.target.classList.contains('pixel')){
+    const selectedItem = document.querySelector('.selected');
+    const palleteColor = window.getComputedStyle(selectedItem).getPropertyValue('background-color')
+    const pixel = event.target
+    pixel.style.backgroundColor = palleteColor
+    }
+}
+document.addEventListener('click',paintPixel);
+
+
