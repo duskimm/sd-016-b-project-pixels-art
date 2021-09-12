f34dbf33a65149d4e766faa0895ca80bea34e473
igor rafael <igor.rafael.briano@gmail.com>
2021-09-12 19:59:53 -0300

    formatacao do codigo e finalizacao da funcionalidade apagar antiga board

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-let tamanho = 6;
+let printColor = 'black';
+let color = [];
+
+//chama cada pixel para uma let e add funcao de botao
+function CreatBoardButton() {
+  for(let index = 0; index < document.getElementsByClassName('pixel').length; index += 1) {
+    pixel.push(document.getElementsByClassName('pixel')[index]);
+    pixel[index].addEventListener('click', print);
+  }
+}
+
+//limpa o quadro de pixel
+function clearBoard () {
+  for(index = 0; index < document.getElementsByClassName('pixel').length; index += 1) {
+    pixel[index].style.backgroundColor = 'white';
+  }
+}
+
+//apaga a antiga tabela
+function eraseBoard() {
+  let elemento = document.getElementById("pixel-board");
+  while (elemento.firstChild) {
+  elemento.removeChild(elemento.firstChild);
+  }
+}
 
 //cria a pixel-board de acordo com o tamanho indicado
-bordCreator(tamanho);
-function bordCreator(tamanho) {
-  
+function bordCreator() {
+  pixel = [];
+  eraseBoard();
+  let tamanho = 5;
   for (let index = 0; index < tamanho; index += 1) {
     for (let indice = 0; indice < tamanho; indice += 1) {
       let div = document.createElement("div");
@@ -12,46 +37,45 @@ function bordCreator(tamanho) {
     let br = document.createElement("br");
     document.querySelector('#pixel-board').appendChild(br);
   }
+  CreatBoardButton();
+  clearBoard();
 }
 
-window.onload = function() {
-  let printColor = 'black';
+//funcao que seleciona e desmarca as outras cores da palheta
+function selectColor(event) {
+  document.querySelector('.selected').classList.remove('selected');
+  event.target.classList.add('selected');
+  printColor = event.target.classList[1];
+}
 
-  //faz com que a cor black seja selecionada
-  setColor();
+//pinta a caixa selecionada
+function print (event) {
+  event.target.style.backgroundColor = printColor;
+}
+
+bordCreator();
+
+window.onload = function() {  
+//faz com que a cor black seja selecionada
   function setColor () {
-      let colorSelect = document.querySelector('.color');
-      colorSelect.classList.add('selected');
+    let colorSelect = document.querySelector('.color');
+    colorSelect.classList.add('selected');
   }
-  //chama cada cor para uma let, adiciona evento nas cores, caso clicke chama uma função que marca ela e desmarca as outras cores
-  let color = [];
-  for(let index = 0; index < document.getElementsByClassName('color').length; index += 1) {
-    color.push(document.getElementsByClassName('color')[index]);
-    color[index].addEventListener('click', selectColor);
-  }
-  //funcao que seleciona e desmarca as outras cores da palheta
-  function selectColor(event) {
-    document.querySelector('.selected').classList.remove('selected');
-    event.target.classList.add('selected');
-    printColor = event.target.classList[1];
-    // event.target.querySelector('.selected').style.backgroundColor = event.target.classList[1];
-  }
-  //chama cada pixel para uma let e add funcao de botao
-  let pixel = [];
-  for(let index = 0; index < document.getElementsByClassName('pixel').length; index += 1) {
-    pixel.push(document.getElementsByClassName('pixel')[index]);
-    pixel[index].addEventListener('click', print);
-  }
-  //pinta o quadrado clickado
-  function print (event) {
-    event.target.style.backgroundColor = printColor;
-  }
-  //limpa o quadro de pixel
-  let buttonClean = document.querySelector('#clear-board');
-  buttonClean.addEventListener('click', clearBoard);
-  function clearBoard () {
-    for(index = 0; index < document.getElementsByClassName('pixel').length; index += 1) {
-      pixel[index].style.backgroundColor = 'white';
+setColor();
+
+//cria os botoes especificos
+  function CreatAllButton () {
+    //cria o botao para limpar o board
+    let buttonClean = document.querySelector('#clear-board');
+    buttonClean.addEventListener('click', clearBoard);
+    //botao para carregar o board
+    let buttonUser = document.querySelector('#generate-board');
+    buttonUser.addEventListener('click', bordCreator);
+    //chama cada cor para uma let, adiciona evento nas cores, caso clicke chama uma função que marca ela e desmarca as outras cores
+    for(let index = 0; index < document.getElementsByClassName('color').length; index += 1) {
+      color.push(document.getElementsByClassName('color')[index]);
+      color[index].addEventListener('click', selectColor);
     }
   }
+CreatAllButton();
 }
