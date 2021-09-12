ace0b038017c40fa56f243ed679a996c4d00ac99
Airtonsilv4 <airton.silva19@hotmail.com>
2021-09-12 16:59:11 -0300

    Requisito 04

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-const colorPalette = document.querySelectorAll('.color');
+const pixelBoard = document.getElementById('pixel-board');
+let pixels;
 
+function createBoardColumn(grids) {
+  for (let indexColumn = 1; indexColumn <= grids; indexColumn += 1) {
+    const pixelColumn = document.createElement('div');
+    pixelColumn.classList.add('pixel');
+    pixels.appendChild(pixelColumn);
+  }
+}
+
+function createBoardRow(grids) {
+  for (let indexRow = 1; indexRow <= grids; indexRow += 1) {
+    pixels = document.createElement('div');
+    pixels.classList.add('pixel-line');
+    pixelBoard.appendChild(pixels);
+    createBoardColumn(grids);
+  }
+}
+
+createBoardRow(5);
