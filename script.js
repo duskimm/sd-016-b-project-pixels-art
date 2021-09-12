55b71fef0855eeb81974eff7d6931237db769418
Danilo-Couto <couto.danilo95@gmail.com>
2021-09-12 10:48:27 -0300

    requisito 9

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function inicialColor() {
   const blackColor = document.querySelector('.color');
   blackColor.classList.add('selected');
 };
- 
+
 //----
 // este era meu codigo orignal, mas com ajuda do Hugo Daniel, cheguei ao debaixo.
 
@@ -40,16 +40,50 @@ for (let index = 0; index < pixelBoardArray.length; index += 1) {
     const BGColorSelected = window.getComputedStyle(actualColor, null).getPropertyValue('background-color');
     event.target.style.backgroundColor = BGColorSelected;
   };
-}; 
+}
 
 /* ou
 const pixelBoardArray = document.getElementsByClassName('pixel');
 for (let index = 0; index < pixelBoardArray.length; index += 1) {
   pixelBoardArray[index].addEventListener ('click', btg);
-  
+
     function btg (event) {
       const actualColor = document.querySelector('.selected');
       const BGColorSelected = window.getComputedStyle(actualColor, null).getPropertyValue('background-color');
       event.target.style.backgroundColor = BGColorSelected;
     };
 }; */
+
+const bigSection = document.createElement('section');
+bigSection.className = 'big session';
+document.body.appendChild(bigSection);
+
+bigSection.appendChild((document.getElementById('color-palette')));
+
+const newSection = document.createElement('section');
+newSection.id = 'new-section';
+bigSection.appendChild(newSection);
+
+bigSection.appendChild((document.getElementById('pixel-board')));
+
+const botaoLimpar = document.createElement('button');
+botaoLimpar.id = 'clear-board';
+botaoLimpar.innerHTML = 'Limpar';
+newSection.appendChild(botaoLimpar);
+
+botaoLimpar.onclick = function clean() {
+  for (let index = 0; index < pixelBoardArray.length; index += 1) {
+    pixelBoardArray[index].style.backgroundColor = 'white';
+  }
+};
+
+
+/* ou
+botaoLimpar.addEventListener ('click', clean);
+
+function clean () {
+  for (let index = 0; index < pixelBoardArray.length; index += 1) {
+    pixelBoardArray[index].style.backgroundColor = 'white';
+  }
+};
+ */
