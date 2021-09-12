18dc9ff9eb97f286a7ae9ad46c507229e1ec5fc0
Celso Fiorani <celsofiorani@gmail.com>
2021-09-12 13:48:48 -0300

    parte 5 e 6

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
+const selecionado = document.querySelectorAll(".selected");
+const cor = document.querySelectorAll(".color");
+const paleta = document.getElementById("color-palette");
 
 
 
-
-
-
-
-
-function criaQuadrados (){
-    const quadradoBranco = document.getElementsByClassName('quadrado-branco');
-    for (let index = 0; index < 5; index+= 1) {
-    let linha = document.createElement('div');
-        quadradoBranco.ap
-        
-    }
+// function que faz a classe `.selected` ser alterada dentro da id `color-palette`;
+function corSelecionada (){
+    cor.className = "selected";
+    paleta.addEventListener('click',selecionado)
 }
+
+
+
+
+
+
+
+
+
+// function criaQuadrados (){
+//     const quadradoBranco = document.getElementsByClassName('quadrado-branco');
+//     for (let index = 0; index < 5; index+= 1) {
+//     let linha = document.createElement('div');
+//         quadradoBranco.ap
+        
+//     }
+// }
