7726732d3a56f2777a41e5343368d4c9edc1d48d
Celso Fiorani <celsofiorani@gmail.com>
2021-09-12 12:55:32 -0300

    partes 1 2 3 4 e 5.1

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+function criaQuadrados (){
+    const quadradoBranco = document.getElementsByClassName('quadrado-branco');
+    for (let index = 0; index < 5; index+= 1) {
+    let linha = document.createElement('div');
+        quadradoBranco.ap
+        
+    }
+}
