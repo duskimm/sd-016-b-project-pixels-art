5c8621478ab7f3f60e5c42fdb7c2ccd2b94cd07a
Danielle Silva <danielle.luisasilva@gmail.com>
2021-09-12 15:11:10 -0300

    adicona requisito 8

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Exercicio 4
 // function PaletaCore(pixels) {
 //     let divPai = document.querySelector('#pixel-board');
-//     let divPixels = document.querySelector('.pixel');
-    
+//     let divPixels = document.querySelector('.pixel')
+
 //     for (let index = 0; index < pixels; index += 1) {
 //         divPixels = document.createElement('div');
 //         divPixels.classList.add('pixel');
@@ -21,8 +21,6 @@ window.addEventListener('load', function() {
 
 // Exercicio 7
 
-// a classe Selected deve ser add a cor selecionada
-// somente uma pode ter por vez (eu acho que passa por for e if)
 let idBlack = document.querySelector('#black');
 let idRed = document.querySelector('#red');
 let idGreen = document.querySelector('#green');
@@ -33,9 +31,23 @@ function mudaClasseSelected (event) {
 
     classSelected.classList.remove('selected');
     event.target.classList.add('selected');
-
 }
+
 idBlack.addEventListener('click', mudaClasseSelected);
 idRed.addEventListener('click', mudaClasseSelected);
 idGreen.addEventListener('click', mudaClasseSelected);
 idBlue.addEventListener('click', mudaClasseSelected);
+
+
+
+// Exercicio 8
+
+function corPixel(event) {
+    if (event.target.classList.contains('pixel')) {
+        let classSelected = document.querySelector('.selected');
+        let corDeFundo = window.getComputedStyle(classSelected, null).getPropertyValue('background-color');
+        let pixel = event.target;
+        pixel.style.backgroundColor = corDeFundo;
+    }
+}
+document.addEventListener('click', corPixel);
