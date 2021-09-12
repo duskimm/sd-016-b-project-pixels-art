6eff829723de46a84f8c11710ff25d895a7ace18
Izabela <izabelahammer@gmail.com>
2021-09-12 03:13:19 -0300

    formatacao do js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // criando a matriz do quadro de pintura 5x5
 // o loop abaixo cria as linhas
 for (let i = 0; i < 5; i += 1) {
-
   const matriz = document.getElementById('pixel-board');
 
   const createLine = document.createElement('div');
@@ -11,14 +10,12 @@ for (let i = 0; i < 5; i += 1) {
   matriz.appendChild(createLine);
 
 }
-
 const line = document.getElementsByClassName('line');
 
 // o loop abaixo pega cada linha e acrescenta colunas.
 for (let i = 0; i < 5; i += 1) {
 
   for (let j = 0; j < 5; j += 1) {
-
     const createColumn = document.createElement('div');
 
     createColumn.className = 'pixel';
@@ -26,23 +23,16 @@ for (let i = 0; i < 5; i += 1) {
     line[i].appendChild(createColumn);
 
   }
-
 }
 // funcao para selecionar cor da paleta de cores para pintar
 function selectedPaint() {
-
   const colorSelected = document.querySelector('#color-palette');
-
   colorSelected.addEventListener('click', (event) => {
 
     const selected = document.querySelector('.selected');
-
     if (event.target.id !== 'color-palette') {
-
       selected.classList.remove('selected'); // fonte: https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
-
       event.target.classList.add('selected');
-
     }
   });
 }
@@ -55,13 +45,9 @@ const matrixPaint = document.getElementById('pixel-board');
 matrixPaint.addEventListener('click', (event) => {
 
   if (event.target.className === 'pixel') {
-
     const currentColor = document.querySelector('.selected').id;
-
     const changeColorPixel = event.target;
-
     changeColorPixel.style.backgroundColor = currentColor;
-
   }
 
 });
@@ -72,11 +58,7 @@ const buttonClear = document.getElementById('clear-board');
 const pixels = document.getElementsByClassName('pixel');
 
 buttonClear.addEventListener('click', () => {
-
   for (let i = 0; i < pixels.length; i += 1) {
-
     pixels[i].style.backgroundColor = 'white';
-
   }
-
 });
