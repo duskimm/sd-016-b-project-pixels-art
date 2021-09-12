c46296955d83c794e647521656ec410a7a5f4170
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-09-12 17:08:38 -0300

    Refatorando codigos

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-const lines = 5;
-const squareDivs = document.getElementById('pixel-board');
+function createTitleH1() {
+  const createTitle = document.getElementById('container-title');
+  const createH1 = document.createElement('h1');
+  createH1.id = 'title';
+  createH1.innerText = 'Paleta de Cores';
+  createTitle.appendChild(createH1);
+}
+createTitleH1();
+
+function colorPalette() {
+  const colors = document.getElementById('color-palette');
+  const quantityColor = 4;
+
+  // Ref.: Encontrei à solução, que retorna as cores aleatórias para as demais paletas no site https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript 
+  for (let paletteIdx = 0; paletteIdx < quantityColor; paletteIdx += 1) {
+    const palette = document.createElement('div');
+    palette.className = 'color';
+    colors.appendChild(palette);
+  }
+  colors.firstElementChild.style.backgroundColor = 'black';
+  colors.firstElementChild.className = 'color selected';
+  colors.childNodes[1].style.backgroundColor = '#' + parseInt((Math.random() * 0xFFFFFF)).toString(16).padStart(6, '0');;
+  colors.childNodes[2].style.backgroundColor = '#' + parseInt((Math.random() * 0xFFFFFF)).toString(16).padStart(6, '0');;
+  colors.childNodes[3].style.backgroundColor = '#' + parseInt((Math.random() * 0xFFFFFF)).toString(16).padStart(6, '0');;
+}
+colorPalette();
 
 function SquarePalette() {
+  const lines = 5;
+  const squareDivs = document.getElementById('pixel-board');
   for (let lineIdx = 0; lineIdx < lines; lineIdx += 1) {
     for (let columnsIdx = 1; columnsIdx <= lines; columnsIdx += 1) {
       const square = document.createElement('div');
@@ -11,3 +37,13 @@ function SquarePalette() {
   }
 }
 SquarePalette();
+
+function buttonClear() {
+  const btn = document.getElementsByClassName('btn');
+  const createBtn = document.createElement('button');
+  const createText = document.createTextNode('Limpar');
+  createBtn.appendChild(createText);
+  createBtn.id = 'clear-board';
+  btn.appendChild(createBtn);
+}
+buttonClear();
