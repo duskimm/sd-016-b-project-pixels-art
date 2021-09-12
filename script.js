620a29cd018bc9f98ef9a5214fe452f912eb6bf1
leandro <leandrochagask@gmail.com>
2021-09-12 19:06:46 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -39,16 +39,36 @@ const generateBoardButton = document.querySelector('#generate-board');
 
 generateBoardButton.addEventListener('click', () => {
   const boardSize = document.querySelector('#board-size');
-  const input = boardSize.value;
+  let input = boardSize.value;
   const pixel = document.querySelectorAll('.pixel');
 
   if (input === '') {
     alert('Board inválido!');
   } 
 
+  if (input < 5) {
+    input = 5;
+    boardSize.value = 5;
+  }
+
+  if (input > 50) {
+    input = 50;
+    boardSize.value = 50;
+  }
+
   for (let i = 0; i < pixel.length; i += 1) {
     pixel[i].style.width = `${input}px`;
     pixel[i].style.height = `${input}px`;
-    pixel[i].style.backgroundColor = 'white';
+    pixel[i].style.backgroundColor = 'unset';
   }
 });
+
+// Requisito 11
+
+// Requisito 12
+for (let i = 1; i < color.length; i += 1) {
+  let a = Math.random() * 255;
+  let b = Math.random() * 255;
+  let c = Math.random() * 255;
+  color[i].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
+}
