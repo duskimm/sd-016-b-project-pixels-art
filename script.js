5c17b6cddad7f74a2a2379beecb923c1cc0ba02b
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-09-11 23:24:49 -0300

    Correcao de erros

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-let lines = 5;
-let linePalette = 4;
-let positions = lines;
-let squareDivs = document.getElementById("pixel-board");
-let linePaletteDivs =document.getElementById("color-palette");
-
-// function lineColors() {
-//   for (let idx = 1; idx <= linePalette; idx += 1) {
-//     let colorPalettes = document.createElement("div");
-//     colorPalettes.className = "color";
-//     linePaletteDivs.appendChild(colorPalettes);
-//   }
-// }
-// lineColors();
+const lines = 5;
+const squareDivs = document.getElementById('pixel-board');
 
 function SquarePalette() {
   for (let lineIdx = 0; lineIdx < lines; lineIdx += 1) {
     for (let columnsIdx = 1; columnsIdx <= lines; columnsIdx += 1) {
-      let square = document.createElement("div");
-      square.className = "pixel";
+      let square = document.createElement('div');
+      square.className = 'pixel';
       squareDivs.appendChild(square);
     }
   }
