dd6f5dca367fc37dcb9ab5795335afc5b0f1569d
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-09-12 12:34:22 -0300

    Fix lint

--- script.js
+++ script.js
@@ -7,22 +7,6 @@ const secondColor = document.getElementById('secondColor');
 const thirdColor = document.getElementById('thirdColor');
 const fourtColor = document.getElementById('fourtColor');
 
-let color = 'black';
-btnClear.addEventListener('click', clearAll);
-
-function colorRgb() {
-  const red = Math.floor(Math.random() * 255);
-  const green = Math.floor(Math.random() * 255);
-  const blue = Math.floor(Math.random() * 255);
-  return `rgba(${red}, ${green}, ${blue}, 1)`;
-}
-console.log(colorRgb());
-
-fisrtColor.style.background = 'black';
-secondColor.style.backgroundColor = colorRgb();
-thirdColor.style.backgroundColor = colorRgb();
-fourtColor.style.backgroundColor = colorRgb();
-
 function createPixelBoxes(size) {
   for (let index = 0; index < size; index += 1) {
     const box = document.createElement('div');
@@ -53,6 +37,9 @@ function clearAll() {
   }
 }
 
+let color = 'black';
+btnClear.addEventListener('click', clearAll);
+
 /**
  * Função que pinta os pixels
  */
@@ -61,13 +48,6 @@ function paintsElement(event) {
     const colorToPaint = event.target;
     colorToPaint.style.backgroundColor = color;
   }
-}
-
-/**
- *Adiciona eventos a todos os pixels
- */
-for (let index = 0; index < pixelBox.length; index += 1) {
-  pixelBox[index].addEventListener('click', paintsElement);
 }
 
 /**
@@ -84,7 +64,27 @@ function chosenColor(event) {
   }
 }
 
+/**
+ *Adiciona eventos a todos os pixels
+ */
+for (let index = 0; index < pixelBox.length; index += 1) {
+  pixelBox[index].addEventListener('click', paintsElement);
+}
+
 for (let index = 0; index < colorSelected.length; index += 1) {
   colorSelected[index].addEventListener('click', chosenColor);
   console.log(color);
 }
+
+function colorRgb() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  return `rgba(${red}, ${green}, ${blue}, 1)`;
+}
+console.log(colorRgb());
+
+fisrtColor.style.background = 'black';
+secondColor.style.backgroundColor = colorRgb();
+thirdColor.style.backgroundColor = colorRgb();
+fourtColor.style.backgroundColor = colorRgb();
