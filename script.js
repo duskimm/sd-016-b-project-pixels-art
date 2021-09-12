e0ace6e1e0d0d29d0354614802317dab77eec817
Renata <renatamagno@live.com>
2021-09-12 15:47:57 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-// Função para selecionar o cor na paleta
-function selectColor(event){
-  if(event.target.classList.contains("color")){
-    const selectedColor = document.querySelector('.selected');
-    selectColor.classlist.remove('selected');
-    target.classList.add('selected');
+const colors = document.getElementsByClassName('color');
+// Função que adiociona e remove a class 'selected'
+function selectColor(event) {
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].classList.remove('selected');
   }
-} 
+  event.target.classList.add('selected');
+}
 
-// let tamanhoGrade = 
-// function criarGrade(tamanhoGrade) {
-//   for (let i = 0; i < tamanhoGrade; i += 1) {
-
-//   }
-//   criarGrade(5);
-
-// window.onload = () => {
-
-
-// };
+for (let index = 0; index < colors.length; index += 1) {
+  colors[index].addEventListener('click', selectColor);
+}
