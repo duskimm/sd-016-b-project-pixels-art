13bdc8b46dc9062d1000cd8afa3a9459f64092dd
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-09-11 23:28:28 -0300

    Mais algumas correcoes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const squareDivs = document.getElementById('pixel-board');
 function SquarePalette() {
   for (let lineIdx = 0; lineIdx < lines; lineIdx += 1) {
     for (let columnsIdx = 1; columnsIdx <= lines; columnsIdx += 1) {
-      let square = document.createElement('div');
+      const square = document.createElement('div');
       square.className = 'pixel';
       squareDivs.appendChild(square);
     }
