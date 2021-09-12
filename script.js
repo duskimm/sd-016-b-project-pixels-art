1a2b7babf5196a2e5241b73d6151d6f944a5f8ec
Nathalia <nathaliamagi@gmail.com>
2021-09-11 21:01:25 -0300

    Requisito-6

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 createColorPalette();
 createRows();
 createColumns();
+blackColorSelected();
 
 function createColorPalette() {
   const colors = ['black', 'yellow', 'red', 'blue'];
@@ -20,24 +21,29 @@ function createColorPalette() {
 
 function createRows() {
   let recoveryDivPixelB = document.getElementById('pixel-board');
-  
-  for(let index = 0; index < rows; index += 1) { //cria as linhas
+  for(let index = 0; index < rows; index += 1) {  //cria as linhas
     let divRow = document.createElement('div');
     divRow.className = 'pixel';
     divRow.style.backgroundColor = 'white';
     recoveryDivPixelB.appendChild(divRow);
-   }
- }
+  }
+}
 
-function createColumns() {
-    let recoveryDivPixelB = document.getElementById('pixel-board');
-    for(let index = 0; index < rows; index += 1){
-        for(let index2 = 0; index2 < (columns -1); index2 +=1 ) {
-            let divColumn = document.createElement('div');
-            divColumn.className = 'pixel'
-            divColumn.style.backgroundColor = 'white';
-           recoveryDivPixelB.appendChild(divColumn);
-        }
+ function createColumns() {
+  let recoveryDivPixelB = document.getElementById('pixel-board');
+  for(let index = 0; index < rows; index += 1) {
+      for(let index2 = 0; index2 < (columns -1); index2 +=1 ) { // o columns -1 é usado pq a primeira linha já foi criada na função anterior
+        let divColumn = document.createElement('div');
+        divColumn.className = 'pixel'
+        divColumn.style.backgroundColor = 'white';
+        recoveryDivPixelB.appendChild(divColumn);
     }
+}
+}
+
+ function blackColorSelected() {
+    let recoveryDivColor = document.querySelector('.color');
+    recoveryDivColor.classList.add('selected');
+    // console.log(recoveryDivColor);
  }
 }
