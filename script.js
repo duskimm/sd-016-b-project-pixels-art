fad665129498ec2d34fc7b51a77ee452a1e28b77
Nathan Nunes <nunesxd7@hotmail.com>
2021-09-12 15:03:24 -0300

    ate a 9 com lint

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function grid() {
   const container = document.getElementById('pixel-board');
   for (let i = 0; i < baseGrid; i += 1) {
     const rowDiv = document.createElement('div');
+    rowDiv.className = 'gridRow';
     for (let index = 0; index < baseGrid; index += 1) {
       const columnGrid = document.createElement('div');
       columnGrid.className = 'pixel';
@@ -24,13 +25,34 @@ function select() {
 function getColored() {
   const container = document.getElementById('pixel-board');
   container.addEventListener('click', function paint(clickedPixel) {
-    let currentColor = document.querySelector('.selected');
-    let currentColors = window.getComputedStyle(currentColor).backgroundColor
-    clickedPixel.target.style.backgroundColor = currentColors
-  })
+    const currentColor = document.querySelector('.selected');
+    const currentColors = window.getComputedStyle(currentColor).backgroundColor;
+    const clickedTarget = clickedPixel.target;
+    if (clickedTarget.className === 'pixel') {
+      clickedTarget.style.backgroundColor = currentColors;
+    }
+  });
+}
+function clean() {
+  const main = document.querySelector('main');
+  const resetButton = document.createElement('button');
+  main.insertBefore(resetButton, main.childNodes[4]);
+  resetButton.id = ('clear-board');
+  resetButton.innerText = 'Limpar';
+  resetButton.addEventListener('click', function clearGrid() {
+    const completeGrid = document.querySelector('#pixel-board').children;
+    for (let i = 0; i < completeGrid.length; i += 1) {
+      const gridDiv = completeGrid[i];
+      for (let index = 0; index < completeGrid.length; index += 1) {
+        const divPixel = gridDiv.children;
+        divPixel[index].style.backgroundColor = 'white';
+      }
+    }
+  });
 }
 window.onload = function start() {
   grid();
   select();
   getColored();
+  clean();
 };
