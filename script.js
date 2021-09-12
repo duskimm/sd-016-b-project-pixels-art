afd264ae7a835116017bc488e82920d28dce290a
Renata <renatamagno@live.com>
2021-09-12 16:42:47 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const colors = document.getElementsByClassName('color');
+
 // Função que adiociona e remove a class 'selected'
 function selectColor(event) {
   for (let i = 0; i < colors.length; i += 1) {
@@ -10,3 +11,19 @@ function selectColor(event) {
 for (let index = 0; index < colors.length; index += 1) {
   colors[index].addEventListener('click', selectColor);
 }
+
+// Função para pintar pixels
+// referencias: https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+// https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/getPropertyValue
+// Se o local do click contém a class "pixel", recupera o item que contém a class "selected" e add na const item, recupera a background-color do item, add ao pixel clicado. 
+
+function paintPixel(event) {
+  if (event.target.classList.contains('pixel')) {
+    const item = document.querySelector('.selected');
+    const color = window.getComputedStyle(item).getPropertyValue('background-color');
+    const pixel = event.target;
+    pixel.style.backgroundColor = color;
+  }
+}
+
+document.addEventListener('click', paintPixel);
