b04fc9b7be015be1caf1b10b3335ca94e8a81941
leandro <leandrochagask@gmail.com>
2021-09-12 18:13:09 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
-let colorPalette = document.querySelector('#color-palette')
-let color = document.querySelectorAll('.color');
+const colorPalette = document.querySelector('#color-palette');
+const color = document.querySelectorAll('.color');
 
-let pixelBoard = document.querySelector('#pixel-board')
+const pixelBoard = document.querySelector('#pixel-board');
 
 // Requisito 7
-colorPalette.addEventListener('click', function (event) {
+colorPalette.addEventListener('click', (event) => {
   for (let i = 0; i < color.length; i += 1) {
     color[i].classList.remove('selected');
   }
 
-  if ( event.target.classList.contains( 'color' ) ) {  
+  if (event.target.classList.contains('color')) {
     event.target.classList.add('selected');
   }
 });
 
 // Requisito 8
-pixelBoard.addEventListener('click', function (event) {
-  if ( event.target.classList.contains( 'pixel' ) ) {
-    let selected = document.querySelector('.selected');
-    let cor = getComputedStyle(selected).backgroundColor;
-    event.target.style.backgroundColor = cor;
+pixelBoard.addEventListener('click', (event) => {
+  if (event.target.classList.contains('pixel')) {
+    const selected = document.querySelector('.selected');
+    const selectedColor = getComputedStyle(selected).backgroundColor;
+    event.target.style.backgroundColor = selectedColor;
   }
 });
 
 // Requisito 9
-let clearBoardButton = document.querySelector('#clear-board');
+const clearBoardButton = document.querySelector('#clear-board');
 
-clearBoardButton.addEventListener('click', function() {
-  let pixel = document.querySelectorAll('.pixel')
+clearBoardButton.addEventListener('click', () => {
+  const pixel = document.querySelectorAll('.pixel');
 
-  for (let i = 0; i < pixel.length; i++) {
-    pixel[i].style.backgroundColor = 'white'
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].style.backgroundColor = 'white';
   }
-})
+});
+
+// Requisito 10
+const generateBoardButton = document.querySelector('#generate-board');
+
+generateBoardButton.addEventListener('click', () => {
+  const boardSize = document.querySelector('#board-size');
+  const input = boardSize.value;
+  const pixel = document.querySelectorAll('.pixel');
+
+  if (input === '') {
+    alert('Board inválido!');
+  } 
+
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].style.width = `${input}px`;
+    pixel[i].style.height = `${input}px`;
+    pixel[i].style.backgroundColor = 'white';
+  }
+});
