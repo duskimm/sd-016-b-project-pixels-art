915abe677162f3ec7808c3dbe7beac29d26b288b
Guilherme Rosa <gui.rsa@hotmail.com>
2021-09-12 20:52:23 -0300

    exercice 7

--- script.js
+++ script.js
@@ -3,13 +3,36 @@ console.log('vamo q vamo');
 window.onload = function selecting() {
   const corPreta = document.getElementById('black');
   corPreta.classList.add('selected');
+  const cores = document.querySelectorAll('.color');
+  for (let index = 1; index < cores.length; index += 1) {
+    cores[index].classList.remove('selected');
+  }
 };
 
-function createPixelBoardLine(colum) {
+function selector(event) {
+  const colorSelector = document.querySelectorAll('.color');
+  for (let index = 0; index < colorSelector.length; index += 1) {
+    colorSelector[index].classList.add('selected');
+    colorSelector[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+
+let blackColor = document.getElementById('black');
+let redColor = document.getElementById('red');
+let greenColor = document.getElementById('green');
+let blueColor = document.getElementById('blue');
+
+blackColor.addEventListener('click', selector);
+redColor.addEventListener('click', selector);
+greenColor.addEventListener('click', selector);
+blueColor.addEventListener('click', selector);
+
+function createPixelBoardLine(line) {
   const pixelBoardhtml = document.getElementById('pixel-board');
   const divisao = document.createElement('div');
   pixelBoardhtml.appendChild(divisao);
-  for (let index = 1; index <= colum; index += 1) {
+  for (let index = 1; index <= line; index += 1) {
     const criar = document.createElement('div');
     criar.classList.add('pixel');
     divisao.appendChild(criar);
@@ -18,23 +41,21 @@ function createPixelBoardLine(colum) {
 
 function createPixelColumns(line) {
   for (let index = 1; index <= line; index += 1) {
-    createPixelBoardLine(colum);
+    createPixelBoardLine(line);
   }
 }
 
-function changeDimension() {
-  const altura = document.getElementById('height-changer');
-  const largura = document.getElementById('width-changer');
-  if (altura.value == '' && largura.value == '') {
-    const line = 5;
-    colum = 5;
-    createPixelColumns(line);
+function generator() {
+  const input = document.getElementById('board-size');
+  if (input.value <= 0) {
+    const lines = 5;
+    createPixelColumns(lines);
   } else {
-    line = parseInt.altura.value;
-    colum = parseInt.largura.value;
-    createPixelColumns(line);
+    lines = parseInt(input.value);
+    createPixelColumns(lines);
   }
 }
 
-const submitButton = document.getElementById('submit');
-submitButton.addEventListener('click', changeDimension());
+const submitButton = document.getElementById('generate-board');
+submitButton.addEventListener('click', generator());
+const input = document.getElementById('board-size');
