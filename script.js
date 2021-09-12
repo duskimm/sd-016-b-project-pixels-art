799bd3c8e6756d7247231504be8a237a6ed1fd28
Renata Domingues <blackbirdfly.r@gmail.com>
2021-09-12 10:43:39 -0300

    Finaliza requisito 6 e 7

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function addClassSelected() {
-  const blackInitial = document.getElementById('color-palette').firstElementChild; // recupera onde quero trabalhar //
+window.onload = function addClassSelected() {
+  const blackInitial = document.getElementById('color-palette').firstElementChild; // recupera onde quero trabalhar, o primeiro filho do ID, no caso a cor black //
   blackInitial.classList.add('selected');
-}
-addClassSelected();
+};
+const palette = document.getElementById('color-palette'); // recupera onde quero trabalhar //
+palette.addEventListener('click', (event) => { // seleciona o evento de "escuta" onde através do clique, ocorre a seguinte ação: //
+  for (let index = 0; index < palette.children.length; index += 1) { // percorre todo o comprimento da paleta de cores //
+    palette.children[index].classList.remove('selected'); // exclui a classe selected, e adiciona a classe selected ao ser clicado no novo evento de origem //
+  }
+  event.target.classList.add('selected');
+});
