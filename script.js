93d83d0e0d6ec2e453bbdf5a77098084e92de7db
Lucca Neiva <luccaneiva12@gmail.com>
2021-09-12 02:13:07 -0300

    Adicionados requisitos 4 e 5

--- script.js
+++ script.js
@@ -23,14 +23,31 @@ function createPalette(colors) {
   body.appendChild(paletteList);
 }
 
-createPalette(['red', 'blue', 'green', 'yellow']);
+createPalette(['red', 'blue', 'green']);
 
 function addPaletteColor(color) {
-  let paletteList = document.getElementById('color-palette');
-  let newColor = document.createElement('div');
+  const paletteList = document.getElementById('color-palette');
+  const newColor = document.createElement('div');
   newColor.className = 'color';
   newColor.style.backgroundColor = color;
   paletteList.prepend(newColor);
 }
 
 addPaletteColor('black');
+
+function pixelBoard(height, width) {
+  const pixelBoard = document.createElement('div');
+  pixelBoard.id = 'pixel-board';
+  for (let i = 0; i < height; i += 1) {
+    const line = document.createElement('div');
+    for (let j = 0; j < width; j += 1) {
+      const paintingPixel = document.createElement('div');
+      paintingPixel.className = 'pixel';
+      line.appendChild(paintingPixel);
+    }
+    pixelBoard.appendChild(line);
+  }
+  body.appendChild(pixelBoard);
+}
+
+pixelBoard(5, 5);
