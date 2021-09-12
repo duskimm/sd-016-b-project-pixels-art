f797edd12c9aaba98fbdf89639cbbbeb806312b9
Israel Sant'Anna <israel.santanna@gmail.com>
2021-09-11 23:52:33 -0300

    Add custom cursor and more:
    * Generate new pallete from title click
    * Show hints on hover
    * Several visual improvements
    * Code organization and adjustment

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 const colors = [];
 let boardSize = 5;
 
+// Cursor functions
+function invert(color) {
+  const r = 255 - color.toString().match(/([0-9]+)/g)[0];
+  const g = 255 - color.toString().match(/([0-9]+)/g)[1];
+  const b = 255 - color.toString().match(/([0-9]+)/g)[2];
+  const invertedColor = `rgb(${r}, ${g}, ${b})`;
+  return invertedColor;
+}
+
+function updateCursorColor(newColor) {
+  const bodyStyle = document.body.style;
+  bodyStyle.setProperty('--cursor-color', newColor);
+  bodyStyle.setProperty('--cursor-border-color', invert(newColor));
+}
+
+// Colors functions
 function isInvalidColor(color) {
-  return color.endsWith('100%, 100%)') || color.endsWith('0%, 0%)') || colors.includes(color);
+  return color === 'rgb(255, 255, 255)' || color === 'rgb(0, 0, 0)' || colors.includes(color);
 }
 
 function getRandomColor() {
-  let randomColor = `hsl(
-    ${Math.ceil(Math.random() * 360)},
-    ${Math.ceil(Math.random() * 100)}%,
-    ${Math.ceil(Math.random() * 100)}%
+  let randomColor = `rgb(
+    ${Math.ceil(Math.random() * 255)},
+    ${Math.ceil(Math.random() * 255)},
+    ${Math.ceil(Math.random() * 255)}
   )`;
   if (isInvalidColor(randomColor)) randomColor = getRandomColor();
   return randomColor;
 }
 
-function setColor(element, color) {
-  const { style } = element;
-  style.backgroundColor = color;
-}
-
-function generateColorPalette() {
-  const colorPaletteSlots = document.getElementById('color-palette').children;
-  for (let index = 0; index < colorPaletteSlots.length - 1; index += 1) {
-    colors.push(getRandomColor());
-  }
-  setColor(colorPaletteSlots[0], 'black');
-  colors.forEach((color, index) => {
-    setColor(colorPaletteSlots[index + 1], color);
-  });
-}
-
+// Board functions
 function paintPixel(evt) {
   const selectedColor = document.querySelector('.selected').style.backgroundColor;
   const element = evt.target;
@@ -52,13 +53,6 @@ function generatePixelBoard() {
   }
 }
 
-function selectColor(evt, inputColors) {
-  inputColors.forEach((inputColor) => {
-    inputColor.classList.remove('selected');
-  });
-  evt.target.classList.add('selected');
-}
-
 function clearPixelBoard() {
   const board = document.getElementById('pixel-board');
   board.innerHTML = '';
@@ -72,6 +66,38 @@ function regeneratePixelBoard(newBoardSize) {
   generatePixelBoard();
 }
 
+// Color palette functions
+function setColor(element, color) {
+  const { style } = element;
+  style.backgroundColor = color;
+}
+
+function selectPaletteColor(evt) {
+  Array.from(evt.target.parentElement.children).forEach((inputColor) => {
+    inputColor.classList.remove('selected');
+  });
+  evt.target.classList.add('selected');
+  updateCursorColor(evt.target.style.backgroundColor);
+}
+
+function updatePaletteColor(evt) {
+  const newColor = getRandomColor();
+  setColor(evt.target, newColor);
+  updateCursorColor(newColor);
+}
+
+function generateColorPalette() {
+  const colorPaletteSlots = document.getElementById('color-palette').children;
+  for (let index = 0; index < colorPaletteSlots.length - 1; index += 1) {
+    colors[index] = getRandomColor();
+  }
+  setColor(colorPaletteSlots[0], 'rgb(0, 0, 0)');
+  colors.forEach((color, index) => {
+    setColor(colorPaletteSlots[index + 1], color);
+  });
+}
+
+// Events functions
 function clamp(min, value, max) {
   let clampedValue = value;
   if (clampedValue < min) clampedValue = 5;
@@ -79,22 +105,25 @@ function clamp(min, value, max) {
   return clampedValue;
 }
 
-function addInputColorsEvents(inputColors) {
+function addInputColorsEvents() {
+  const inputColors = document.querySelectorAll('.color');
   inputColors.forEach((inputColor) => {
-    inputColor.addEventListener('click', (evt) => selectColor(evt, inputColors));
+    inputColor.addEventListener('click', selectPaletteColor);
     if (!inputColor.classList.contains('fixed-color')) {
-      inputColor.addEventListener('dblclick', (evt) => setColor(evt.target, getRandomColor()));
+      inputColor.addEventListener('dblclick', updatePaletteColor);
     }
   });
 }
 
-function addClearButtonEvents(clearButton) {
+function addClearButtonEvents() {
+  const clearButton = document.getElementById('clear-board');
   clearButton.addEventListener('click', () => {
     regeneratePixelBoard(boardSize);
   });
 }
 
-function addBoardSizeInputEvents(boardSizeInput) {
+function addBoardSizeInputEvents() {
+  const boardSizeInput = document.getElementById('board-size');
   boardSizeInput.addEventListener('change', (evt) => {
     const element = evt.target;
     element.value = clamp(5, element.value, 50);
@@ -102,25 +131,45 @@ function addBoardSizeInputEvents(boardSizeInput) {
   });
 }
 
-function addGenerateBoardButtonEvents(generateBoardButton, boardSizeInput) {
+function addGenerateBoardButtonEvents() {
+  const generateBoardButton = document.getElementById('generate-board');
   generateBoardButton.addEventListener('click', () => {
+    const boardSizeInput = document.getElementById('board-size');
     regeneratePixelBoard(boardSizeInput.value);
   });
 }
 
-function addEvents(inputColors, clearButton, boardSizeInput, generateBoardButton) {
-  addInputColorsEvents(inputColors);
-  addClearButtonEvents(clearButton);
-  addBoardSizeInputEvents(boardSizeInput);
-  addGenerateBoardButtonEvents(generateBoardButton, boardSizeInput);
+function addCursorEvents() {
+  // Custom cursor idea from 'Circle Cursors' pen by Chris Heuberger, available @:
+  // https://codepen.io/ChrisBup/pen/rNwLyzV
+  document.addEventListener('mousemove', (evt) => {
+    const customCursor = document.getElementById('custom-cursor');
+    window.requestAnimationFrame(() => {
+      customCursor.style.top = `${evt.clientY - customCursor.offsetHeight / 2}px`;
+      customCursor.style.left = `${evt.clientX - customCursor.offsetWidth / 2}px`;
+    });
+  });
+}
+
+function addTitleEvents() {
+  const title = document.getElementById('title');
+  title.addEventListener('click', () => {
+    generateColorPalette();
+    document.querySelector('.fixed-color').click();
+  });
+}
+
+function addEvents() {
+  addInputColorsEvents();
+  addClearButtonEvents();
+  addBoardSizeInputEvents();
+  addGenerateBoardButtonEvents();
+  addCursorEvents();
+  addTitleEvents();
 }
 
 window.onload = () => {
-  const inputColors = document.querySelectorAll('.color');
-  const clearButton = document.getElementById('clear-board');
-  const boardSizeInput = document.getElementById('board-size');
-  const generateBoardButton = document.getElementById('generate-board');
   generateColorPalette();
   generatePixelBoard();
-  addEvents(inputColors, clearButton, boardSizeInput, generateBoardButton);
+  addEvents();
 };
