2dc5d9baed173d64aaa5331cc1df187565ea2ef1
Izabela <izabelahammer@gmail.com>
2021-09-12 02:26:12 -0300

    requisito 8 npm test ok

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
- // criando a matriz do quadro de pintura 5x5
+// criando a matriz do quadro de pintura 5x5
 // o loop abaixo cria as linhas
 for (let i = 0; i < 5; i += 1) {
 
-    const matriz = document.getElementById('pixel-board');
-  
-    const createLine = document.createElement('div');
-  
-    createLine.className = 'line';
-  
-    matriz.appendChild(createLine);
-  
+  const matriz = document.getElementById('pixel-board');
+
+  const createLine = document.createElement('div');
+
+  createLine.className = 'line';
+
+  matriz.appendChild(createLine);
+
 }
-   
+
 const line = document.getElementsByClassName('line');
 
-// o loop abaixo pega cada linha e acrescenta colunas. 
+// o loop abaixo pega cada linha e acrescenta colunas.
 for (let i = 0; i < 5; i += 1) {
- 
+
   for (let j = 0; j < 5; j += 1) {
-  
+
     const createColumn = document.createElement('div');
-  
+
     createColumn.className = 'pixel';
-  
+
     line[i].appendChild(createColumn);
-  
+
   }
 
 }
@@ -39,7 +39,7 @@ function selectedPaint() {
 
     if (event.target.id !== 'color-palette') {
 
-      selected.classList.remove('selected');  // fonte: https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
+      selected.classList.remove('selected'); // fonte: https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
 
       event.target.classList.add('selected');
 
@@ -48,3 +48,20 @@ function selectedPaint() {
 }
 
 selectedPaint();
+
+// pintar apos selecionar a cor e somente o pixel desejado
+const matrixPaint = document.getElementById('pixel-board');
+
+matrixPaint.addEventListener('click', (event) => {
+
+  if (event.target.className === 'pixel') {
+
+    const currentColor = document.querySelector('.selected').id;
+
+    const changeColorPixel = event.target;
+
+    changeColorPixel.style.backgroundColor = currentColor;
+
+  }
+
+});
