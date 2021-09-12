b09816ee56c6bbabc01d627aedc18be49003bdd3
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-09-12 14:05:19 -0300

    passo 5, 6 e 7

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
+// Cria divs com classe color com cores dinamicas
+
 let chamaDivPai = document.getElementById('color-palette');
 
-function colorPalette(cor){
-    chamaDivPai;
-   let primeiroFilhoDaPaletaRosa = document.createElement('div');
-    primeiroFilhoDaPaletaRosa.className = 'color';
-    primeiroFilhoDaPaletaRosa.style.backgroundColor =cor;
-    chamaDivPai.appendChild(primeiroFilhoDaPaletaRosa);
+function colorPalette(cor) {
+  let primeiroFilhoDaPaletaRosa = document.createElement('div');
+  primeiroFilhoDaPaletaRosa.className = 'color';
+  primeiroFilhoDaPaletaRosa.style.backgroundColor = cor;
+  chamaDivPai.appendChild(primeiroFilhoDaPaletaRosa);
+  if (cor === 'black') {
+    primeiroFilhoDaPaletaRosa.classList.add('selected');
+  }
 }
 colorPalette('black');
 colorPalette('pink');
 colorPalette('purple');
 colorPalette('green');
 
-// function colorPalette2(){
-//     chamaDivPai;
-//     let segundoFilhoDaPaletaVerde = document.createElement('div');
-//     segundoFilhoDaPaletaVerde.className = 'color';
-//     segundoFilhoDaPaletaVerde.style.backgroundColor ='green';
-//     chamaDivPai.appendChild(segundoFilhoDaPaletaVerde);
-// }
-// colorPalette2();
+function createDivBorderPixel(numero) {
+  let divPixel = document.querySelector('#pixel-board');
+  // for pra criar as linhas do quadro pixel.
+  for (let index = 0; index < numero; index += 1) {
+    let divLine = document.createElement('div');
+    divPixel.appendChild(divLine);
+    divLine.classList.add('line');
 
-// function colorPalette3(){
-//     chamaDivPai;
-//    let terceiroFilhoDaPaletaPreto = document.createElement('div');
-//      terceiroFilhoDaPaletaPreto.className = 'color';
-//      terceiroFilhoDaPaletaPreto.style.backgroundColor ='gray';
-//    chamaDivPai.appendChild(terceiroFilhoDaPaletaPreto);
-//    }
-//    colorPalette3();
+    // for pra criar as divs dentro das divline
+
+    for (let segundoIndex = 0; segundoIndex < numero; segundoIndex += 1) {
+      let divPaiLine = document.querySelectorAll('.line')[index];
+      let divFilho = document.createElement('div');
+      divPaiLine.appendChild(divFilho);
+      divFilho.classList.add('pixel');
+    }
+  }
+}
+
+createDivBorderPixel(5);
+
+function adicionaSelected(event) {
+  let selecionaCor = document.querySelectorAll('.color')
+  for (let i = 0; i < selecionaCor.length; i += 1) {
+    // className é uma constante, por isso vai manter color em todas. Pega o array inteiro
+    selecionaCor[i].className = 'color';
+  //  classList é uma lista de classes, pega o index do array
+    event.target.classList.add('selected');
+
+  }
+}
+chamaDivPai.addEventListener('click', adicionaSelected);
