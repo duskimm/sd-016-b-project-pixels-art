b2371a2c65850c7fbabec643a1b51ded4befcca6
victor_calidor <victor.calidor@gmail.com>
2021-09-11 21:31:46 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+
 // Criando os pixels
 
 const pixelBoard = document.getElementById('pixel-board');
@@ -5,4 +7,19 @@ for (let i = 0; i < 25; i += 1) {
   const pixel = document.createElement('div');
   pixel.className = 'pixel';
   pixelBoard.appendChild(pixel);
+}
+
+// Selecionar a cor
+const colorP = document.getElementById('color-palette');
+
+function selectColor(event) {
+  const selectedColor = document.getElementsByClassName('selected');
+  selectedColor[0].classList.remove('selected');
+  event.target.className += ' selected';
+  console.log(colorP);
+}
+
+for (let i = 0; i < colorP.children.length; i += 1) {
+  colorP.children[i].addEventListener('click', selectColor);
+  console.log(colorP.children[i]);
 }
