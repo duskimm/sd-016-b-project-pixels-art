29c77393f722487433daaea2c7a0201ffa7c5f63
Calixto <wellington1081@gmail.com>
2021-09-12 17:23:43 -0300

    Desafio 4 e 5

--- script.js
+++ script.js
@@ -50,3 +50,42 @@ function color4 (){
   div.appendChild(div1)
 }
 color4 ()
+
+
+function quadroPixel (){
+  let body = document.querySelector('body');
+  let div = document.createElement('div');
+  div.id = "pixel-board";
+  body.appendChild(div)
+}
+quadroPixel();
+
+// Desafio 4 e 5
+
+//  criei uma variável para guardar o elemento Pai, nesse caso pixel-board
+let elementoPixelBoard = document.querySelector('#pixel-board');
+
+function criarLinhas() {
+
+  // Primeiro criei as div das linhas 
+    for (let index = 0; index < 5; index += 1) {
+    let linhas = document.createElement('div');
+  
+    // Fiz a apende das linhas com a pixel-board 
+    elementoPixelBoard.appendChild(linhas);
+  
+    //  agora criei as div das colunass
+    for (let colunas = 0; colunas < 5; colunas += 1) {
+      let colunas = document.createElement('div');
+  
+      // aqui define a classe 'pixel' para todas as colunass
+      colunas.className = 'pixel';
+  
+      // aqui fiz o apende das linhas com as colunas, definindo as colunas como filhas das linhas
+      linhas.appendChild(colunas);
+    }
+  }
+}
+
+criarLinhas();
+
