80acd7d1f1dc69e0e29a8ebf031f1bb2e0d13900
Danilo-Couto <couto.danilo95@gmail.com>
2021-09-12 09:49:39 -0300

    requisito 8

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function inicialColor() {
   const blackColor = document.querySelector('.color');
   blackColor.classList.add('selected');
 };
-
+ 
 //----
 // este era meu codigo orignal, mas com ajuda do Hugo Daniel, cheguei ao debaixo.
 
@@ -24,9 +24,32 @@ function clickAlert() {
 //} */
 
 const colorArray = document.querySelector('#color-palette');
+
 colorArray.addEventListener('click', (event) => {
   for (let index = 0; index < colorArray.children.length; index += 1) {
     colorArray.children[index].classList.remove('selected');
   }
   event.target.classList.add('selected');
 });
+//-----
+
+const pixelBoardArray = document.getElementsByClassName('pixel');
+for (let index = 0; index < pixelBoardArray.length; index += 1) {
+  pixelBoardArray[index].onclick = function (event) {
+    const actualColor = document.querySelector('.selected');
+    const BGColorSelected = window.getComputedStyle(actualColor, null).getPropertyValue('background-color');
+    event.target.style.backgroundColor = BGColorSelected;
+  };
+}; 
+
+/* ou
+const pixelBoardArray = document.getElementsByClassName('pixel');
+for (let index = 0; index < pixelBoardArray.length; index += 1) {
+  pixelBoardArray[index].addEventListener ('click', btg);
+  
+    function btg (event) {
+      const actualColor = document.querySelector('.selected');
+      const BGColorSelected = window.getComputedStyle(actualColor, null).getPropertyValue('background-color');
+      event.target.style.backgroundColor = BGColorSelected;
+    };
+}; */
