e4f7c560de59b1400dfeb3e730f7c48215bfd1e8
OiEuSouOPi <rafasc866@gmail.com>
2021-09-13 14:42:12 -0300

    adicionando mais requisistos

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let quadradoSelecao=0;
-
 function criandoPixels(){
     let pixelBoard = document.getElementById("pixel-board")
     for (let coluna=0;coluna<25;coluna+=1) {
         let pixel = document.createElement("div");
-        pixel.classList.add('pixel');
+        pixel.setAttribute("id",coluna);
+        pixel.setAttribute("class",'pixel');
+        pixel.onclick = new Function('colorir(this)');
         pixelBoard.appendChild(pixel);
     }
 }
@@ -21,23 +22,19 @@ quadradoSelecao = 2;
 document.getElementById('square4').onclick=function(){
 quadradoSelecao = 3;
     }
-
-document.getElementsByClassName("pixel")[0].onclick = function(){
-    console.log(quadradoSelecao);   
+function colorir(a){
     switch (quadradoSelecao) {
-        case 0:     
-     this.style.background = "black";
-            
+        case 0:
+            document.getElementById(a.id).style.background = "black";
             break;
         case 1:
-        this.style.background ="#6FD6CB";
-    
+            document.getElementById(a.id).style.background ="#6FD6CB";
             break;
         case 2:
-        this.style.background = "#F0E948";
+            document.getElementById(a.id).style.background = "#F0E948";
             break;
         case 3:
-        this.style.background = "#4300F5";
+            document.getElementById(a.id).style.background = "#4300F5";
             break;
     }
 }
