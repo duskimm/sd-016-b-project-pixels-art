a354e1cc71b53c2758ecc43f7cdb2106a43809ed
Eglany Junior <contas.eglanyjr@gmail.com>
2021-09-13 10:02:53 -0400

    requesito bonus randomColor, correcao lint

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const numberPixels = 5;
 const linePixel = document.querySelectorAll('.line-pixel');
 const boardElements = document.getElementById('pixel-board');
 
-creatBoard(linePixel);
+function createPixel(classe) {
+  const createDiv = document.createElement('div');
+  createDiv.className = classe;
+  return createDiv;
+}
 
 function creatBoard(linePixel) {
   for (let index = 0; index < linePixel.length; index += 1) {
@@ -13,54 +17,71 @@ function creatBoard(linePixel) {
 
 function createLine(line) {
   for (let index = 1; index <= numberPixels; index += 1) {
-    let pixel = createPixel('pixel');
+    const pixel = createPixel('pixel');
     line.appendChild(pixel);
   }
 }
 
-function createPixel(classe) {
-  let pixel = document.createElement('div');
-  pixel.className = 'pixel';
-  return pixel;
+creatBoard(linePixel);
+
+//button clear board
+const clearboard = document.getElementById('clear-board');
+const divPixel = document.getElementsByClassName('pixel');
+
+function resetBoard() {
+  for (let index = 0; index < divPixel.length; index += 1) {
+    divPixel[index].style.backgroundColor = 'white';
+  }
 }
 
-//colors
-const colorPallete = document.getElementById('color-palette');
-const colorType = document.getElementsByClassName('color');
-colorType[0].style.backgroundColor = 'black';
-colorType[1].style.backgroundColor = 'rgb(154, 49, 29)';
-colorType[2].style.backgroundColor = 'rgb(247, 136, 14)';
-colorType[3].style.backgroundColor = 'rgb(77, 255, 0)';
+clearboard.addEventListener('click', resetBoard);
 
-//preset pincel onload (black)
-boardElements.addEventListener('click', function(event) {
-  if (divTarget === '' ) {
-    event.target.style.backgroundColor = 'black';
-  }
-});
+//colors
+function random() {
+  const randomPalette = Math.floor(Math.random()*16777215).toString(16);
+  const randomColor = `#${randomPalette}`
+  return randomColor
+}
+
+const colorPalette = document.getElementById('color-palette');
+const colorType = document.getElementsByClassName('color');
+colorType[0].classList = 'color selected';
+colorType[0].style.backgroundColor = 'black';
+colorType[2].style.backgroundColor = random();
+colorType[3].style.backgroundColor = random();
+colorType[1].style.backgroundColor = random();
 
 //pincel select and remove
 let divTarget = '';
-let pincelColor  = '';
-colorPallete.addEventListener('click', selectColor);
-boardElements.addEventListener('click', pincel);
-boardElements.addEventListener('dblclick', eraser)
-
+let pincelColor = '';
 
 function selectColor(event) {
   divTarget = event.target;
   pincelColor = divTarget.style.backgroundColor;
-  console.log(divTarget);
 }
 
 function pincel(event) {
   if (divTarget !== '') {
-    event.target.style.backgroundColor = pincelColor;
-    console.log(event.target);
+    const localPaint = event.target;
+    localPaint.style.backgroundColor = pincelColor;
   }
 }
 
 function eraser(event) {
-  event.target.style.backgroundColor = 'white';
-  console.log(event.target);
+  const erase = event.target;
+  erase.style.backgroundColor = 'white';
 }
+//preset pincel onload (black)
+function presetColor(event) {
+  if (divTarget === '') {
+    const onloadPaint = event.target;
+    onloadPaint.style.backgroundColor = 'black';
+  }
+}
+
+boardElements.addEventListener('click', presetColor);
+
+//eventos
+colorPalette.addEventListener('click', selectColor);
+boardElements.addEventListener('click', pincel);
+boardElements.addEventListener('dblclick', eraser);
