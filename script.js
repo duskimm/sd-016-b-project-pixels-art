29c4f0fb49a31ef15edc018cb8167c89b1833b56
Morallys <morallys.juan@gmail.com>
2021-09-13 15:51:15 -0300

    Organização de código

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-const mainContent = document.querySelector('.main-content');
+// const btnAndInput = document.querySelector('.main-content');
+const btnAndInput = document.querySelector('.btnAndInput');
 const divColorClass = document.getElementsByClassName('color');
 const pixelBox = document.getElementsByClassName('pixel');
 const getColor = document.querySelectorAll('.color');
-// const sectionColorPalette = document.querySelector('#color-palette');
+const sectionPixel = document.getElementById('pixel-board');
 let colorTabela = 'black';
 
+// Cria o pixel padrão
+
 function defaultSquare(boardStyle) {
-  const change = boardStyle;
-  change.style.border = '1px solid black';
-  change.style.width = '40px';
-  change.style.height = '40px';
-  change.style.margin = '0.01px';
-  change.style.padding = '15px';
-  change.style.display = 'inline-block';
-  change.style.backgroundColor = 'white';
+  const square = boardStyle;
+  square.style.border = '1px solid black';
+  square.style.width = '40px';
+  square.style.height = '40px';
+  square.style.margin = '1px';
+  square.style.padding = '15px';
+  square.style.display = 'inline-block';
+  square.style.backgroundColor = 'white';
 
   return boardStyle;
 }
+
+// Cria uma div genérica com Classe
 
 function createDiv(nameClass) {
   const genericDiv = document.createElement('div');
@@ -25,23 +30,18 @@ function createDiv(nameClass) {
   return genericDiv;
 }
 
-function createSectionId(nameId) {
+// Cria uma seção genérica com ID
+
+function createSectionClass(nameClass) {
   const genericSection = document.createElement('section');
-  genericSection.id = nameId;
+  genericSection.className = nameClass;
 
   return genericSection;
 }
 
 // Cria a paleta de cores
 
-// function createPaletteColors() {
-//   sectionColorPalette.appendChild(createDiv('color black selected'));
-//   sectionColorPalette.appendChild(createDiv('color red'));
-//   sectionColorPalette.appendChild(createDiv('color blue'));
-//   sectionColorPalette.appendChild(createDiv('color green'));
-// }
-
-// createPaletteColors();
+// Cria a cor aleatória
 
 function aleatoryColors() {
   const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -51,23 +51,28 @@ function aleatoryColors() {
 
 const colorPack = document.querySelectorAll('.color');
 
+// Seleciona a cor da tabela e chama a criação da cor aleatória
+
 function setColorPalette() {
   colorPack[0].style.backgroundColor = 'black';
   for (let index = 1; index < colorPack.length; index += 1) {
     colorPack[index].style.backgroundColor = aleatoryColors();
   }
-  // return q;
 }
 
 setColorPalette();
 
+// Cria o botão de limpar o quadro
+
 function buttonClear() {
   const createButtonClear = document.createElement('div');
   createButtonClear.id = 'clear-board';
-  mainContent.appendChild(createButtonClear).innerText = 'Limpar';
+  btnAndInput.appendChild(createButtonClear).innerText = 'Limpar';
 }
 
 buttonClear();
+
+// Cria o espaço para recuperar o tamanho que o usuário colocará
 
 function boardInput() {
   const newBoardInput = document.createElement('input');
@@ -77,17 +82,19 @@ function boardInput() {
   newBoardInput.placeholder = 'Insira aqui um valor entre 5 e 50';
   newBoardInput.type = 'number';
 
-  mainContent.appendChild(newBoardInput);
+  btnAndInput.appendChild(newBoardInput);
 
   return newBoardInput.value;
 }
 
 boardInput();
 
+// Cria botão para recuperar os dados do input
+
 function valueBoardBtn() {
   const generateBoard = document.createElement('div');
   generateBoard.id = 'generate-board';
-  mainContent.appendChild(generateBoard).innerText = 'VQV';
+  btnAndInput.appendChild(generateBoard).innerText = 'VQV';
 }
 
 valueBoardBtn();
@@ -96,22 +103,24 @@ let newUserCount = document.querySelector('#board-size').value;
 newUserCount = 5;
 const valueBtnBoard = document.querySelector('#generate-board');
 
-// monta o quadro
+// Seleciona a cor correta para a pintura do pixel
 
 function inputColor(e) {
   e.style.backgroundColor = colorTabela;
 }
 
+// Monta o quadro, criando as seções inicialmente e depois criando as divs e colocando dentro das seções
+
 function mountBoard(numberBoard) {
   for (let index = 0; index < numberBoard; index += 1) {
-    const sectionPixel = createSectionId('pixel-board');
-    mainContent.appendChild(sectionPixel);
+    const sectionBoardPixel = createSectionClass('line');
+    sectionPixel.appendChild(sectionBoardPixel);
   }
 
   for (let indexX = 0; indexX < numberBoard; indexX += 1) {
     for (let indexY = 0; indexY < numberBoard; indexY += 1) {
       const divPixel = createDiv('pixel');
-      const divFather = document.querySelectorAll('#pixel-board');
+      const divFather = document.querySelectorAll('.line');
       defaultSquare(divPixel);
       divFather[indexY].appendChild(divPixel);
     }
@@ -121,13 +130,14 @@ function mountBoard(numberBoard) {
 // exclui o quadro
 
 function unMountBoard() {
-  const board = document.querySelectorAll('#pixel-board');
+  const board = document.querySelectorAll('.line');
   for (let index = 0; index < board.length; index += 1) {
-    mainContent.removeChild(board[index]);
+    sectionPixel.removeChild(board[index]);
   }
 }
 
 // Insere evento a cada pixel e pinta cada um deles
+
 function insertEventSquare() {
   for (let index = 0; index < pixelBox.length; index += 1) {
     pixelBox[index].addEventListener('click', () => {
@@ -137,8 +147,8 @@ function insertEventSquare() {
   }
 }
 
-// testa valores < > e vazio, exclui tabela antiga e
-// monta a nova com o novo número do input
+// testa valores < > e vazio, exclui tabela antiga,
+// monta a nova com o novo número do input e insere novamente o evento nos pixels
 
 valueBtnBoard.addEventListener('click', () => {
   console.log('teste click newUserCount');
@@ -178,18 +188,18 @@ function changeClass(cor) {
 // busca as cores selecionadas
 
 function getColorPalette(a) {
-  if (a.target.className.includes('black')) {
+  if (a.target.className.includes('color1')) {
     colorTabela = colorPack[0].style.backgroundColor;
-    changeClass('black');
-  } else if (a.target.className.includes('red')) {
-    colorTabela = colorPack[1].style.backgroundColor; // https://eslint.org/docs/rules/prefer-destructuring
-    changeClass('red');
-  } else if (a.target.className.includes('blue')) {
+    changeClass('color1');
+  } else if (a.target.className.includes('color2')) {
+    colorTabela = colorPack[1].style.backgroundColor;
+    changeClass('color2');
+  } else if (a.target.className.includes('color3')) {
     colorTabela = colorPack[2].style.backgroundColor;
-    changeClass('blue');
-  } else if (a.target.className.includes('green')) {
+    changeClass('color3');
+  } else if (a.target.className.includes('color4')) {
     colorTabela = colorPack[3].style.backgroundColor;
-    changeClass('green');
+    changeClass('color4');
   }
   return colorTabela;
 }
