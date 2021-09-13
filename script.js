3dbf9ddcc6bc26a37cc08227d93d95e7ec3122d0
natashiaromanzini <natashiaromanzini1@gmail.com>
2021-09-13 19:21:21 -0300

    ta indo

---
+++ script.js
@@ -0,0 +1,6 @@
+let quadrados = document.getElementsByClassName ('pixel-board')
+function crialinhas (){
+for (let i=0;i<5;i++){
+let quadrados = document.createElement('div');
+quadrados.className = 'pixel'}
+}
