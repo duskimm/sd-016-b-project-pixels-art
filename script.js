8b5e57db53afc62f30a91c9251a69b5f6b2f099e
Nathan Nunes <nunesxd7@hotmail.com>
2021-09-13 02:32:28 -0300

    ate o onze com lint

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function grid() {
-  const baseGrid = 5;
-  const container = document.getElementById('pixel-board');
+let baseGrid = 5;
+const main = document.querySelector('main');
+const input = document.createElement('input');
+const VQVbutton = document.createElement('button');
+const container = document.getElementById('pixel-board');
+
+function grid(baseGrider) {
+  baseGrid = baseGrider;
+  if (baseGrid < 5) {
+    baseGrid = 5;
+  } else if (baseGrid > 50) {
+    baseGrid = 50;
+  }
   for (let i = 0; i < baseGrid; i += 1) {
     const rowDiv = document.createElement('div');
     rowDiv.className = 'gridRow';
@@ -12,6 +22,28 @@ function grid() {
     }
   }
 }
+
+function inputButton() {
+  input.type = 'number';
+  input.setAttribute('min', 1);
+  input.setAttribute('max', 50);
+  input.id = 'board-size';
+  VQVbutton.id = 'generate-board';
+  VQVbutton.innerHTML = 'VQV';
+  main.insertBefore(input, main.childNodes[5]);
+  main.insertBefore(VQVbutton, main.childNodes[6]);
+  VQVbutton.addEventListener('click', function inputTest() {
+    if (input.value === '') {
+      alert('Board inválido!');
+    }
+    const remove = document.querySelectorAll('.pixel');
+    for (let index = 0; index < remove.length; index += 1) {
+      remove[index].remove();
+    }
+    grid(input.value);
+  });
+}
+
 function select() {
   const pickedColor = document.querySelector('.color');
   pickedColor.classList.add('selected');
@@ -22,8 +54,8 @@ function select() {
     toPaint.target.classList.add('selected');
   });
 }
+
 function getColored() {
-  const container = document.getElementById('pixel-board');
   container.addEventListener('click', function paint(clickedPixel) {
     const currentColor = document.querySelector('.selected');
     const currentColors = window.getComputedStyle(currentColor).backgroundColor;
@@ -33,8 +65,8 @@ function getColored() {
     }
   });
 }
+
 function clean() {
-  const main = document.querySelector('main');
   const resetButton = document.createElement('button');
   main.insertBefore(resetButton, main.childNodes[4]);
   resetButton.id = ('clear-board');
@@ -50,9 +82,11 @@ function clean() {
     }
   });
 }
+
 window.onload = function start() {
-  grid();
   select();
   getColored();
   clean();
+  inputButton();
+  grid(baseGrid);
 };
