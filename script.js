72628bf61127721b18f1f72e26fe9d07e85db4ec
Renata <renatamagno@live.com>
2021-09-13 12:39:02 -0300

    Ajuste requisito 4 e ajustes css

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+// Função que adiociona e remove a class 'selected'
+
 const colors = document.getElementsByClassName('color');
 
-// Função que adiociona e remove a class 'selected'
 function selectColor(event) {
   for (let i = 0; i < colors.length; i += 1) {
     colors[i].classList.remove('selected');
@@ -15,7 +16,7 @@ for (let index = 0; index < colors.length; index += 1) {
 // Função para pintar pixels
 // referencias: https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
 // https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/getPropertyValue
-// Se o local do click contém a class "pixel", recupera o item que contém a class "selected" e add na const item, recupera a background-color do item, add ao pixel clicado. 
+// Se o local do click contém a class "pixel", recupera o item que contém a class "selected" e add na const item, recupera a background-color do item, add ao pixel clicado.
 
 function paintPixel(event) {
   if (event.target.classList.contains('pixel')) {
@@ -28,7 +29,8 @@ function paintPixel(event) {
 
 document.addEventListener('click', paintPixel);
 
-//Função para limpar board
+// Função para limpar board
+
 const clear = document.querySelector('#clear-board');
 function clearBoard() {
   const pixels = document.getElementsByClassName('pixel');
