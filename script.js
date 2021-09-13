7b7b533ad46454f5066a2bcfd56d81a3af2e172d
AirtonL <junynnhoh@gmail.com>
2021-09-13 14:03:52 -0300

    terminando requisitos bonus

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let linhas = 5;
+
 function criaHeader() {
   const titulo = document.querySelector('header');
   const criaH1 = document.createElement('h1');
@@ -8,8 +10,17 @@ function criaHeader() {
 criaHeader();
 
 function criaCores() {
-  const cores = ['black', 'red', 'pink', 'green'];
+  const cores = ['black'];
   const quadrados = document.querySelectorAll('.color');
+  let value1 = 0;
+  let value2 = 0;
+  let value3 = 0;
+  for (let index1 = 0; index1 < 3; index1 += 1) {
+    value1 = Math.floor(Math.random() * (255));
+    value2 = Math.floor(Math.random() * (255));
+    value3 = Math.floor(Math.random() * (255));
+    cores.push(`rgb(${value1}, ${value2}, ${value3})`);
+  }
   for (let index = 0; index < cores.length; index += 1) {
     quadrados[index].style.backgroundColor = cores[index];
   }
@@ -18,11 +29,11 @@ criaCores();
 
 function criaQuadro() {
   const localizaDiv = document.querySelector('#pixel-board');
-  for (let index = 0; index < 5; index += 1) {
+  for (let index = 0; index < linhas; index += 1) {
     const criaUl = document.createElement('ul');
     criaUl.className = 'uls';
     localizaDiv.appendChild(criaUl);
-    for (let index2 = 0; index2 < 5; index2 += 1) {
+    for (let index2 = 0; index2 < linhas; index2 += 1) {
       const localizaUl = document.querySelectorAll('.uls')[index];
       const criaLi = document.createElement('li');
       criaLi.className = 'pixel';
@@ -39,34 +50,65 @@ function trocaClass(event) {
 }
 
 const black = document.getElementById('black');
-const red = document.getElementById('red');
-const pink = document.getElementById('pink');
-const green = document.getElementById('green');
+const color2 = document.getElementById('color2');
+const color3 = document.getElementById('color3');
+const color4 = document.getElementById('color4');
 
 black.addEventListener('click', trocaClass);
-red.addEventListener('click', trocaClass);
-pink.addEventListener('click', trocaClass);
-green.addEventListener('click', trocaClass);
+color2.addEventListener('click', trocaClass);
+color3.addEventListener('click', trocaClass);
+color4.addEventListener('click', trocaClass);
 
 function trocaCor() {
   const pixel = document.getElementById('pixel-board');
   pixel.addEventListener('click', (event) => {
     if (event.target.className === 'pixel') {
-      const corAtual = document.querySelector('.selected').id;
+      const corAtual = document.querySelector('.selected');
       const corSelecionada = event.target;
-      corSelecionada.style.backgroundColor = corAtual;
+      corSelecionada.style.backgroundColor = corAtual.style.backgroundColor;
     }
   });
 }
 trocaCor();
 
 function limpaQuadro() {
-  const pegaButton = document.getElementById('clear-board');
-  const pegaPixels = document.querySelectorAll('.pixel');
-  pegaButton.addEventListener('click', function () {
+  document.getElementById('clear-board').addEventListener('click', () => {
+    const pegaPixels = document.querySelectorAll('.pixel');
     for (let index = 0; index < pegaPixels.length; index += 1) {
       pegaPixels[index].style.backgroundColor = 'white';
     }
   });
 }
 limpaQuadro();
+
+function removeClasse(classe) {
+  let remove = document.getElementsByClassName(classe);
+  for (let index = 0; index < remove.length; index += 0) {
+    remove[0].remove();
+    remove = document.getElementsByClassName(classe);
+  }
+}
+
+function inputQuadro() {
+  const localizaInput = document.getElementById('board-size');
+  document.getElementById('generate-board').addEventListener('click', () => {
+    let tamanho = localizaInput.value;
+    if (tamanho === ''){
+      alert('Board inválido!');
+    } else if (tamanho > 50) {
+      tamanho = 50;
+    }
+    else if(tamanho < 5){
+      tamanho = 5;
+    }
+    if (tamanho >= 5 && tamanho <= 50) {
+      linhas = tamanho;
+      removeClasse('uls');
+      criaQuadro();
+    }
+  });
+}
+inputQuadro();
+
+let teste = document.querySelector('.selected').style.backgroundColor;
+console.log(teste);
