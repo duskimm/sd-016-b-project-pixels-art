bf276e5d91e6cc73e8552f0c057f7f21861587e9
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-09-13 10:55:20 -0300

    modifica para lint

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 // Cria divs com classe color com cores dinamicas
 //  Adicione à página uma paleta contendo quatro cores distintas.
-
-
-let chamaDivPai = document.getElementById('color-palette');
-
+const chamaDivPai = document.getElementById('color-palette');
 function colorPalette(cor) {
-  let primeiroFilhoDaPaletaRosa = document.createElement('div');
+  const primeiroFilhoDaPaletaRosa = document.createElement('div');
   primeiroFilhoDaPaletaRosa.className = 'color';
   primeiroFilhoDaPaletaRosa.style.backgroundColor = cor;
   chamaDivPai.appendChild(primeiroFilhoDaPaletaRosa);
@@ -21,18 +18,18 @@ colorPalette('green');
 //  Adicione à página um quadro de pixels, com 25 pixels.
 
 function createDivBorderPixel(numero) {
-  let divPixel = document.querySelector('#pixel-board');
+  const divPixel = document.querySelector('#pixel-board');
   // for pra criar as linhas do quadro pixel.
   for (let index = 0; index < numero; index += 1) {
-    let divLine = document.createElement('div');
+    const divLine = document.createElement('div');
     divPixel.appendChild(divLine);
     divLine.classList.add('line');
 
     // for pra criar as divs dentro das divline
 
     for (let segundoIndex = 0; segundoIndex < numero; segundoIndex += 1) {
-      let divPaiLine = document.querySelectorAll('.line')[index];
-      let divFilho = document.createElement('div');
+      const divPaiLine = document.querySelectorAll('.line')[index];
+      const divFilho = document.createElement('div');
       divPaiLine.appendChild(divFilho);
       divFilho.classList.add('pixel');
     }
@@ -44,23 +41,22 @@ createDivBorderPixel(5);
 // Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
 
 function adicionaSelected(event) {
-  let selecionaCor = document.querySelectorAll('.color')
+  const selecionaCor = document.querySelectorAll('.color')
   for (let i = 0; i < selecionaCor.length; i += 1) {
     // className é uma constante, por isso vai manter color em todas. Pega o array inteiro
     selecionaCor[i].className = 'color';
     //  classList é uma lista de classes, pega o index do array
     event.target.classList.add('selected');
-
   }
 }
 chamaDivPai.addEventListener('click', adicionaSelected);
 
 //  Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.
-let classePixelBoard = document.querySelector('#pixel-board');
+const classePixelBoard = document.querySelector('#pixel-board');
 function pintaPixel(event) {
   let selecionarCor = document.querySelector('.selected');
-  let cor = selecionarCor.style.backgroundColor;
-  let pixel = document.querySelectorAll('.pixel');
+  const cor = selecionarCor.style.backgroundColor;
+  const pixel = document.querySelectorAll('.pixel');
   for (let i = 0; i < pixel.length; i += 1) {
     event.target.style.backgroundColor = cor;
   }
@@ -69,12 +65,11 @@ classePixelBoard.addEventListener('click', pintaPixel);
 
 // fiz o botão pelo html e então inseri o evento por aqui
 
-let button = document.querySelector('#clear-board');
+const button = document.querySelector('#clear-board');
 button.innerText = 'Limpar';
 button.addEventListener('click', function () {
-  let pixelClass = document.querySelectorAll('.pixel');
+  const pixelClass = document.querySelectorAll('.pixel');
   for (let i = 0; i < pixelClass.length; i += 1) {
     pixelClass[i].style.backgroundColor = 'white';
   }
-
 });
