1cb01cc34b5a13f73166c925b1d3cd77d251073d
Victor <victor.hugofla@hotmail.com>
2021-09-13 11:45:53 -0300

    Finaliza requisito 12

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const colors = document.querySelectorAll('.color');
 const pixelBoard = document.getElementById('pixel-board');
 const button = document.getElementById('clear-board');
 const generateBoard = document.getElementById('generate-board');
+const backgroundColor = 'background-color';
 
 function addClassName() {
   for (let index = 0; index < colors.length; index += 1) {
@@ -18,15 +19,13 @@ function addClassName() {
 addClassName();
 
 function paintingPixels(e) {
-  if (e.target && e.target.className === 'pixel') {
+  if (e.target.className === 'pixel') {
     const checkClass = document.querySelector('.selected');
-    e.target.style.backgroundColor = window.getComputedStyle(checkClass, null).getPropertyValue('background-color');
+    const getStyle = window.getComputedStyle(checkClass, null).getPropertyValue(backgroundColor);
+    e.target.style.backgroundColor = getStyle;
   }
 }
 pixelBoard.addEventListener('click', paintingPixels);
-
-// Aprendi a usar o getComputedStyle nesse site: https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp para checar o valor do background da classe selected.
-// Aprendi a usar o event delegation nesse site: https://davidwalsh.name/event-delegate para identificar o elemento que foi clicado dentro do elemento pai.
 
 function cleaningFrame() {
   const pixels = document.getElementsByClassName('pixel');
@@ -64,5 +63,33 @@ function addPixels() {
   generateBoardSize(inputValue);
 }
 
+function randomColors() {
+  const color = document.getElementsByClassName('color');
+  const storeColors = ['#9C528B', '#51CB20', '#2708A0',
+    '#59656F', '#51A3A3', '#DD403A', '#FFB627', '#02A9EA', '#9E643C'];
+  for (let index = 1; index < color.length; index += 1) {
+    const sortN = Math.floor(Math.random() * (Math.floor(8) - Math.ceil(0))) + 0;
+    color[index].style.backgroundColor = storeColors[sortN];
+  }
+  const cor1 = window.getComputedStyle(color[1], null).getPropertyValue(backgroundColor);
+  const cor2 = window.getComputedStyle(color[2], null).getPropertyValue(backgroundColor);
+  const cor3 = window.getComputedStyle(color[3], null).getPropertyValue(backgroundColor);
+  if (cor1 === cor2) {
+    color[1].style.backgroundColor = '#E9F7CA';
+  } if (cor2 === cor3) {
+    color[2].style.backgroundColor = '#F694C1';
+  } if (cor3 === cor1) {
+    color[3].style.backgroundColor = '#34623F';
+  }
+}
+randomColors();
+
 generateBoard.addEventListener('click', addPixels);
 generateBoardSize(5);
+
+/* Referências:
+getComputedStyles - https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
+event delegation - https://davidwalsh.name/event-delegate
+Math.random - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+Math.floor - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+*/
