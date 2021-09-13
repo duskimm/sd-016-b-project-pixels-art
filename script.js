53f08e90a63bfd1af785323b0253e071a94062a2
Diego-AS-Mendes <dasm021@msn.com>
2021-09-13 01:15:42 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const caixaDePixels = document.getElementById('pixel-board');
 
-function caixaPixel(nPixels, quadrados) {
+function caixaPixel(nPixels) {
   for (let i = 0; i < nPixels; i += 1) {
     const linhaDePixel = document.createElement('div');
-    for (let j = 0; j < quadrados; j += 1) {
+    for (let j = 0; j < nPixels; j += 1) {
       const quadradoDePixel = document.createElement('div');
-      quadradoDePixel.className = 'pixel';
+      quadradoDePixel.classList.add('pixel');
       linhaDePixel.appendChild(quadradoDePixel);
     }
     caixaDePixels.appendChild(linhaDePixel);
   }
 }
-caixaPixel(5, 5);
+caixaPixel(5);
 
-function addRemoveClass(event) { 
+const paleta = document.getElementById('color-palette');
+function addRemoveClass(event) {
   const pegaCor = document.querySelector('.selected');
+  console.log(pegaCor);
   pegaCor.classList.remove('selected');
   event.target.classList.add('selected');
 }
+paleta.addEventListener('click', addRemoveClass);
 
-preto.addEventListener('click', addRemoveClass);
-cinza.addEventListener('click', addRemoveClass);
-vermelho.addEventListener('click', addRemoveClass);
-roxo.addEventListener('click', addRemoveClass);
+const colorePixel = document.querySelectorAll('.pixel');
+for (let i = 0; i < colorePixel.length; i += 1) {
+  colorePixel[i].addEventListener('click', (addColor) => {
+    const pegaCor = document.querySelector('.selected');
+    const pintaCor = addColor.target;
+    pintaCor.style.backgroundColor = window.getComputedStyle(pegaCor).backgroundColor;
+  });
+}
