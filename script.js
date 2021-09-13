70272bd52766bfbbb0a778e70626fbc9ccf4be8b
Victor <victor.hugofla@hotmail.com>
2021-09-13 11:57:02 -0300

    Corrige lint e finaliza o projeto

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const backgroundColor = 'background-color';
 
 function addClassName() {
   for (let index = 0; index < colors.length; index += 1) {
-    function add(event) {
+    const add = (event) => {
       const selected = event.target;
       for (let i = 0; i < colors.length; i += 1) {
         colors[i].classList.remove('selected');
       }
       selected.classList.add('selected');
-    }
+    };
     colors[index].addEventListener('click', add);
   }
 }
