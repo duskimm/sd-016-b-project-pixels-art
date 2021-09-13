5223694188c4e767754bb49287306ab522bef129
Cleitonsub <cleitonrmacedo@gmail.com>
2021-09-13 18:23:19 -0300

    Setimo, oitavo e nono requisito concluido

--- script.js
+++ script.js
@@ -1,43 +1,32 @@
-window.onload = function() {
-
-  let box = document.createElement("div");
-
-
-
-
-
-
-
-
-
-
-  
-
-
-  function createBox(className) {
-    let box = document.createElement("div");
-    box.className = className;
-    return box;
+function addClassSelected(event) {
+  const classSelected = document.querySelector('.selected');
+  const eventTarget = event.target;
+  if (eventTarget.classList.contains('selected') === false
+  && eventTarget.classList.contains('color') === true) {
+    classSelected.classList.remove('selected');
+    event.target.classList.add('selected');
   }
-
-  function fillLine(divLine) {
-    for (let lineColumn = 1; lineColumn <= basePyramid; lineColumn += 1) {
-      if(lineColumn >= controlLeft && lineColumn <= controlRight) {
-        divLine.appendChild(createBox("box"));
-      } else {
-        divLine.appendChild(createBox("box-empty"));
-      }
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-
 }
+
+function changeColor(event) {
+  const eventTarget = event.target;
+  const colorSelected = document.querySelector('.selected');
+  const bgColor = window.getComputedStyle(colorSelected, null).getPropertyValue('background-color');
+  eventTarget.style.backgroundColor = bgColor;
+}
+function cleanPixels() {
+  const pixels = document.getElementsByClassName('pixel');
+  const pixelBoard = document.getElementById('pixel-board');
+  pixelBoard.style.backgroundColor = 'white';
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+}
+window.onload = function page() {
+  const pixelColor = document.querySelector('#pixel-board');
+  pixelColor.addEventListener('click', changeColor);
+  const colorSelect = document.querySelector('#color-palette');
+  colorSelect.addEventListener('click', addClassSelected);
+  const clearBtn = document.querySelector('#clear-board');
+  clearBtn.addEventListener('click', cleanPixels);
+};
