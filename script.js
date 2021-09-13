5c59e5238c570ae000231cc1e72921e383006dd5
Everton-Curti <evercurti@gmail.com>
2021-09-12 22:24:58 -0300

    exercicio 8 resolvido

--- script.js
+++ script.js
@@ -14,22 +14,30 @@ for (let index = 0; index < coresIndividuais; index += 1){
     capturarSection.appendChild(criarDivsPaleta);
 }
 
-let configurarCoresPaletas = document.querySelectorAll('.color');
-configurarCoresPaletas[0].style.backgroundColor = 'black';
-configurarCoresPaletas[1].style.backgroundColor = 'red';
-configurarCoresPaletas[2].style.backgroundColor = 'blue';
-configurarCoresPaletas[3].style.backgroundColor = 'green';
-
-let capturarSectionQuadro = document.querySelector('#pixel-board');
-let elementosQuadro = 25;
-for (let index = 0; index < elementosQuadro; index +=1){
-    let criarDivsQuadro = elementosQuadro;
-    criarDivsQuadro = document.createElement('div');
-    criarDivsQuadro.className = 'pixel';
-    capturarSectionQuadro.appendChild(criarDivsQuadro);      
+function colorirPaleta (){
+    let configurarCoresPaletas = document.querySelectorAll('.color');
+    configurarCoresPaletas[0].style.backgroundColor = 'black';
+    configurarCoresPaletas[1].style.backgroundColor = 'red';
+    configurarCoresPaletas[2].style.backgroundColor = 'blue';
+    configurarCoresPaletas[3].style.backgroundColor = 'green';
 }
+colorirPaleta();
+
+function enquandrarPixels(){
+    let capturarSectionQuadro = document.querySelector('#pixel-board');
+    let elementosQuadro = 25;
+    for (let index = 0; index < elementosQuadro; index +=1){
+        let criarDivsQuadro = elementosQuadro;
+        criarDivsQuadro = document.createElement('div');
+        criarDivsQuadro.className = 'pixel';
+        capturarSectionQuadro.appendChild(criarDivsQuadro);      
+    }
+}
+enquandrarPixels();
+
 
 let buscarCor = 'black';
+let configurarCoresPaletas = document.querySelectorAll('.color');
 for (let index = 0; index < configurarCoresPaletas.length; index += 1){
     if (configurarCoresPaletas[index].style.backgroundColor === buscarCor){
         configurarCoresPaletas[index].classList.add('selected');
@@ -43,4 +51,21 @@ function configurarSelecao(event){
 }
 for (let index = 0; index < configurarCoresPaletas.length; index += 1){
     configurarCoresPaletas[index].addEventListener('click', configurarSelecao);
+}
+
+let capturarSelect = document.querySelector('.selected');
+let capturarPixels = document.querySelectorAll('.pixel');
+let capturarIdColorPalette = document.querySelector('#color-palette');
+for (let index = 0; index < capturarPixels.length; index += 1){
+    capturarPixels[index].addEventListener('click', function(){
+        if (capturarIdColorPalette.children[0].classList.contains('selected')){
+            capturarPixels[index].style.backgroundColor = 'black';
+        } else if (capturarIdColorPalette.children[1].classList.contains('selected')){
+            capturarPixels[index].style.backgroundColor = 'red';
+        } else if (capturarIdColorPalette.children[2].classList.contains('selected')) {
+            capturarPixels[index].style.backgroundColor = 'blue';
+        } else if (capturarIdColorPalette.children[3].classList.contains('selected')) {
+            capturarPixels[index].style.backgroundColor = 'green';
+        }
+    });
 }
