b6eef74e7424e21b453e36d14d39b9b49e64bd19
OiEuSouOPi <rafasc866@gmail.com>
2021-09-13 16:10:51 -0300

    adicionadno requisitos finais

--- script.js
+++ script.js
@@ -11,30 +11,34 @@ function criandoPixels(){
 }
 criandoPixels();
 document.getElementById('square1').onclick=function(){
+this.classList.add("selected");
 quadradoSelecao = 0;
 }
 document.getElementById('square2').onclick=function(){
+this.classList.add("selected");
 quadradoSelecao = 1;
     }
 document.getElementById('square3').onclick=function(){
+this.classList.add("selected");
 quadradoSelecao = 2;
     }    
 document.getElementById('square4').onclick=function(){
+this.classList.add("selected");
 quadradoSelecao = 3;
     }
 function colorir(a){
     switch (quadradoSelecao) {
         case 0:
-            document.getElementById(a.id).style.background = "black";
+            document.getElementById(a.id).style.background ="rgb(0, 0, 0)";
             break;
         case 1:
-            document.getElementById(a.id).style.background ="#6FD6CB";
+            document.getElementById(a.id).style.background ="rgb(111, 214, 203)";
             break;
         case 2:
-            document.getElementById(a.id).style.background = "#F0E948";
+            document.getElementById(a.id).style.background ="rgb(240, 233, 72)";
             break;
         case 3:
-            document.getElementById(a.id).style.background = "#4300F5";
+            document.getElementById(a.id).style.background ="rgb(67, 0, 245)";
             break;
     }
 }
