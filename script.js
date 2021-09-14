09f221ea49c0dff39a44360a3e7d3d435104d049
AmandaSottani <amandasottani@gmail.com>
2021-09-14 18:12:57 -0300

    correção

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ window.onload = function beginning (){
   document.getElementsByClassName("color")[0].classList.add("selected");
   
 	let colorList = document.getElementsByClassName("color");
-	for(i=0; i < colorList.length; i++) {
-	  //document.getElementsByClassName("color")[i].addEventListener("click", changeColorSelected);
-		colorList[i].addEventListener("click", changeColorSelected);
+	for (let i=0; i < colorList.length; i++) {
+	  colorList[i].addEventListener("click", changeColorSelected);
 	}
   function changeColorSelected(event){
 		const lastColor = document.querySelector(".selected");
