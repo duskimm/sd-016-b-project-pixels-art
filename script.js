ba62e5f1ad0a10d917311da0ffc065e208edcc24
Danielle Silva <danielle.luisasilva@gmail.com>
2021-09-13 23:13:20 -0300

    adiciona requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 // Exercicio 4
+const number = 5;
 
-let divPai = document.getElementById('pixel-board');
+const divPai = document.getElementById('pixel-board');
 
-function linha (numero) {
-    let criaLinha = document.createElement('div');
-    criaLinha.className = 'linhaPixel';
-    divPai.appendChild(criaLinha);
-    
-    for(let index = 0; index < numero; index += 1) {
-        let criaPIxel = document.createElement('div');
-        criaPIxel.className = 'pixel';
-        criaLinha.appendChild(criaPIxel);
+function matriz (numero) {
+  let criaLinha = null;
+  for(let index = 0; index < numero * numero; index += 1) {
+    if(index % numero === 0) {
+      criaLinha = document.createElement('div');
+      criaLinha.className = 'linhaPixel';
+      divPai.appendChild(criaLinha);
     }
-}
-linha(5);
+    let criaPIxel = document.createElement('div');
+    criaPIxel.className = 'pixel';
+    criaLinha.appendChild(criaPIxel);
+  }
 
-for (let index = 0; index < 4; index += 1){
-    linha(5);
 }
+matriz(number);
+
+
 
 // Requisito 6: adiciona classe 'selected' para cor preta
 window.addEventListener('load', function() {
-    let corPreta = document.querySelector('#black');
-    corPreta.classList.add('selected');
+  let corPreta = document.querySelector('#black');
+  corPreta.classList.add('selected');
 });
 
 // Requisito 7: adicona classe 'selected' para as outras cores
@@ -33,10 +35,10 @@ let idGreen = document.querySelector('#green');
 let idBlue = document.querySelector('#blue');
 
 function mudaClasseSelected (event) {
-    let classSelected = document.querySelector('.selected');
+  let classSelected = document.querySelector('.selected');
 
-    classSelected.classList.remove('selected');
-    event.target.classList.add('selected');
+  classSelected.classList.remove('selected');
+  event.target.classList.add('selected');
 }
 
 idBlack.addEventListener('click', mudaClasseSelected);
@@ -49,22 +51,48 @@ idBlue.addEventListener('click', mudaClasseSelected);
 // Requisito 8: pinta os pixels
 
 function corPixel(event) {
-    if (event.target.classList.contains('pixel')) {
-        let classSelected = document.querySelector('.selected');
-        let corDeFundo = window.getComputedStyle(classSelected, null).getPropertyValue('background-color');
-        let pixel = event.target;
-        pixel.style.backgroundColor = corDeFundo;
-    }
+  if (event.target.classList.contains('pixel')) {
+    const classSelected = document.querySelector('.selected');
+    const corDeFundo = window.getComputedStyle(classSelected, null).getPropertyValue('background-color');
+    const pixel = event.target;
+    pixel.style.backgroundColor = corDeFundo;
+  }
 }
 document.addEventListener('click', corPixel);
 
 // Requisito 9: botão para limpar o quadro
 
-let botaoLimpar = document.querySelector('#clear-board');
-let pixels = document.querySelectorAll('.pixel');
+const botaoLimpar = document.querySelector('#clear-board');
 
-botaoLimpar.addEventListener("click", function(){
-    for (let index = 0; index < pixels.length; index += 1){
-        pixels[index].style.backgroundColor = 'white';
-    }
-})
+botaoLimpar.addEventListener("click", function() {
+  const pixels = document.querySelectorAll('.pixel');
+
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
+  }
+});
+
+// Requisito 10
+
+// botão VQV
+const botaoVQV = document.getElementById('generate-board');
+const input = document.getElementById('board-size');
+
+botaoVQV.addEventListener('click', function() {
+
+  if (input.value === '') {
+    alert('Board inválido!');
+    return;
+  }
+
+  if (input.value < 5) {
+    input.value = 5;
+  }
+  if (input.value > 50) {
+    input.value = 50;
+  }
+
+  divPai.innerHTML = '';
+  matriz(input.value);
+
+});
