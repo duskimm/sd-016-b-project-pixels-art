74e2b9b3bbc7378766012b998a1b7a277430dc8d
carrers <carrersimoes@gmail.com>
2021-09-14 18:51:39 -0300

    requisito 7

---
+++ script.js
@@ -0,0 +1,15 @@
+const black = document.querySelector('.color1');
+black.classList.add = '.selected'
+
+const selectColor = document.getElementById('#color');
+function selectedColor(event) {
+
+    for (let index = 0; index < selectColor.length; index += 1) {
+
+        selectedColor[i].classList.remove('.selected');
+    event.target.classList.add('.selected');
+}
+
+    selectColor[index].addEventListener('click', selectColor);
+}
+
