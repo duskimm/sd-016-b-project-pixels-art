69778f9365670efcd5b5303c927ee1228e437b57
Mariana Saraiva <marianascm8@gmail.com>
2021-09-14 15:05:44 -0300

    Ajuste nos itens do lint

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let number = 5;
 // criar div para armazenar as cores do projeto, com classe "color" e ainda colocar a cor de forma dinâmica.
 function createColorsPallete(color) {
   const palleteColor = document.getElementById('color-palette');
-  let element = document.createElement('span');
+  const element = document.createElement('span');
   element.classList = 'color';
   element.style.backgroundColor = color;
   palleteColor.appendChild(element);
-};
+}
 createColorsPallete('black');
 createColorsPallete('red');
 createColorsPallete('orange');
@@ -15,15 +15,15 @@ createColorsPallete('yellow');
 
 // selecionando a cor preta do span com a classe selected
 function colorBlack() {
-  let colorFixed = document.getElementsByTagName('span')[0];
+  const colorFixed = document.getElementsByTagName('span')[0];
   colorFixed.className = 'color selected';
 }
 colorBlack();
 
-//criar a div com id "container"
+// criar a div com id "container"
 function createDivContainer() {
   const elementFather = document.querySelector('main');
-  let createDiv = document.createElement('div');
+  const createDiv = document.createElement('div');
   createDiv.id = 'container';
   elementFather.appendChild(createDiv);
 }
@@ -32,27 +32,27 @@ createDivContainer();
 // criar o Botão Clear
 function createButtonClear() {
   const elementFather = document.querySelector('#container');
-  let createButton = document.createElement('button');
+  const createButton = document.createElement('button');
   createButton.id = 'clear-board';
   createButton.innerText = 'Limpar';
   elementFather.appendChild(createButton);
 }
-createButtonClear()
+createButtonClear();
 
 // limpar o quadro pixel ao clicar no button
-let clearPixels = document.getElementById('clear-board');
+const clearPixels = document.getElementById('clear-board');
 function clearBoarder() {
-  let clearDivs = document.getElementsByClassName('pixel');
+  const clearDivs = document.getElementsByClassName('pixel');
   for (let index = 0; index < clearDivs.length; index += 1) {
     clearDivs[index].style.backgroundColor = 'white';
   }
 }
 clearPixels.addEventListener('click', clearBoarder);
 
-//criar a input com id "board-size"
+// criar a input com id "board-size"
 function createInputText() {
   const elementFather = document.querySelector('#container');
-  let createInput = document.createElement('input');
+  const createInput = document.createElement('input');
   createInput.id = 'board-size';
   createInput.placeholder = 'Adicione um número';
   createInput.type = 'number';
@@ -64,29 +64,21 @@ createInputText();
 // criar button com id 'button'
 function createButtonVQV() {
   const elementFather = document.querySelector('#container');
-  let createButton = document.createElement('button');
+  const createButton = document.createElement('button');
   createButton.id = 'generate-board';
   createButton.innerText = 'VQV';
   elementFather.appendChild(createButton);
 }
 createButtonVQV();
 
-
-
-// pintar os pixels de preto ao carregar a página
-function colorBlackReload() {
-  let pixels = document.getElementsByClassName('pixel');
-  pixels.style.backgroundColor = 'black'
-}
-
-//criar a div com id "pixel-board'
+// criar a div com id "pixel-board'
 function createDivFathetBoardPixel() {
   const elementFather = document.querySelector('main');
-  let createDiv = document.createElement('div');
+  const createDiv = document.createElement('div');
   createDiv.id = 'pixel-board';
   elementFather.appendChild(createDiv);
 }
-createDivFathetBoardPixel()
+createDivFathetBoardPixel();
 
 // criar quadro Pixel com 5 elementos de largura e 5 elementos de altura. Fazer a lógica do quadrado de asteriscos.
 // Auxílio Airton Lopes para implementar a div line
@@ -94,13 +86,13 @@ function createboardPixel(number) {
   const boardPixel = document.querySelector('#pixel-board');
   // for para criar as linhas do Quadro Pixel.
   for (let index = 0; index < number; index += 1) {
-    let div = document.createElement('div');
+    const div = document.createElement('div');
     boardPixel.appendChild(div);
     div.className = 'line';
     // for para criar as divs dentro da anterior.
     for (let index1 = 0; index1 < number; index1 += 1) {
-      let divPai = document.querySelectorAll('.line')[index];
-      let element = document.createElement('div');
+      const divPai = document.querySelectorAll('.line')[index];
+      const element = document.createElement('div');
       element.className = 'pixel';
       divPai.appendChild(element);
     }
@@ -110,12 +102,12 @@ createboardPixel(number);
 
 // remover o selected de onde estava:
 // Auxilio do Luis Gustavo no segundo for.
-let color = document.querySelectorAll('.color');
+const color = document.querySelectorAll('.color');
 function alteraClass(event) {
   for (let index = 0; index < color.length; index += 1) {
     color[index].classList.remove('selected');
   }
-  //seleciona o evento para iniciar ao ser clicado. toggle: alterna as classes.
+  // seleciona o evento para iniciar ao ser clicado. toggle: alterna as classes.
   event.target.classList.toggle('selected');
 }
 
@@ -126,9 +118,7 @@ for (let index = 0; index < color.length; index += 1) {
 // selecionar cor e pintar os pixels ao clicar. Ajuda questão 10 do calendário.
 // auxilio grupo de estudos.
 function paintSelected() {
-  let paintColorSelected = document.querySelector('.selected');
-  //console.log(paintColorSelected.style.backgroundColor);
-
+  const paintColorSelected = document.querySelector('.selected');
   paintColorSelected.addEventListener('click', function (event) {
     event.target.style.backgroundColor;
   });
@@ -137,44 +127,43 @@ paintSelected();
 
 function printColor() {
   // Auxilio Grupo de estudo sabado noite: Luis Wanderson.
-  let pixels = document.getElementsByClassName('pixel');
+  const pixels = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixels.length; index += 1) {
     pixels[index].addEventListener('click', function (event) {
-      let paintColorSelected = document.querySelector('.selected');
+      const paintColorSelected = document.querySelector('.selected');
       event.target.style.backgroundColor = paintColorSelected.style.backgroundColor;
     });
   }
 }
 printColor();
 
-// criar funcao para recuperar o valor do input ao clicar o botao com id generate-board (VQV) 
+// criar funcao para recuperar o valor do input ao clicar o botao com id generate-board (VQV)
 // auxilio do Gabriel Silvestre, Victor Hugor Baum, Airton Lopes.
 // pesquisa no site: https://www.w3schools.com/js/tryit.asp?filename=tryjs_form_elements
 function valueInput() {
-  let valueInput = document.querySelector('#board-size');
-  let buttonValue = document.querySelector('#generate-board');
-  let pixelBoard = document.querySelector('#pixel-board');
+  const valueInput = document.querySelector('#board-size');
+  const buttonValue = document.querySelector('#generate-board');
+  const pixelBoard = document.querySelector('#pixel-board');
   buttonValue.addEventListener('click', function () {
-    let numberInput = valueInput.value;
+    const numberInput = valueInput.value;
     number = numberInput;
     if (numberInput === '') {
-      alert('Board inválido!')
+      alert('Board inválido!');
     } else {
       // utilizando o innerHTML para remover o quadro pixel anterior.
       pixelBoard.innerHTML = '';
       createboardPixel(number);
       printColor();
     }
-
     if (numberInput < 5) {
       pixelBoard.innerHTML = '';
       createboardPixel(5);
       printColor();
-    } else if (numberInput > 50){
+    } else if (numberInput > 50) {
       pixelBoard.innerHTML = '';
       createboardPixel(50);
       printColor();
     }
-  })
+  });
 }
 valueInput();
