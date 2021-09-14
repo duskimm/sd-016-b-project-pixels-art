b4cd25ca4c6e64365d7eb9d0de310c8ed6dd214a
Lucas <lb.camargo94@gmail.com>
2021-09-14 00:56:42 -0300

    concluido item 2

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-//Lista de cores possiveis
-let arrayColors = ['--blue', '--red', '--green', '--yellow', '--purple', '--orange', '--aqua', '--black', '--gray'];
-//Cria as divs de paleta de cores
-for (ind = 0; ind < 4; ind += 1) {
-  const element = document.getElementById('color-palette');
-  const divElement = document.createElement('div');
-  let newID = 'item' + (ind + 1);
-  divElement.setAttribute("id", newID);
-  element.appendChild(divElement);
+//Função gerando cores hexadecimais
+function gerar_cor_hexadecimal() {
+  return '#' + parseInt((Math.random() * 0xFFF))
+    .toString(16)
+    .padStart(3, '0');
 }
-//Função para retoranr um numero ramdômico
-function getRandomNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-//Função para adicionar cores nas divs de paleta de cores
-function setPixelColor(pixel) {
-  pixel.style.backgroundColor = arrayColors[getRandomNumber(0, arrayColors.length)];
-}
-//Adicionando cores nas divs de paleta de cores
-const colorPalette = document.getElementById('color-palette');
-for (ind = 0; ind < colorPalette.children.length(); ind += 1) {
-
-}
-//Função para criar a grid de pixels
-function createPrixelGrid(lin, col) {
-  for (line = 0; line <= lin; line += 1) {
-    for (colum = 0; colum <= col; colum += 1) {
-      const grid = getElementById('grid-drawing');
-      const gridElement = document.createElement('div');
-      divElement.setAttribute("class", 'pixel-empty');
-      grid.appendChild(gridElement);
-    }
+//Criar Bloco de Cores
+function criarBlocoDeCores(blocos) {
+  for (let ind = 0; ind < blocos; ind += 1) {
+    const newDiv = document.getElementById('color-palette');
+    const colorBlock = document.createElement('div');
+    colorBlock.classList.add('color');
+    colorBlock.setAttribute('id', `color${ind+1}`)
+    newDiv.appendChild(colorBlock);
   }
 }
+criarBlocoDeCores(4);
+//Montando Paleta de cores
+function criarPaleta() {
+  let cores = ['#000'];
+  for (let ind = 0; ind < cores.length; ind += 1) {
+    while (cores.length < 4) {
+      let newColor = gerar_cor_hexadecimal();
+      if (newColor != 0xFFF && newColor != cores[ind]) {
+        cores.push(newColor);
+      }
+    }
+  }
+  return cores;
+}
+//Adiconar core aos blocos da paleta de cores
+function addCores(){
+    const cores = criarPaleta();
+    const element = document.querySelector('#color-palette');
+    for(let ind = 0; ind < element.children.length; ind += 1){
+        const boxColor = document.getElementById(`color${ind+1}`);
+        console.log (boxColor);
+        boxColor.style.backgroundColor = cores[ind];
+    }
+}
+addCores();
