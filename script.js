d441e6175b6bc738e7e9d00125c3cea938d31e47
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-09-14 10:49:14 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const colors = document.getElementById('color-palette');
+const boardPixels = document.getElementById('pixel-board');
 
 // FUNÇÃO QUE CRIA PALETA DE CORES
 function colorPalette() {
@@ -9,32 +10,42 @@ function colorPalette() {
     colors.appendChild(palette);
   }
 }
-colorPalette();
 
 // FUNÇÃO PARA GERAR CORES ALEATÓRIAS
 // Ref.: Encontrei à solução, que retorna as cores aleatórias para as demais paletas no site https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
 function randomColor() {
-  const randomcolors = document.getElementById('color-palette');
   const quantityColor = 4;
   for (let idx = 1; idx < quantityColor; idx += 1) {
-    randomcolors.childNodes[idx].style.background = `#${parseInt((Math.random() * 0xFFFFFF))
+    colors.childNodes[idx].style.background = `#${parseInt((Math.random() * 0xFFFFFF))
       .toString(16)
       .padStart(6, '0')}`;
   }
   colors.firstElementChild.className = 'color selected';
   colors.firstElementChild.style.backgroundColor = 'black';
 }
-randomColor();
 
+// FUNÇÃO QUE SELECIONA AS CORES NA PALETA
 function selectColor(event) {
   const color = document.querySelector('.selected');
   const getColor = event.target;
   getColor.style.backgroundColor = color.style.backgroundColor;
 }
 
+// FUNÇÃO QUE PREENCHE FUNDO DO BOARD DE BRANCO
+function clearPixelBoard() {
+  const cleanPxl = document.getElementsByClassName('pixel');
+  for (let idx = 0; idx < cleanPxl.length; idx += 1) {
+    cleanPxl[idx].style.backgroundColor = 'white';
+  }
+}
+
+function clearPixel() {
+  const cleaning = document.getElementById('clear-board');
+  cleaning.addEventListener('click', clearPixelBoard);
+}
+
 // FUNÇÃO QUE CRIA QUADRO
 function boardPalette() {
-  const boardPixels = document.getElementById('pixel-board');
   const lines = 5;
   for (let lineIdx = 0; lineIdx < lines; lineIdx += 1) {
     const boardColor = document.createElement('div');
@@ -47,7 +58,6 @@ function boardPalette() {
     boardPixels.appendChild(boardColor);
   }
 }
-boardPalette();
 
 // FUNÇÃO PARA SELECIONAR COR
 function toggleSelector(event) {
@@ -56,3 +66,8 @@ function toggleSelector(event) {
   event.target.classList.add('selected');
 }
 colors.addEventListener('click', toggleSelector);
+
+colorPalette();
+randomColor();
+clearPixel();
+boardPalette();
