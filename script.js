fb0fe453f887f3347b8e373d5a9edacd1e412f03
Savio <s.drksilva@gmail.com>
2021-09-14 13:00:08 -0300

    correction identation

--- script.js
+++ script.js
@@ -1,73 +1,59 @@
 let colors = {
-    active: 'black',
-    previous: ['black', 'red', 'green', 'blue']
+  active: 'black',
+  previous: ['black', 'red', 'green', 'blue']
 }
 let board = {
-    size: 5,
-    content: []
+  size: 5,
+  content: []
 }
 
 const message = document.createElement('p');
 
 /* Paleta de coress */
 function generatePalette() {
-    const element = document.querySelector('#color-palette');
-    /* Ultimas cores selecionadas */
-    for (let item of colors.previous) {
-        const div = document.createElement('div');
-        div.classList.add('color');
-        div.style.background = item;
-        if (item === 'black')
-            div.classList.add('selected');
-        element.appendChild(div);
-    }
-
-    /* Seletor das cores */
-    // const form = document.createElement('form');
-    // const input = document.createElement('input');
-
-    // input.classList.add('color', 'colorpick');
-    // input.setAttribute('type', 'color');
-
-    // message.innerHTML = "Seletor de Cores";
-
-    // element.appendChild(form);
-    // form.appendChild(message);
-    // form.appendChild(input);
+  const element = document.querySelector('#color-palette');
+  /* Ultimas cores selecionadas */
+  for (let item of colors.previous) {
+    const div = document.createElement('div');
+    div.classList.add('color');
+    div.style.background = item;
+    if (item === 'black')
+      div.classList.add('selected');
+    element.appendChild(div);
+  }
 }
 
 function generatePixelGrid() {
-    let columns = [], rows = [];
-    
-    const element = document.querySelector('#pixel-board');
-    const div = document.createElement('div');
-    div.style.background = 'white';
-    div.classList.add('pixel');
+  let columns = [], rows = [];
 
-    for (let i = 0; i < board.size; i++) {
-        let per = '1fr';
-        columns.push(per);
+  const element = document.querySelector('#pixel-board');
+  const div = document.createElement('div');
+  div.style.background = 'white';
+  div.classList.add('pixel');
+
+  for (let i = 0; i < board.size; i++) {
+    let per = '1fr';
+    columns.push(per);
+  }
+
+  element.style.gridTemplateColumns = columns.join(' ');
+  element.style.gridTemplateRows = columns.join(' ');
+
+  for (let k = 0; k < board.size; k++) {
+    for (let v = 0; v < board.size; v++) {
+      div.style.background = 'white';
+      div.classList.add('pixel');
+      element.appendChild(div);
     }
-
-    element.style.gridTemplateColumns = columns.join(' ');
-    element.style.gridTemplateRows = columns.join(' ');
-
-    for (let k = 0; k < board.size; k++) {
-        for (let v = 0; v < board.size; v++) {
-            const div = document.createElement('div');
-            div.style.background = 'white';
-            div.classList.add('pixel');
-            element.appendChild(div);
-        }
-    }
+  }
 }
 /*
 * Atualiza as cores dos elementos;
 */
 function updateElements(elements) {
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].style.background = colors.previous[i];
-    }
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.background = colors.previous[i];
+  }
 }
 
 /*
@@ -75,66 +61,53 @@ function updateElements(elements) {
 */
 function updateColorPrevious(active) {
 
-    for (let i in colors.previous) {
-        if ((colors.previous.length - 1) === Number(i))
-            continue;
-        colors.previous[i] = colors.previous[Number(i) + 1];
-    }
-    colors.previous[colors.previous.length - 1] = active;
+  for (let i in colors.previous) {
+    if ((colors.previous.length - 1) === Number(i))
+      continue;
+    colors.previous[i] = colors.previous[Number(i) + 1];
+  }
+  colors.previous[colors.previous.length - 1] = active;
 }
 
 function updatePalette() {
-    // let input = document.querySelector('.colorpick');
-    let elements = document.querySelectorAll('.color:not(.colorpick)');
-    let prev = elements[elements.length - 1].style.background,
-        active, lastElement;
-    // input.addEventListener('change', event => {
-    //     active = input.value;
-    //     // Atualiza as cores do objeto colors;
-    //     updateColorPrevious(active);
-    //     // Atualiza as cores da paleta
-    //     updateElements(elements);
-    //     // Seleciona a cor
-    //     colors.active = active;
-    // });
+  let elements = document.querySelectorAll('.color:not(.colorpick)');
+  let active, lastElement;
 
-    elements.forEach(element => {
-        element.addEventListener('click', event => {
-            active = element.style.background;
-            // Atualiza as cores do objeto colors;
-            //updateColorPrevious(active);
-            // Atualiza as cores da paleta
-            updateElements(elements);
-            for (let value of elements){
-                value.classList.remove('selected');
-            }
-            element.classList.add('selected');
-            // Seleciona a cor
-            colors.active = active;
-        });
+  elements.forEach(element => {
+    element.addEventListener('click', event => {
+      active = element.style.background;
+      // Atualiza as cores da paleta
+      updateElements(elements);
+      for (let value of elements) {
+        value.classList.remove('selected');
+      }
+      element.classList.add('selected');
+      // Seleciona a cor
+      colors.active = active;
     });
+  });
 }
 
 function clearPixelGrid() {
-    const element = document.querySelectorAll('.pixel');
-    element.forEach(element => {
-        element.style.background = 'white';
-    });
+  const elements = document.querySelectorAll('.pixel');
+  elements.forEach(element => {
+    element.style.background = 'white';
+  });
 }
 
-window.onload = function() {
-    // Inicializade uma paleta de cores com valores padrões
-    generatePalette();
-    // Atualizaz a paleta de cores;
-    updatePalette();
-    // Gera o Pixel Grid;
-    generatePixelGrid();
-    document.querySelector('#clear-board').addEventListener('click', event => {
-        clearPixelGrid();
+window.onload = function () {
+  // Inicializade uma paleta de cores com valores padrões
+  generatePalette();
+  // Atualizaz a paleta de cores;
+  updatePalette();
+  // Gera o Pixel Grid;
+  generatePixelGrid();
+  document.querySelector('#clear-board').addEventListener('click', event => {
+    clearPixelGrid();
+  });
+  document.querySelectorAll('.pixel').forEach(element => {
+    element.addEventListener('click', event => {
+      element.style.background = colors.active;
     });
-    document.querySelectorAll('.pixel').forEach(element => {
-        element.addEventListener('click', event =>  {
-            element.style.background = colors.active;
-        });
-    });
+  });
 }
