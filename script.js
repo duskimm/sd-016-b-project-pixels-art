6d62d3e6ff6c0a720be5db4915502e575d1c1b27
ErickVianaC <erickcosta535@gmail.com>
2021-09-14 18:59:28 -0300

    Melhorando requisitos

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-let boxBlue = document.getElementById("blue");
-let boxBlack = document.getElementById("black");
-let boxRed = document.getElementById("red");
-let boxYellow = document.getElementById("yellow");
-let bigBox = document.getElementById(".pixel")
 
-boxBlue.addEventListener('click', selectColor);
-boxBlack.addEventListener('click', selectColor);
-boxRed.addEventListener('click', selectColor);
-boxYellow.addEventListener('click', selectColor);
-
-bigBox.addEventListener('click', function)
-
-function selectColor() {
-    const color = this.id;
-    localStorage.setItem("cor",color);
