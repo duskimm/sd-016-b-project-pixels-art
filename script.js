ff00a0f2e120f1a67ac4d329a935db99268e6966
Marcus Thadeu <marcusthadeu@outlook.com>
2021-09-14 15:48:16 -0300

    Step 7

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-window.onload = document.querySelector('#black');
+const firstColor = document.querySelector('#black');
+firstColor.classList.add('selected');
+
+const colors = document.querySelectorAll('.color')
+
+for (let i = 0; i < colors.length; i++) {
+  colors[i].addEventListener('click', checkColor);
+}
+  function checkColor(color) {
+  for (let i = 0; i < colors.length; i++) {
+    colors[i].classList.remove('selected');
+  }
+  color.target.classList.add('selected')
+}
+
+  
