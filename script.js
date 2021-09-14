a63249e71e52a72fb8e2529fc633077dab0f4f03
Lucas Bertarelli <lucas.bertarelli@gmail.com>
2021-09-14 18:52:01 -0300

    finaliza os obrigatórios

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-let colorOnload = document.getElementById('black');
+const colorOnload = document.getElementById('black');
 colorOnload.classList.add('selected');
 
 const colors = document.getElementsByClassName('color');
-function colorSelection (event) {
-    for (let index = 0; index < colors.length; index += 1) {
-        colors[index].classList.remove('selected');
-    }
-    event.target.classList.add('selected');
+function colorSelection (event){
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
 }
 for (let index = 0; index < colors.length; index += 1) {
-    colors[index].addEventListener('click', colorSelection);
+  colors[index].addEventListener('click', colorSelection);
 }
 // Exercicio solucionado com auxilio do Gabriel Pinheiro.
-function paintPixel (event) {
-    const color = document.querySelector('.selected');
-    event.target.style.backgroundColor = color.innerText;
+function paintPixel (event){
+  const color = document.querySelector('.selected');
+  event.target.style.backgroundColor = color.innerText;
 }
 const pixels = document.getElementsByClassName('pixel');
 for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].addEventListener('click', paintPixel);
+  pixels[index].addEventListener('click', paintPixel);
 }
 // botão que remove as cores:
 
 function removeColors () {
-    const pixels = document.getElementsByClassName('pixel');
-    for (let index = 0; index < pixels.length; index += 1) {
-        if (pixels[index].style.backgroundColor !== 'white') {
-            pixels[index].style.backgroundColor = 'white';
-        }
+  const pixels = document.getElementsByClassName('pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    if (pixels[index].style.backgroundColor !== 'white') {
+        pixels[index].style.backgroundColor = 'white';
     }
+  }
 }
 document.getElementById('clear-board').addEventListener('click', removeColors);
