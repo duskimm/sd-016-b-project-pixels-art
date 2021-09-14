82ebf1e8d0962577701b1ca32fd731b4c7ba28ae
AmandaSottani <amandasottani@gmail.com>
2021-09-14 20:08:24 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-window.onload = function beginning (){
-  document.getElementsByClassName("color")[0].classList.add("selected");
+window.onload = function beginning() {
+  document.getElementsByClassName('color')[0].classList.add('selected');
   
-	let colorList = document.getElementsByClassName("color");
-	for (let i=0; i < colorList.length; i++) {
-	  colorList[i].addEventListener("click", changeColorSelected);
+	
+	let colorList = document.getElementsByClassName('color');
+	for (let i = 0; i < colorList.length; i++) {
+	  colorList[i].addEventListener('click', changeColorSelected);
 	}
-  function changeColorSelected(event){
-		const lastColor = document.querySelector(".selected");
-		lastColor.classList.remove("selected");
-		event.target.classList.add("selected");	
-	}
-	
-	
-	
+  function changeColorSelected(event) {
+		const lastColor = document.querySelector('.selected');
+		lastColor.classList.remove('selected');
+		event.target.classList.add('selected');	
+  }
 
+	let pixelsList = document.getElementsByClassName('pixel');
+	for (let i = 0; i < pixelsList.length; i++) {
+	  pixelsList[i].addEventListener('click', changeColor);
+	}
+
+	function changeColor(event) {
+		let currentSelected = document.querySelector('.selected');
+		let color = window.getComputedStyle(currentSelected, null).getPropertyValue("background-color");
+		event.target.style.backgroundColor = color ;
+	}
+
+ 
 }
