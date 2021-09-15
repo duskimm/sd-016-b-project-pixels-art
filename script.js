6bb765ea7416eb28a9bae34bdae489b9ff363f01
Helder Emanuel Alves de Oliveira <hlremanuel16@gmail.com>
2021-09-15 19:07:59 -0300

    resolve o requisito 7.

--- script.js
+++ script.js
@@ -28,14 +28,35 @@ function createPixelBoard() {
       SetPixelLineContainer.appendChild(setPixelLine);
     }
   }
-  
 }
 
 createPixelBoard();
 
 function selectFirstColor() {
-  let getBlack = document.getElementsByClassName('color')[0]
-  getBlack.classList.add('color','selected')
+  let getBlack = document.getElementsByClassName('color')[0];
+  getBlack.classList.add('color','selected');
 }
 
 selectFirstColor()
+// Código abaixo  (até o ".") Inspirado Por Juliane Cardoso: 
+//https://github.com/tryber/sd-016-b-project-pixels-art/pull/30/commits/372f2ce3dea0943945b8b4b573e45c1b80073429#diff-ed3ee7e0beea2498ff3b8ca85973d122fc6fa3d585d62b5807ec034d0cf076b3R18
+let getColors = document.getElementsByClassName('color');
+
+function removeSelect() {
+  for (let i = 0; i < getColors.length; i += 1) {
+    getColors[i].classList.remove('selected');
+  }
+}
+
+function addSelect(event) {
+  event.target.classList.add('selected');
+}
+
+for (let index = 0; index < getColors.length; index += 1) {
+  getColors[index].addEventListener('click', removeSelect);
+  getColors[index].addEventListener('click', addSelect);
+}
+//. 
+
+
+
