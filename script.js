9e3b17364188cd7f10734879c7280338dffe0fba
Celso Fiorani <celsofiorani@gmail.com>
2021-09-15 11:32:30 -0300

    parte 7

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ const paleta = document.getElementById("color-palette");
 
 
 // function que faz a classe `.selected` ser alterada dentro da id `color-palette`;
-function corSelecionada (){
-    cor.className = "selected";
-    paleta.addEventListener('click',selecionado)
-}
+// 1 Eu quero mudar a classe selected
+// 2 dentro da classe color
 
+function corSelecionada (event){
+    for (let index = 0; index < cor.length; index+= 1) {
+        cor[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected')
+}
+paleta.addEventListener('click',corSelecionada)
 
 
 
