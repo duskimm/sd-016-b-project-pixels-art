f465fc219af12e7a8f3faafe0a40b7ffa4ea8a0a
Istvan <olivaistvan@gmail.com>
2021-09-15 10:45:35 -0300

    Requisito 11

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
 const paleta = document.getElementById('color-palette');
-const cores = document.querySelectorAll('.color');
 const input = document.getElementById('board-size');
 const button = document.getElementById('generate-board');
 const pixelBoard = document.getElementById('pixel-board');
 const pixel = document.querySelector('.pixel')
 const buttonLimpar = document.getElementById('clear-board');
 
-// Alerta botão VQV
-button.addEventListener('click', function(){
-    if(input.value === ''){
-        alert('Board inválido!');
-    } else {
-        
-    }
-})
-
 // Requisito 07
 function selecionarCor(event){
     let selecionada = document.querySelector('.selected');
     let selecionar = event.target;
     selecionada.classList.remove('selected');
-    selecionar.classList.add('selected')
-    console.log(selecionar)
+    selecionar.classList.add('selected');
 }
 
 paleta.addEventListener('click', selecionarCor)
@@ -37,15 +26,42 @@ function pintar(event){
     event.target.style.backgroundColor = backgorund;
 }
 
-const pix = document.querySelectorAll('.pixel');
-
-for (let index = 0; index < pix.length; index += 1){
-pix[index].addEventListener('click', pintar)
-}
-// Requisito 09
-
 buttonLimpar.addEventListener('click', function(){
+    const pix = document.querySelectorAll('.pixel');
     for (let index = 0; index < pix.length; index += 1){
         pix[index].style.backgroundColor = 'white'
     }
 })
+
+// Alerta botão VQV - Requisito 10
+button.addEventListener('click', function(){
+    if(input.value === ''){
+      alert('Board inválido!');
+  } else {
+      createBoard();
+  }
+})
+
+// Essa função aqui em baixo , está dentro da função de cima
+function createBoard (){
+    let n = input.value;
+        if(n < 5) {
+        n = 5
+    } else if(n > 50){
+        n = 50
+    }
+    pixelBoard.innerHTML = '';
+
+    for (let i = 0; i < n ; i += 1){
+        let linha = document.createElement('div');
+        linha.classList.add('linha')
+        pixelBoard.appendChild(linha);
+        for(let index = 0; index < n ; index +=1){
+            let coluna = document.createElement('div');
+            linha.appendChild(coluna);  
+            coluna.classList.add('pixel');
+            coluna.addEventListener('click', pintar)
+        }
+    }
+};
+createBoard();
