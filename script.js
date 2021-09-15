f40dacfb32460c08a209d00fc51469922d7e261b
carrers <carrersimoes@gmail.com>
2021-09-15 11:06:46 -0300

    correção requisito 7

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const black = document.querySelector('.color1');
-black.classList.add = '.selected'
+window.onload = function() {
+    const color0 = document.querySelector('.color0');
+    color0.classList.add('selected');
+}
 
-const selectColor = document.getElementById('#color');
+const color0 = document.getElementsByClassName('color0');
+const color1 = document.getElementsByClassName('color1');
+const color2 = document.getElementsByClassName('color2');
+const color3 = document.getElementsByClassName('color3');
+
+//requisito 7, com o auxílio da Mariana Saraiva que salvou minha vida.
+
+const selectColor = document.querySelectorAll('.color');
 function selectedColor(event) {
-
     for (let index = 0; index < selectColor.length; index += 1) {
-
-        selectedColor[i].classList.remove('.selected');
-    event.target.classList.add('.selected');
+        selectColor[index].classList.remove('selected');
 }
-
-    selectColor[index].addEventListener('click', selectColor);
+event.target.classList.toggle('selected');
 }
-
+    for (let index = 0; index < selectColor.length; index += 1) {
+        selectColor[index].addEventListener('click', selectedColor);
+}
