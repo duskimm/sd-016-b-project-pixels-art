a99847021f196e527a4af263e0b1f7d25a03d672
AmandaSottani <amandasottani@gmail.com>
2021-09-15 20:47:14 -0300

    criei uma função para definir quantidade de pixels inicial

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function beginning() {
   document.getElementsByClassName('color')[0].classList.add('selected');
   
-	
+	//criando funcão que seleciona as cores da paleta
 	let colorList = document.getElementsByClassName('color');
 	for (let i = 0; i < colorList.length; i++) {
 	  colorList[i].addEventListener('click', changeColorSelected);
@@ -12,7 +12,23 @@ window.onload = function beginning() {
 		event.target.classList.add('selected');	
   }
 
-	let pixelsList = document.getElementsByClassName('pixel');
+	//criando quadro de pixels 5x5
+  let linesBoard = document.getElementsByClassName('pixel-row');
+	for (let i = 0; i < linesBoard.length; i ++) {
+		for (let index = 0; index < 5; index ++) {
+			createPixelColumn(linesBoard[i]);
+		}
+	}
+
+	function createPixelColumn (parent) {
+		let column = document.createElement('div');
+		column.classList.add("pixel");
+		parent.appendChild(column);
+	}
+
+
+	//criando função que colore os pixels
+	let pixelsList = document.getElementsByClassName('pixel-row');
 	for (let i = 0; i < pixelsList.length; i++) {
 	  pixelsList[i].addEventListener('click', changeColor);
 	}
@@ -22,7 +38,8 @@ window.onload = function beginning() {
 		let color = window.getComputedStyle(currentSelected, null).getPropertyValue("background-color");
 		event.target.style.backgroundColor = color ; 
 	}
-
+  
+	//criando função que limpa os pixels
 	document.querySelector('#clear-board').addEventListener('click',cleanAll);
 
 	function cleanAll(event) {
@@ -31,12 +48,10 @@ window.onload = function beginning() {
 		}
 	}
 
-	let size = document.querySelector('#board-size');
+  // criando função para escolher o tamanho do quadro de pixels
+	let tamanhoEscolhido = parseInt(document.getElementById('board-size').value);
+	
 	document.querySelector('#vqv').addEventListener('click',defineTheSize);
   
-	function defineTheSize(event) {
-		if (size != Number ){
-			alert("Board inválido!");
-		}
-	}
+
 }
