14803f39cfc2f13078bca2ff09b7da0a84439e55
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-16 15:34:38 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,70 @@
+window.onload = function() {
+  
+
+    function selectedColor() {
+        let pixelCor1 = document.getElementById('cor1')
+        let pixelCor2 = document.getElementById('cor2')
+        let pixelCor3 = document.getElementById('cor3')
+        let pixelCor4 = document.getElementById('cor4')
+        let pixelCor5 = document.getElementById('cor5')
+        let selectedColor = document.getElementsByClassName('selectedColor')
+
+        pixelCor1.addEventListener('click', function() {
+            if(selectedColor.length === 0) {
+              pixelCor1.className = 'color selectedColor'
+            }else{
+              pixelCor1.className = 'color'  
+            }
+        })
+             
+        pixelCor2.addEventListener('click', function() {
+            if(selectedColor.length === 0) {
+              pixelCor2.className = 'color selectedColor'
+            }else{
+              pixelCor2.className = 'color'  
+            }
+        })
+
+        pixelCor3.addEventListener('click', function() {
+            if(selectedColor.length === 0) {
+              pixelCor3.className = 'color selectedColor'
+            }else{
+              pixelCor3.className = 'color'  
+            }
+        })
+
+        pixelCor4.addEventListener('click', function() {
+            if(selectedColor.length === 0) {
+              pixelCor4.className = 'color selectedColor'
+            }else{
+              pixelCor4.className = 'color'  
+            }
+        })
+
+        pixelCor5.addEventListener('click', function() {
+            if(selectedColor.length === 0) {
+              pixelCor5.className = 'color selectedColor'
+            }else{
+              pixelCor5.className = 'color'
+            }
+        })
+
+    }
+
+    selectedColor()
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+   function pintaPixel() {
+    let pixel = document.getElementsByClassName('pixel')
+    let selectedColor = document.getElementsByClassName('selectedColor')
+    for(let i = 0; i < pixel.length; i++) {
+      pixel[i].addEventListener('click', function(){
+        pixel[i].style.backgroundColor = selectedColor[0].style.backgroundColor.toString()
+      })
+    }
+   }
+
+   pintaPixel();
+   
+
+
+}
