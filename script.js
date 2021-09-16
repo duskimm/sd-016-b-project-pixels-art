65b89e02be9945cac7988fdf1f81abc81813f7cc
natashiaromanzini <natashiaromanzini1@gmail.com>
2021-09-16 15:01:47 -0300

    ta indo

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-let quadrados = document.getElementsByClassName ('pixel-board')
-function crialinhas (){
-for (let i=0;i<5;i++){
-let quadrados = document.createElement('div');
-quadrados.className = 'pixel'}
+const L1 = document.getElementById('L1');
+const L2 = document.getElementById('L2');
+const L3 = document.getElementById('L3');
+const L4 = document.getElementById('L4');
+const L5 = document.getElementById('L5');
+
+function criacoluna (quadradao){
+
+for (i=0; i<5; i+=1){
+  const coluna= document.createElement('div');
+  coluna.className='pixel';
+  quadradao.appendChild(coluna)
 }
+}
+
+
+criacoluna(L1)
+criacoluna(L2)
+criacoluna(L3)
+criacoluna(L4)
+criacoluna(L5)]
+
+let selecionacor = 
+
