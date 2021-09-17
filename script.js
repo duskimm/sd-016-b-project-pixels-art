079e404721a3beca929f57c3a2a0f4eebb2ac7f4
Matheus Machado <matheusmdec@hotmail.com>
2021-09-17 14:56:54 -0300

    Requisito 08

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function povoaBoard() {
         let pixel = document.createElement('div')
         pixel.className = 'pixel'
         board.appendChild(pixel)
+        pixel.addEventListener('click', function(event) {
+            
+            
+        })
     }
 }
 //Requisito 07
@@ -12,20 +16,45 @@ function setColorSelectEvent() {
     let corLista = document.getElementsByClassName('color')
     paleta.addEventListener('click',function(event) {
         let id = event.target.id
-        let cor = document.getElementById(id)
+        let div = document.getElementById(id)
         for(let i = 0; i < corLista.length; i++){
             if (corLista[i].id === id) {
                 corLista[i].className = 'color selected'
+                let cor = window.getComputedStyle(div, null).getPropertyValue('background-color')
+                saveSelectedColorInSession(cor)
             } else {
                 corLista[i].className = 'color'
             }
         }
     })
 }
+function saveSelectedColorInSession(cor) {
+    if(sessionStorage.getItem('colorSelected') === null){
+        sessionStorage.setItem('colorSelected', JSON.stringify([]))
+    }
+    sessionStorage.setItem('colorSelected', JSON.stringify(cor))
+}
+//Requisito 08
+function setColorPaintEvent() {
+   document.querySelectorAll('.pixel').forEach(pixel => {
+       pixel.addEventListener('click', event => {
+           console.log(pixel);
+           let pixelCor = window.getComputedStyle(event.target, null).getPropertyValue('background-color')
+           let corSelecionada = JSON.parse(sessionStorage.getItem('colorSelected'))
+           console.log(pixelCor);
+           console.log(corSelecionada);
+           if(pixelCor !== corSelecionada){
+               event.target.style.backgroundColor = corSelecionada
+           }           
+       })
+   })
+}
 
 
 
 window.onload = function() {
     povoaBoard()
+    saveSelectedColorInSession('rgb(0,0,0)')
     setColorSelectEvent()
+    setColorPaintEvent()
 }
