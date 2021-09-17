6420a2bd01637b6ad268551260cc2ae8639a4b1c
Matheus Machado <matheusmdec@hotmail.com>
2021-09-17 06:44:13 -0300

    Requisito 07

--- script.js
+++ script.js
@@ -6,9 +6,26 @@ function povoaBoard() {
         board.appendChild(pixel)
     }
 }
+//Requisito 07
+function setColorSelectEvent() {
+    let paleta = document.getElementById('color-palette')
+    let corLista = document.getElementsByClassName('color')
+    paleta.addEventListener('click',function(event) {
+        let id = event.target.id
+        let cor = document.getElementById(id)
+        for(let i = 0; i < corLista.length; i++){
+            if (corLista[i].id === id) {
+                corLista[i].className = 'color selected'
+            } else {
+                corLista[i].className = 'color'
+            }
+        }
+    })
+}
 
 
 
 window.onload = function() {
     povoaBoard()
+    setColorSelectEvent()
 }
