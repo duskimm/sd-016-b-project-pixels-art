ba47fa8494a58ee23fbb76c36cff06ab572177ed
Hugo Maximiano <hugomaximiano@hotmail.com>
2021-09-17 15:55:29 -0300

    passo 3

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+let idx = 5;
+let pixelBoard = document.getElementById("pixel-board");
+
+function coluna (){
+    for (let index = 0; index <= idx; index+=1) {
+        let pixelColuna = document.createElement("div");
+        pixelBoard.appendChild(pixelColuna);
+    }
+}
+
+function linha(){
+    for (let index = 0; index < idx; index+=1) {
+        let pixelLinha = document.createElement("div");
+        pixelLinha.className= "linhas";
+        pixelBoard.appendChild(pixelLinha)
+    }
+}
+
+linha()
+coluna ()
