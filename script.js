e0da2a3c9354f995c3290364f265e2713041e854
Hugo Maximiano <hugomaximiano@hotmail.com>
2021-09-17 19:04:32 -0300

    passo 4

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-
+window.onload
 let idx = 5;
 let pixelBoard = document.getElementById("pixel-board");
 
-function coluna (){
+function linha(){
+   for (let index = 0; index < idx; index+=1) {
+        let pixelLinha = document.createElement("div");
+        pixelLinha.className = "linha";
+        pixelBoard.appendChild(pixelLinha)
+       coluna (pixelLinha)
+   }   
+}
+function coluna (linha){
     for (let index = 0; index <= idx; index+=1) {
         let pixelColuna = document.createElement("div");
-        pixelBoard.appendChild(pixelColuna);
+        pixelColuna.className = "pixel"
+        linha.appendChild(pixelColuna);
     }
 }
 
-function linha(){
-    for (let index = 0; index < idx; index+=1) {
-        let pixelLinha = document.createElement("div");
-        pixelLinha.className= "linhas";
-        pixelBoard.appendChild(pixelLinha)
-    }
-}
+
 
 linha()
-coluna ()
